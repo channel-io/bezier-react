2404ed805373cf472f08cbe037a16cf0152f29a8
hahnlee <hanlee.dev@gmail.com>
2020-06-17 23:32:11 +0900

    Add storybook knobs addon

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,11 +1,13 @@
 /* External dependencies */
 import React from 'react'
+import { withKnobs } from '@storybook/addon-knobs'
 
 /* Internal dependencies */
 import Button from './Button'
 
 export default {
   title: 'Button',
+  decorators: [withKnobs],
 }
 
 export const Primary = () => <Button />
