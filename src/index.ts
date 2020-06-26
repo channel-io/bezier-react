c13c61dc9938527e7168eea780caf28930ad62a6
Leo Jeong <thinkratomos@gmail.com>
2020-06-26 11:27:10 +0900

    Layout export 추가 (#30)

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,9 @@ export * from './components/Button'
 export * from './components/Switch'
 export * from './components/Text'
 
+/* Layout */
+export * from './layout/GNB'
+export * from './layout/Navigation'
+
+/* Styling */
 export * from './styling/Theme'
