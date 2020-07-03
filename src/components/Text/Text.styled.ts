4c556880086409a90649ad77a154b576bc035b9a
Jinwoo Leo Jeong <thinkratomos@gmail.com>
2020-07-03 17:22:11 +0900

    Han 리뷰 반영

--- src/components/Text/Text.styled.ts
+++ src/components/Text/Text.styled.ts
@@ -7,7 +7,7 @@ import TextProps from './Text.types'
 const Text = styled.span<TextProps>`
   ${props => props.typo};
   color: ${props => props.theme?.colors?.textBase ?? 'black'};
-  font-weight: ${props => (props.bold ? 800 : 400)};
+  font-weight: ${props => (props.bold ? 'bold' : 'normal')};
   font-style: ${props => (props.italic ? 'italic' : 'normal')};
 `
 
