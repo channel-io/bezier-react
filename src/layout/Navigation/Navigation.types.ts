2dae593994c911e14b8a9b05a6230e6c734dffdd
Jinwoo Leo Jeong <thinkratomos@gmail.com>
2020-07-07 18:14:50 +0900

    width prop 추가

--- src/layout/Navigation/Navigation.types.ts
+++ src/layout/Navigation/Navigation.types.ts
@@ -4,6 +4,7 @@ import { ChildrenComponentProps } from '../../types/ComponentProps'
 export default interface NavigationProps extends Omit<ChildrenComponentProps, 'as'> {
   title?: string
   fixedTitle?: boolean
+  width?: number
   minWidth?: number
   maxWidth?: number
   disableResize?: boolean
