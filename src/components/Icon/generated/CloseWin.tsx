f8a9b5672dccc0fd92e74cd9a6317cfa3ec7e309
Jinwoo Leo Jeong <thinkratomos@gmail.com>
2020-07-09 15:19:48 +0900

    Windows 용 아이콘 추가

---
+++ src/components/Icon/generated/CloseWin.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function SvgCloseWin(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+      <path
+        fill="#A7A7AA"
+        fillRule="evenodd"
+        d="M16.5 6.793l.707.707-4.5 4.5 4.5 4.5-.707.707-4.5-4.5-4.5 4.5-.707-.707 4.5-4.5-4.5-4.5.707-.707 4.5 4.5 4.5-4.5z"
+      />
+    </svg>
+  )
+}
+
+export default SvgCloseWin
