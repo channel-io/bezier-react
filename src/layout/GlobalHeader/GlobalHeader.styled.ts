e48a9870c1f86908b783f1e72e37efbac2991a29
Jinwoo Leo Jeong <thinkratomos@gmail.com>
2020-07-16 17:52:39 +0900

    max-width 추가

--- src/layout/GlobalHeader/GlobalHeader.styled.ts
+++ src/layout/GlobalHeader/GlobalHeader.styled.ts
@@ -9,6 +9,7 @@ export const StyledGlobalHeader = styled.div<GlobalHeaderProps>`
   flex: none;
   width: 100%;
   height: ${props => (props.isWindows ? 32 : 40)}px;
+  max-width: 100vw;
   background-color: ${props => props.theme?.colors?.background1};
   box-shadow: inset 0 -1px 0 0 ${props => props.theme?.colors?.background3};
   transition: background-color 200ms ease-in-out;
