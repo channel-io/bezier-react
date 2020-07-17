50a14dfae10b5b50c7a0d14e6d359b42ea6b43c5
Leo Jeong <thinkratomos@gmail.com>
2020-07-17 17:48:53 +0900

    webkit-app-region css 속성 삭제 (#62)

--- src/layout/GlobalHeader/GlobalHeader.styled.ts
+++ src/layout/GlobalHeader/GlobalHeader.styled.ts
@@ -13,6 +13,4 @@ export const StyledGlobalHeader = styled.div<GlobalHeaderProps>`
   background-color: ${props => props.theme?.colors?.background1};
   box-shadow: inset 0 -1px 0 0 ${props => props.theme?.colors?.background3};
   transition: background-color 200ms ease-in-out;
-  -webkit-app-region: drag;
-  -webkit-user-select: none; /* stylelint-disable-line property-no-vendor-prefix */
 `
