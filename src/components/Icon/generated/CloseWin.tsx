2e4838380a68addbc540d129d63be4dd0da92dd3
Jinwoo Leo Jeong <thinkratomos@gmail.com>
2020-07-20 14:16:42 +0900

    svgr 설정 변경 - fill 을 명시적으로 추가하도록 변경

--- src/components/Icon/generated/CloseWin.tsx
+++ src/components/Icon/generated/CloseWin.tsx
@@ -4,7 +4,7 @@ function SvgCloseWin(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
       <path
-        fill="#A7A7AA"
+        fill="currentColor"
         fillRule="evenodd"
         d="M16.5 6.793l.707.707-4.5 4.5 4.5 4.5-.707.707-4.5-4.5-4.5 4.5-.707-.707 4.5-4.5-4.5-4.5.707-.707 4.5 4.5 4.5-4.5z"
       />
--- src/components/Icon/generated/EnlargeWin.tsx
+++ src/components/Icon/generated/EnlargeWin.tsx
@@ -4,7 +4,7 @@ function SvgEnlargeWin(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
       <path
-        fill="#A7A7AA"
+        fill="currentColor"
         fillRule="evenodd"
         d="M17 7v10H7V7h10zm-1 1H8v8h8V8z"
       />
