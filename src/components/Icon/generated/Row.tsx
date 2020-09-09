a0cc1372c813dcc0c96d9e273c517d1bd46eac21
Hakyeon Manuel Kim <khy@aooen.com>
2020-09-09 15:24:11 +0900

    row, column 아이콘 변경 (#122)

--- src/components/Icon/generated/Row.tsx
+++ src/components/Icon/generated/Row.tsx
@@ -5,8 +5,8 @@ function SvgRow(props: React.SVGProps<SVGSVGElement>) {
     <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
       <path
         fill="currentColor"
+        d="M19.5 13c.827 0 1.5.673 1.5 1.5v5c0 .827-.673 1.5-1.5 1.5h-15c-.827 0-1.5-.673-1.5-1.5v-5c0-.827.673-1.5 1.5-1.5h15zm-.5 2H5v4h14v-4zm.5-12c.827 0 1.5.673 1.5 1.5v5c0 .827-.673 1.5-1.5 1.5h-15C3.673 11 3 10.327 3 9.5v-5C3 3.673 3.673 3 4.5 3h15zM19 5H5v4h14V5z"
         fillRule="evenodd"
-        d="M19.5 3A1.5 1.5 0 0121 4.5v15a1.5 1.5 0 01-1.5 1.5h-15A1.5 1.5 0 013 19.5v-15A1.5 1.5 0 014.5 3h15zM5 5v14h2V5H5zm14 14v-3H9v3h10zm0-5v-4H9v4h10zM9 8h10V5H9v3z"
       />
     </svg>
   )
