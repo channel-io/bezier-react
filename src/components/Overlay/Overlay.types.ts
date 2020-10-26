a05c14fd1bf4e2e0d69ac15bbeedb02cc0b0e3c0
danivelop <mofg266@gmail.com>
2020-10-26 17:24:11 +0900

    lloyd 리뷰반영(타입수정)

--- src/components/Overlay/Overlay.types.ts
+++ src/components/Overlay/Overlay.types.ts
@@ -4,7 +4,7 @@ import OverlayPosition from './OverlayPosition'
 
 export default interface OverlayProps extends UIComponentProps, ChildrenComponentProps {
   show?: boolean
-  target: HTMLElement | null | undefined
+  target?: HTMLElement | null
   placement?: OverlayPosition
   marginX?: number
   marginY?: number
@@ -12,7 +12,7 @@ export default interface OverlayProps extends UIComponentProps, ChildrenComponen
 }
 
 export interface getOverlayStyleProps {
-  target: HTMLElement | null | undefined
+  target?: HTMLElement | null
   overlay: HTMLElement
   placement: OverlayPosition
   marginX: number
