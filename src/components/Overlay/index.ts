a8003863765d2b679eac445be0991c8be4f05982
danivelop <mofg266@gmail.com>
2020-10-28 19:26:26 +0900

    OverlayPosition export문제있던 부분 수정

--- src/components/Overlay/index.ts
+++ src/components/Overlay/index.ts
@@ -1,13 +1,13 @@
 /* Internal dependencies */
 import Overlay from './Overlay'
-import OverlayPosition from './OverlayPosition'
+import type OverlayPosition from './OverlayPosition'
 import type OverlayProps from './Overlay.types'
 
 export type {
   OverlayProps,
+  OverlayPosition,
 }
 
 export {
   Overlay,
-  OverlayPosition,
 }
