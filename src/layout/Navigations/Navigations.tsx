251d96e62016dff0e663714a4df2a342d398dd47
Mong <mong@channel.io>
2020-12-02 13:55:02 +0900

    set과 mergeRef를 함께 이용할 때 발생하는 버그 수정

--- src/layout/Navigations/Navigations.tsx
+++ src/layout/Navigations/Navigations.tsx
@@ -76,11 +76,9 @@ function Navigations({ children }: NavigationsProps) {
           if (!isNil(child.ref)) {
             set(child, 'ref.current', nav)
           }
-          set(navigationRefs.current, index, {
-            target: nav,
-            minWidth: child.props.minWidth,
-            maxWidth: child.props.maxWidth,
-          })
+          set(navigationRefs.current, [index, 'target'], nav)
+          set(navigationRefs.current, [index, 'minWidth'], child.props.minWidth)
+          set(navigationRefs.current, [index, 'maxWidth'], child.props.maxWidth)
         },
         optionIndex: index,
         onMouseDown: handleMouseDown,
@@ -95,11 +93,9 @@ function Navigations({ children }: NavigationsProps) {
   ])
 
   return (
-    <>
-      <NavigationsWrapper>
-        { renderNavigationList(children) }
-      </NavigationsWrapper>
-    </>
+    <NavigationsWrapper>
+      { renderNavigationList(children) }
+    </NavigationsWrapper>
   )
 }
 
