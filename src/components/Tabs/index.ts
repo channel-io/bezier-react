f6f1fe8ccd9267311772dd6fa9c50ab8579a2ddf
Hakyeon Manuel Kim <khy@aooen.com>
2021-03-04 19:14:00 +0900

    TabsSize가 export type이던 것을 그냥 export로 변경

--- src/components/Tabs/index.ts
+++ src/components/Tabs/index.ts
@@ -1,14 +1,12 @@
 import Tabs from './Tabs'
-/* eslint-disable import/no-duplicates */
+import { TabsSize } from './Tabs.types'
 import type TabsProps from './Tabs.types'
-import type { TabsSize } from './Tabs.types'
-/* eslint-enable import/no-duplicates */
 
 export type {
   TabsProps,
-  TabsSize,
 }
 
 export {
   Tabs,
+  TabsSize,
 }
