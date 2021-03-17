726aa83ce051c2e9b2ab4966d9b32c2c09c11e56
danivelop <mofg266@gmail.com>
2021-03-18 01:42:51 +0900

    overlay의 hide이벤트에는 capturing하도록 변경

--- src/hooks/useEventHandler.ts
+++ src/hooks/useEventHandler.ts
@@ -2,23 +2,36 @@
 import { useEffect } from 'react'
 import { noop, isNil, isFunction } from 'lodash-es'
 
+interface HandlerOptionsProps {
+  capture?: boolean
+  once?: boolean
+  passive?: boolean
+}
+
 function useEventHandler<K extends keyof HTMLElementEventMap>(
   element: any,
   eventName: K,
   handler: (event: HTMLElementEventMap[K]) => any,
   condition: boolean = true,
+  options: boolean | HandlerOptionsProps = false,
 ) {
   useEffect(() => {
     if (!isNil(element) && isFunction(element.addEventListener) && condition) {
-      element.addEventListener(eventName, handler)
+      element.addEventListener(eventName, handler, options)
 
       return function cleanup() {
-        element.removeEventListener(eventName, handler)
+        element.removeEventListener(eventName, handler, options)
       }
     }
 
     return noop
-  }, [condition, element, eventName, handler])
+  }, [
+    condition,
+    element,
+    eventName,
+    options,
+    handler,
+  ])
 
   return null
 }
