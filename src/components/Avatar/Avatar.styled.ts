6472516c1d9780c7def0072942a93615a6a18b19
Ed Sungik Choi <sungik.dev@gmail.com>
2021-04-08 20:18:04 +0900

    스타일 린트 에러 수정 및 상수 분리

--- src/components/Avatar/Avatar.styled.ts
+++ src/components/Avatar/Avatar.styled.ts
@@ -1,6 +1,8 @@
 /* Internal denpendencies */
-import { styled, css, smoothCorners } from '../../foundation'
+import { styled, smoothCorners } from '../../foundation'
 import { AvatarSize } from './Avatar.types'
+
+const AVATAR_BORDER_RAIDUS_PERCENTAGE = 42
 
 export interface AvatarProps {
   size: AvatarSize
@@ -20,11 +22,11 @@ export const StyledAvatar = styled.div<AvatarProps>`
   background-color: transparent;
   background-image: ${({ src }) => `url(${src})`};
   background-size: cover;
-  border-radius: 42%;
+  border-radius: ${AVATAR_BORDER_RAIDUS_PERCENTAGE}%;
   outline: none;
 
   ${({ src }) => smoothCorners({
-    n: 2.38,
+    borderRadius: `${AVATAR_BORDER_RAIDUS_PERCENTAGE}%`,
     backgroundColor: '#EFEFF0', // TODO: fallback default avatar 설정
     backgroundImage: src,
   })};
@@ -42,15 +44,15 @@ export const AvatarEllipsis = styled.div<AvatarGroupProps>`
   position: absolute;
   top: 0;
   right: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
   z-index: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   width: ${({ spacing }) => `calc(100% - ${spacing}px)`};
   height: 100%;
 
   ${({ foundation }) => smoothCorners({
-    n: 2.38,
+    borderRadius: `${AVATAR_BORDER_RAIDUS_PERCENTAGE}%`,
     backgroundColor: foundation?.theme?.['dim-dark'],
   })}
 `
