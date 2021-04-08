ed1bfa4d3a6a42257c2f93ce3534b461cc67e65f
Leo Jeong <thinkratomos@gmail.com>
2021-04-08 17:32:14 +0900

    SmoothCorners 로직을 수정 (#315)
    * SmoothCorners 로직을 수정
    
    * borderRadius 를 가져와 이로부터 초타원의 n(지수)를 역산하도록 변경
    * borderRadius 를 number 와 string (percentage) 모두 받을 수 있도록 변경
    * image 그리기 시작 지점이 올바르지 못하게 계산된 문제 수정
    
    * Revert: shadow 기본값 수정
    
    * n = 2.5 로 보정하던 상수 제거
    
    * percentage 로 들어오는 border radius 를 absolute 한 값으로 재계산 하는 로직 추가
    
    * 이미 number 로 형 변환 된 변수를 중복으로 형 변환 시도하는 부분 제거

--- src/foundation/Mixins.ts
+++ src/foundation/Mixins.ts
@@ -21,7 +21,7 @@ export const hideScrollbars = () => css`
 `
 
 interface SmoothCornersOptions {
-  n?: number
+  borderRadius?: number | string
   shadow?: string
   backgroundColor?: string
   backgroundImage?: string
@@ -30,7 +30,7 @@ interface SmoothCornersOptions {
 }
 
 export const smoothCorners = ({
-  n = 3.5,
+  borderRadius = 0,
   shadow = 'none',
   backgroundColor = 'white',
   backgroundImage = '',
@@ -40,16 +40,16 @@ export const smoothCorners = ({
   @supports (background: paint(smooth-corners)) {
     padding: ${shadowBlur * 2}px;
     margin: ${-(shadowBlur * 2) + margin}px;
-    border-radius: 0;
-    box-shadow: none;
     background: paint(smooth-corners);
-
+    border-radius: 0;
     /* Custom property 는 CSSUnparsedValue 로만 잡혀서 사용하는 임시 속성 */
     border-image-source: url(${backgroundImage});
+    box-shadow: none;
 
-    --smooth-corners: ${n};
+    --smooth-corners: ${borderRadius};
     --smooth-corners-shadow: ${shadow};
     --smooth-corners-bg-color: ${backgroundColor};
     --smooth-corners-padding: ${shadowBlur * 2};
+    --smooth-corners-radius-unit: ${typeof borderRadius === 'string' ? 'string' : 'number'};
   }
 `
