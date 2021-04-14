7efedc0b87ac50701a67510179e69b84974d4b24
Dugi (Dongwook Hwang) <inhibitor@kaist.ac.kr>
2021-04-14 16:11:03 +0900

    SectionLabel 컴포넌트 추가 (#301)
    * SectionLabel 컴포넌트 추가
    
    * lodash -> lodash-es로 변경 (daniel 리뷰 반영)
    
    * Styled component의 prop이 optional하지 않도록 변경 (daniel 리뷰 반영)
    
    * SectionLabel 본체와 child를 div로 감싸도록 수정 (daniel 리뷰 반영)
    
    * SectionLabelProps가 ChildrenComponentProps를 상속하도록 변경 (mong 리뷰 반영)
    
    * Children 감추는 로직을 unmount 대신 display: none으로 처리 (mong 리뷰 반영)
    
    * 디자인팀과 싱크에 따라 SectionLabel 스펙 확정된 점 적용
    
    * 각 Wrapper 컴포넌트에 className prop 지원
    
    * Element key를 array index로 사용하지 않도록 수정
    
     * 대신 item이 수정될 때마다 uuid로 부여
    
    * Interpolation을 주입할 수 있도록 prop interface 확장
    
    * RightContentWrapper 보여주는 조건의 readability 개선 (daniel 리뷰 반영)
    
    * lodash-es tree shaking 되도록 개별 유틸 함수를 import (daniel 리뷰 반영)
    
    * 불필요한 newline 제거 (mong 리뷰 반영)
    
    * InjectedInterpolation 타입을 별도 파일로 분리 (mong 리뷰 반영)
    
    * WithInterpolation에서 InjectedInterpolation 사용하도록 변경
    
    * rightContent 렌더링하지 않는 조건을 더 명확하게 처리 (daniel 리뷰 반영)

---
+++ src/components/SectionLabel/SectionLabel.types.ts
@@ -0,0 +1,41 @@
+/* External dependencies */
+import type React from 'react'
+
+/* Internal dependencies */
+import type { ThemeKey } from '../../foundation/Theme/ThemeType'
+import type { ChildrenComponentProps } from '../../types/ComponentProps'
+import type InjectedInterpolation from '../../types/InjectedInterpolation'
+import type { IconName, IconSize } from '../Icon'
+
+export type SectionLabelHelpProps = {
+  icon?: IconName
+  iconSize?: IconSize
+  iconColor?: ThemeKey
+  tooltipContent: string
+}
+
+export type SectionLabelItemProps = {
+  icon: IconName
+  iconColor?: ThemeKey
+  onClick?(e: React.MouseEvent): void
+} | {
+  content: React.ReactElement
+}
+
+export default interface SectionLabelProps extends ChildrenComponentProps {
+  content?: React.ReactNode
+  open?: boolean
+  divider?: boolean
+  help?: SectionLabelHelpProps
+  left?: SectionLabelItemProps
+  right?: SectionLabelItemProps | SectionLabelItemProps[]
+  wrapperClassName?: string
+  wrapperInterpolation?: InjectedInterpolation
+  contentWrapperClassName?: string
+  contentWrapperInterpolation?: InjectedInterpolation
+  leftWrapperClassName?: string
+  leftWrapperInterpolation?: InjectedInterpolation
+  rightWrapperClassName?: string
+  rightWrapperInterpolation?: InjectedInterpolation
+  onClick?: (e: React.MouseEvent) => void
+}
