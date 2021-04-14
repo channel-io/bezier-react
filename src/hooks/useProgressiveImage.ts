694ff9b2f2c9cb06afb149cc65b9bd527d714d46
Ed Sungik Choi <sungik.dev@gmail.com>
2021-04-14 18:20:56 +0900

    useProgressiveImage에서 이미지를 캐시하도록 변경
    - 기존에 useState 초기값을 null로 가져갔기 때문에, 브라우저에서 이미지를 캐시하더라도 다시 이미지가 마운트될때는 기본 이미지가 보였다(깜빡이는듯한 현상 발생). 이를 방지하기위해 내부에 이미지 캐시를 만들고, 마운트될 때 state 초기값을 캐시에 접근해서 가져올 수 있도록 했다.

--- src/hooks/useProgressiveImage.ts
+++ src/hooks/useProgressiveImage.ts
@@ -1,24 +1,54 @@
 /* External dependencies */
 import { useState, useEffect } from 'react'
 
+interface CacheImage {
+  src: string
+  isLoaded: boolean
+}
+
+const LOAD = 'load'
+const ERROR = 'error'
+
+const imageCache = new Map<string, CacheImage>()
+
+function getInitialSource(src: string) {
+  const cachedImage = imageCache.get(src)
+  if (!cachedImage) { return null }
+  return cachedImage
+}
+
 export default function useProgressiveImage(src: string) {
-  const [source, setSource] = useState<string | null>(null)
+  const [source, setSource] = useState<CacheImage | null>(() => getInitialSource(src))
 
   useEffect(() => {
-    const img = new Image()
-    img.src = src
+    if (source) { return undefined }
 
-    function loadImage() {
-      setSource(src)
+    const image = new Image()
+    image.src = src
+
+    function loadImage(event: Event) {
+      const cachedImage = {
+        src,
+        isLoaded: event.type === LOAD,
+      }
+      setSource(cachedImage)
+      imageCache.set(src, cachedImage)
     }
 
-    img.addEventListener('load', loadImage)
+    image.addEventListener(LOAD, loadImage)
+    image.addEventListener(ERROR, loadImage)
 
     return function cleanup() {
-      img.removeEventListener('load', loadImage)
+      image.removeEventListener(LOAD, loadImage)
+      image.removeEventListener(ERROR, loadImage)
     }
-  }, [src])
+  }, [
+    src,
+    source,
+  ])
 
-  return source
+  if (!source) { return null }
+
+  return source.isLoaded ? source.src : null
 }
 
