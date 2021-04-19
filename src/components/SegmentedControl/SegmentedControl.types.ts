d997d0212f228c79993f44df1a734a559c6adb72
Junbong Lee <koliie9039@gmail.com>
2021-04-19 16:59:58 +0900

    피드백 반영 (mong)
    - 불필요한 extending UIComponentProps 제거

--- src/components/SegmentedControl/SegmentedControl.types.ts
+++ src/components/SegmentedControl/SegmentedControl.types.ts
@@ -1,15 +1,15 @@
 /* Internal dependencies */
-import { UIComponentProps, ChildrenComponentProps } from '../../types/ComponentProps'
+import { ChildrenComponentProps } from '../../types/ComponentProps'
 import OptionItemHost from '../../types/OptionItemHost'
 import { ExplicitDefaulting, BoxSizingUnit } from '../../types/CSS'
 
-export default interface SegmentedControlProps extends UIComponentProps, ChildrenComponentProps, OptionItemHost<number> {
+export default interface SegmentedControlProps extends ChildrenComponentProps, OptionItemHost<number> {
   width?: number | string | ExplicitDefaulting | BoxSizingUnit
   height?: number | string | ExplicitDefaulting | BoxSizingUnit
   disabled?: boolean
 }
 
-export interface SegmentedControlItemProps extends UIComponentProps, ChildrenComponentProps {
+export interface SegmentedControlItemProps extends ChildrenComponentProps {
   active?: boolean
   disabled?: boolean
 }
