e17400346f3fc7d35e35c1156b70d1415391a412
Mong <mong@channel.io>
2021-04-23 13:28:27 +0900

    Variant 확정하여 반영

--- src/components/TagBadge/TagBadgeCommon/constants/TagBadgeVariant.ts
+++ src/components/TagBadge/TagBadgeCommon/constants/TagBadgeVariant.ts
@@ -1,35 +1,55 @@
 /* Internal dependneices */
 import { SemanticNames } from '../../../../foundation/Colors/Theme'
 
-// FIXME: 임시 Variant입니다. 협의하여 결정 필요함
 enum TagBadgeVariant {
   Default = 'Default',
-  Important = 'Important',
-  Information = 'Information',
+  Deactivated = 'Deactivated',
+  Blue = 'Blue',
+  Cobalt = 'Cobalt',
   Teal = 'Teal',
-  Trial = 'Trial',
+  Green = 'Green',
   Olive = 'Olive',
   Pink = 'Pink',
+  Navy = 'Navy',
+  Yellow = 'Yellow',
+  Orange = 'Orange',
+  Red = 'Red',
+  Purple = 'Purple',
+  Grey = 'Grey',
 }
 
 export const TagBadgeBgColorPreset: { [T in TagBadgeVariant]: SemanticNames } = {
   [TagBadgeVariant.Default]: 'bg-black-lighter',
-  [TagBadgeVariant.Important]: 'bgtxt-blue-lighter',
-  [TagBadgeVariant.Information]: 'bgtxt-cobalt-lighter',
+  [TagBadgeVariant.Deactivated]: 'bg-black-lighter',
+  [TagBadgeVariant.Blue]: 'bgtxt-blue-lighter',
+  [TagBadgeVariant.Cobalt]: 'bgtxt-cobalt-lighter',
   [TagBadgeVariant.Teal]: 'bgtxt-teal-lighter',
-  [TagBadgeVariant.Trial]: 'bgtxt-green-lighter',
+  [TagBadgeVariant.Green]: 'bgtxt-green-lighter',
   [TagBadgeVariant.Olive]: 'bgtxt-olive-lighter',
   [TagBadgeVariant.Pink]: 'bgtxt-pink-lighter',
+  [TagBadgeVariant.Navy]: 'bgtxt-navy-lighter',
+  [TagBadgeVariant.Yellow]: 'bgtxt-yellow-lighter',
+  [TagBadgeVariant.Orange]: 'bgtxt-orange-lighter',
+  [TagBadgeVariant.Red]: 'bgtxt-red-lighter',
+  [TagBadgeVariant.Purple]: 'bgtxt-purple-lighter',
+  [TagBadgeVariant.Grey]: 'bg-black-darkest',
 }
 
 export const BadgeColorPreset: { [T in TagBadgeVariant]: SemanticNames } = {
-  [TagBadgeVariant.Default]: 'bg-black-darkest',
-  [TagBadgeVariant.Important]: 'bgtxt-blue-normal',
-  [TagBadgeVariant.Information]: 'bgtxt-cobalt-normal',
+  [TagBadgeVariant.Default]: 'txt-black-darkest',
+  [TagBadgeVariant.Deactivated]: 'txt-black-dark',
+  [TagBadgeVariant.Blue]: 'bgtxt-blue-normal',
+  [TagBadgeVariant.Cobalt]: 'bgtxt-cobalt-normal',
   [TagBadgeVariant.Teal]: 'bgtxt-teal-normal',
-  [TagBadgeVariant.Trial]: 'bgtxt-green-normal',
+  [TagBadgeVariant.Green]: 'bgtxt-green-normal',
   [TagBadgeVariant.Olive]: 'bgtxt-olive-normal',
   [TagBadgeVariant.Pink]: 'bgtxt-pink-normal',
+  [TagBadgeVariant.Navy]: 'bgtxt-navy-normal',
+  [TagBadgeVariant.Yellow]: 'bgtxt-yellow-normal',
+  [TagBadgeVariant.Orange]: 'bgtxt-orange-normal',
+  [TagBadgeVariant.Red]: 'bgtxt-red-normal',
+  [TagBadgeVariant.Purple]: 'bgtxt-purple-normal',
+  [TagBadgeVariant.Grey]: 'bgtxt-absolute-white-dark',
 }
 
 export default TagBadgeVariant
