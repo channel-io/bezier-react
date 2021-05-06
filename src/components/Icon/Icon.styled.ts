ef55f8da7c90acf0898f18d17c61f63198b41b9c
Ed Sungik Choi <sungik.dev@gmail.com>
2021-05-06 15:45:12 +0900

    Icon이 flex-item일 때 줄어들지 않도록 스타일링

--- src/components/Icon/Icon.styled.ts
+++ src/components/Icon/Icon.styled.ts
@@ -12,6 +12,7 @@ function getMargin({
 }
 
 const Icon = styled.svg<IconStyledProps>`
+  flex: 0 0 auto;
   margin: ${getMargin};
   color: ${({ foundation, color }) => (color && foundation?.theme?.[color]) || 'inherit'};
   ${({ foundation }) => foundation?.transition?.getTransitionsCSS('color')}
