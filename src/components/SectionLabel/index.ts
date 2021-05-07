bacac7363683b3f6f30f1ccd75eba939d07215e0
MongWook Song <dongwook627@gmail.com>
2021-05-07 17:40:33 +0900

    Export interpolation, SectionLabelItemProps (#370)
    * export InjectedInterpolation, WithInterpolation
    
    * export SectionLabelItemProps

--- src/components/SectionLabel/index.ts
+++ src/components/SectionLabel/index.ts
@@ -1,8 +1,11 @@
+/* eslint-disable import/no-duplicates */
 import SectionLabel from './SectionLabel'
+import type { SectionLabelItemProps } from './SectionLabel.types'
 import type SectionLabelProps from './SectionLabel.types'
 
 export type {
   SectionLabelProps,
+  SectionLabelItemProps,
 }
 
 export {
