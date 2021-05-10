2672f64b93a749a998a3b2e8d68798818c0eb3cb
MongWook Song <dongwook627@gmail.com>
2021-05-10 14:14:43 +0900

    KeyValueListItem 컴포넌트 작성 (#374)
    * KeyValueListItem 작성
    
    * 데스크에서의 타입을 디자인시스템에 맞추어 변경
    
    * isString을 isIconName으로 교체, TODO 주석 삭제
    
    * 마누엘 리뷰 반영
    
    Internal dependencies를 올바르게 표시
    
    * isIconName을 export
    
    * ValueComponent 오탈자 수정
    
    * 두기리뷰 반영
    
    - children prop삭제
    - MultilineValueComponent를 제거하고 ValueComponent를 사용하게 변경

---
+++ src/components/KeyValueListItem/KeyValueListItem.tsx
@@ -0,0 +1,143 @@
+/* External dependencies */
+import React, { forwardRef, Ref, useCallback, useMemo } from 'react'
+import _ from 'lodash'
+import { v4 as uuid } from 'uuid'
+
+/* Internal dependencies */
+import { Icon, IconSize } from '../Icon'
+import { Typography } from '../../foundation'
+import { isIconName } from '../Icon/util'
+import { KeyValueActionProps, KeyValueListItemProps } from './KeyValueListItem.types'
+import Styled from './KeyValueListItem.styled'
+
+export const KEY_VALUE_LIST_ITEM_TEST_ID = 'ch-design-system-key-value-list-item'
+
+function KeyValueListItem(
+  {
+    className,
+    interpolation,
+    valueWrapperInterpolation,
+    keyWrapperInterpolation,
+    keyIcon,
+    keyContent,
+    actions,
+    multiline = false,
+    testId = KEY_VALUE_LIST_ITEM_TEST_ID,
+    children,
+    ...props
+  }: KeyValueListItemProps,
+  forwardedRef: Ref<any>,
+) {
+  const renderAction = useCallback((action: KeyValueActionProps, key?: string) => {
+    if ('icon' in action) {
+      const iconElement = (
+        <Styled.ActionIconWrapper
+          key={key}
+          hoverBackgroundColor={action.hoverBackgroundColor ?? 'bg-black-lighter'}
+          hoverIconColor={action.hoverIconColor ?? 'txt-black-darkest'}
+          show={_.isBoolean(action.show) ? action.show : true}
+          onClick={action.onClick ?? _.noop}
+        >
+          <Styled.ActionIcon
+            name={action.icon}
+            color={action.iconColor ?? 'txt-black-dark'}
+            size={IconSize.XS}
+          />
+        </Styled.ActionIconWrapper>
+      )
+
+      if (!_.isEmpty(action.tooltip)) {
+        return (
+          <Styled.ActionIconTooltip
+            content={action.tooltip}
+          >
+            { iconElement }
+          </Styled.ActionIconTooltip>
+        )
+      }
+
+      return iconElement
+    }
+
+    return (
+      React.cloneElement(
+        action,
+        { key },
+      )
+    )
+  }, [])
+
+  const KeyIconComponent = useMemo(() => {
+    if (isIconName(keyIcon)) {
+      return (
+        <Icon
+          name={keyIcon}
+          size={IconSize.S}
+          color="txt-black-dark"
+        />
+      )
+    }
+
+    return keyIcon
+  }, [keyIcon])
+
+  const ActionsComponent = useMemo(() => {
+    if (_.isNil(actions) || _.isEmpty(actions)) {
+      return null
+    }
+
+    const item = _.isArray(actions)
+      ? actions.map((action) => renderAction(action, uuid()))
+      : renderAction(actions)
+
+    return item
+  }, [
+    actions,
+    renderAction,
+  ])
+
+  const ValueComponent = useMemo(() => (
+    <Styled.ValueWrapper
+      interpolation={valueWrapperInterpolation}
+    >
+      { children }
+    </Styled.ValueWrapper>
+  ), [
+    children,
+    valueWrapperInterpolation,
+  ])
+
+  return (
+    <Styled.Wrapper
+      ref={forwardedRef}
+      interpolation={interpolation}
+      className={className}
+      data-testid={testId}
+      // eslint-disable-next-line react/jsx-props-no-multi-spaces
+      {...props}
+    >
+      <Styled.Row>
+        { KeyIconComponent }
+        <Styled.KeyContentWrapper
+          interpolation={keyWrapperInterpolation}
+        >
+          <Styled.KeyText bold typo={Typography.Size12}>
+            { keyContent }
+          </Styled.KeyText>
+        </Styled.KeyContentWrapper>
+
+        { !multiline && ValueComponent }
+
+        { ActionsComponent }
+      </Styled.Row>
+
+      { multiline && (
+        <Styled.Row>
+          { ValueComponent }
+        </Styled.Row>
+      ) }
+    </Styled.Wrapper>
+  )
+}
+
+export default forwardRef(KeyValueListItem)
