51311355722f0ad438a3b60262cd4e6f6eb98e00
Mong <mong@channel.io>
2021-05-13 23:34:28 +0900

    Client의 불필요한 prop을 삭제

--- src/layout/Client/Client.types.ts
+++ src/layout/Client/Client.types.ts
@@ -1,7 +1,4 @@
 /* Internal dependencies */
 import { ChildrenComponentProps } from '../../types/ComponentProps'
-import { LayoutState } from '../redux/LayoutReducer'
 
-export default interface ClientProps extends ChildrenComponentProps {
-  layoutInitialState: LayoutState
-}
+export default interface ClientProps extends ChildrenComponentProps {}
