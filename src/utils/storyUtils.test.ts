d4f822b6160b1d01cc0f3874973f3990ed0640ed
Mong <mong@channel.io>
2021-05-19 19:04:37 +0900

    storyUtil 테스트코드 작성

---
+++ src/utils/storyUtils.test.ts
@@ -0,0 +1,47 @@
+import { isArray } from 'lodash'
+import { IconName } from '../components/Icon'
+import { TabsSize } from '../components/Tabs/Tabs.types'
+import {
+  getObjectFromEnum,
+  getTitle, iconList,
+} from './storyUtils'
+
+describe('storyUtils Test >', () => {
+  describe('getTitle Test >', () => {
+    it('정상 동작 테스트들', () => {
+      expect(getTitle('/src/layout/GlobalHeader/')).toBe('layout/GlobalHeader')
+      expect(getTitle('/foo/bar/ipsum/')).toBe('bar/ipsum')
+    })
+  })
+
+  describe('iconList Test >', () => {
+    it('iconList는 리스트이다', () => {
+      expect(isArray(iconList)).toBe(true)
+    })
+
+    it('iconList는 IconName type인 string을 가지고 있다', () => {
+      const sampleIconName: IconName = 'arrow-hook-left-down'
+      expect(iconList.includes(sampleIconName)).toBe(true)
+    })
+
+    it('iconList는 IconName이 아닌 것들은 가지고 있지 않다', () => {
+      // @ts-ignore
+      expect(iconList.includes(undefined)).toBe(false)
+      // @ts-ignore
+      expect(iconList.includes(null)).toBe(false)
+      // @ts-ignore
+      expect(iconList.includes('iphone')).toBe(false)
+    })
+  })
+
+  describe('getObejctFromEnum Test >', () => {
+    it('getObjectFromEnum 정상 동작', () => {
+      expect(getObjectFromEnum(TabsSize)).toMatchObject({ L: 53, Normal: 45, XS: 33 })
+    })
+
+    it('Empty Enum이 들어가면 {} 을 뱉는다', () => {
+      enum EmptyEnum {}
+      expect(getObjectFromEnum(EmptyEnum)).toMatchObject({})
+    })
+  })
+})
