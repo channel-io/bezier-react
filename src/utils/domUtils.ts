a51f92c2ec5dbf76411b2a75f7752d7ef1ecc56e
Jinwoo Jeong <thinkratomos@gmail.com>
2021-05-19 18:22:42 +0900

    domUtils 의 이미 해결된 TODO 주석 삭제

--- src/utils/domUtils.ts
+++ src/utils/domUtils.ts
@@ -8,7 +8,6 @@ export const document = getDocument()
 
 export const window = getWindow()
 
-// TODO: Test 가능하도록 형태 변경 필요, jest 타이밍에 module 해석 시에는 rootElement 가 항상 null 임.
 export const getRootElement = () =>
   document.getElementById!('main') ||
   document.getElementById!('root') ||
