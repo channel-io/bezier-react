f14b26826237ffc2a9de6e15b8083cdb7b3e7751
MongWook Song <dongwook627@gmail.com>
2021-05-21 18:57:42 +0900

    Tag Badge 에 HTMLAttr를  타입으로 추가 (#488)
    * tag, badge가 htmlattrs을 extends 하게 변경
    
    * 두기 코멘트 반영 - wrapperInterpolation, wrapperClassName => interpolation, className

--- src/components/TagBadge/Tag/Tag.types.ts
+++ src/components/TagBadge/Tag/Tag.types.ts
@@ -1,21 +1,17 @@
 /* Internal dependencies */
 import { SemanticNames } from '../../../foundation/Colors/Theme'
 import { ChildrenComponentProps } from '../../../types/ComponentProps'
-import InjectedInterpolation from '../../../types/InjectedInterpolation'
 import {
   TagBadgeSize,
   TagBadgeVariant,
 } from '../TagBadgeCommon'
 
-interface TagProps extends ChildrenComponentProps {
+interface TagProps extends ChildrenComponentProps, React.HTMLAttributes<HTMLDivElement> {
   size?: TagBadgeSize
   variant?: TagBadgeVariant
   color?: SemanticNames
   // Handlers
   onDelete?: (e: React.MouseEvent) => void
-  // Injected Styles
-  wrapperClassName?: string
-  wrapperInterpolation?: InjectedInterpolation
 }
 
 export default TagProps
