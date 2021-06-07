b5a890580d333b2c120dcc26f0e41f8222a0ad55
Ed Sungik Choi <sungik.dev@gmail.com>
2021-06-07 21:55:18 +0900

    fix(overlay): 오버레이가 뿌옇게 보이는 현상 수정
    translate 값을 정수로 반올림해서 sub-pixel이 안티앨리어싱되며 뿌옇게 되는 현상을 제거
    
    fix #523

--- src/components/Overlay/utils/positionUtils.ts
+++ src/components/Overlay/utils/positionUtils.ts
@@ -190,7 +190,7 @@ export function getOverlayStyle({
     return css`
       top: ${top}px;
       left: ${left}px;
-      transform: translateX(${translateX}px) translateY(${translateY}px);
+      transform: translateX(${Math.round(translateX)}px) translateY(${Math.round(translateY)}px);
     `
   }
   return css``
