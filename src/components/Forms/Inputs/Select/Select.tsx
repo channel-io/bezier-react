05b99f26eada5533c34834e23c8a60c1f9861369
Ed Sungik Choi <sungik.dev@gmail.com>
2021-12-15 22:35:20 +0900

    feat(select): select가 formControlContext를 전달받을 수 있도록 하고, 폼 인터페이스를 상속받도록 개선

--- src/components/Forms/Inputs/Select/Select.tsx
+++ src/components/Forms/Inputs/Select/Select.tsx
@@ -7,6 +7,8 @@ import { Typography } from 'Foundation'
 import { Icon, IconSize, isIconName } from 'Components/Icon'
 import { Text } from 'Components/Text'
 import { OverlayPosition } from 'Components/Overlay'
+import useFormControlContext from 'Components/Forms/useFormControlContext'
+
 import SelectProps, { SelectRef, SelectSize } from './Select.types'
 import * as Styled from './Select.styled'
 
@@ -18,39 +20,48 @@ export const SELECT_DROPDOWN_TEST_ID = 'bezier-react-select-dropdown'
 const DEFAULT_DROPDOWN_MARGIN_Y = 6
 const DEFAULT_DROPDOWN_Z_INDEX = 10
 
-function Select(
-  {
-    testId = SELECT_CONTAINER_TEST_ID,
-    triggerTestId = SELECT_TRIGGER_TEST_ID,
-    triggerTextTestId = SELECT_TRIGGER_TEXT_TEST_ID,
-    dropdownTestId = SELECT_DROPDOWN_TEST_ID,
-    as,
-    style,
-    className,
-    interpolation,
-    dropdownInterpolation,
-    size = SelectSize.M,
-    disabled = false,
-    defaultFocus = false,
-    placeholder = '',
-    iconComponent,
-    iconColor = 'txt-black-dark',
-    text,
-    textColor = 'txt-black-darkest',
-    withoutChevron = false,
-    chevronColor = 'txt-black-darker',
-    hasError = false,
-    dropdownContainer,
-    dropdownMarginX,
-    dropdownMarginY = DEFAULT_DROPDOWN_MARGIN_Y,
-    dropdownZIndex = DEFAULT_DROPDOWN_Z_INDEX,
-    dropdownPosition = OverlayPosition.BottomLeft,
-    onClickTrigger = noop,
-    onHideDropdown = noop,
-    children,
-  }: SelectProps,
-  forwardedRef: Ref<SelectRef>,
+function Select({
+  testId = SELECT_CONTAINER_TEST_ID,
+  triggerTestId = SELECT_TRIGGER_TEST_ID,
+  triggerTextTestId = SELECT_TRIGGER_TEXT_TEST_ID,
+  dropdownTestId = SELECT_DROPDOWN_TEST_ID,
+  as,
+  dropdownInterpolation,
+  size = SelectSize.M,
+  defaultFocus = false,
+  placeholder = '',
+  iconComponent,
+  iconColor = 'txt-black-dark',
+  text,
+  textColor = 'txt-black-darkest',
+  withoutChevron = false,
+  chevronColor = 'txt-black-darker',
+  dropdownStyle,
+  dropdownClassName,
+  dropdownContainer,
+  dropdownMarginX,
+  dropdownMarginY = DEFAULT_DROPDOWN_MARGIN_Y,
+  dropdownZIndex = DEFAULT_DROPDOWN_Z_INDEX,
+  dropdownPosition = OverlayPosition.BottomLeft,
+  onClickTrigger = noop,
+  onHideDropdown = noop,
+  children,
+  ...rest
+}: SelectProps,
+forwardedRef: Ref<SelectRef>,
 ) {
+  const contextValue = useFormControlContext()
+
+  const {
+    disabled = false,
+    hasError = false,
+    Wrapper,
+    ...ownProps
+  } = contextValue?.getFieldProps(rest) ?? {
+    ...rest,
+    Wrapper: React.Fragment,
+  }
+
   const containerRef = useRef<HTMLDivElement>(null)
   const triggerRef = useRef<HTMLDivElement>(null)
 
@@ -101,60 +112,63 @@ function Select(
 
   const hasContent = !isEmpty(text)
 
+  // TODO: 접근성을 지키도록 개선
   return (
-    <Styled.Container
-      data-testid={testId}
-      ref={containerRef}
-      style={style}
-      className={className}
-      interpolation={interpolation}
-    >
-      <Styled.Trigger
-        data-testid={triggerTestId}
-        as={as}
-        ref={triggerRef}
-        size={size}
-        focus={isDropdownOpened && !disabled}
-        error={hasError}
-        disabled={disabled}
-        onClick={handleClickTrigger}
+    <Wrapper>
+      <Styled.Container
+        data-testid={testId}
+        ref={containerRef}
+        {...ownProps}
       >
-        <Styled.MainContentWrapper>
-          { LeftComponent }
-          <Text
-            testId={triggerTextTestId}
-            typo={Typography.Size14}
-            color={hasContent ? textColor : 'txt-black-dark'}
-          >
-            { hasContent ? text : placeholder }
-          </Text>
-        </Styled.MainContentWrapper>
-        { !withoutChevron && (
+        <Styled.Trigger
+          data-testid={triggerTestId}
+          as={as}
+          ref={triggerRef}
+          size={size}
+          focus={isDropdownOpened && !disabled}
+          error={hasError}
+          disabled={disabled}
+          onClick={handleClickTrigger}
+        >
+          <Styled.MainContentWrapper>
+            { LeftComponent }
+            <Text
+              testId={triggerTextTestId}
+              typo={Typography.Size14}
+              color={hasContent ? textColor : 'txt-black-dark'}
+            >
+              { hasContent ? text : placeholder }
+            </Text>
+          </Styled.MainContentWrapper>
+          { !withoutChevron && (
           <Icon
             name={`chevron-${isDropdownOpened ? 'up' : 'down'}` as const}
             size={IconSize.XS}
             color={chevronColor}
             marginLeft={6}
           />
-        ) }
-      </Styled.Trigger>
+          ) }
+        </Styled.Trigger>
 
-      <Styled.Dropdown
-        testId={dropdownTestId}
-        withTransition
-        show={isDropdownOpened && !disabled}
-        zIndex={dropdownZIndex}
-        marginX={dropdownMarginX}
-        marginY={dropdownMarginY}
-        target={triggerRef.current}
-        container={dropdownContainer || containerRef.current}
-        position={dropdownPosition}
-        interpolation={dropdownInterpolation}
-        onHide={handleHideDropdown}
-      >
-        { children }
-      </Styled.Dropdown>
-    </Styled.Container>
+        <Styled.Dropdown
+          style={dropdownStyle}
+          className={dropdownClassName}
+          testId={dropdownTestId}
+          withTransition
+          show={isDropdownOpened && !disabled}
+          zIndex={dropdownZIndex}
+          marginX={dropdownMarginX}
+          marginY={dropdownMarginY}
+          target={triggerRef.current}
+          container={dropdownContainer || containerRef.current}
+          position={dropdownPosition}
+          interpolation={dropdownInterpolation}
+          onHide={handleHideDropdown}
+        >
+          { children }
+        </Styled.Dropdown>
+      </Styled.Container>
+    </Wrapper>
   )
 }
 
