85c76efbc2bf1f426e34aa343974b53abdfe436f
Ed Sungik Choi <sungik.dev@gmail.com>
2022-01-05 13:40:53 +0900

    feat(inputs): update inputs shadow color spec

--- src/components/Forms/Inputs/mixins.ts
+++ src/components/Forms/Inputs/mixins.ts
@@ -12,16 +12,16 @@ export const inputPlaceholderStyle = css`
 `
 
 export const inputWrapperStyle = css`
-  box-shadow: 0 1px 2px ${({ foundation }) => foundation?.theme?.['bg-black-lightest']},
-    inset 0 0 0 1px ${({ foundation }) => foundation?.theme?.['bg-black-lighter']};
+  box-shadow: 0 1px 2px ${({ foundation }) => foundation?.theme?.['bg-black-lighter']},
+    inset 0 0 0 1px ${({ foundation }) => foundation?.theme?.['bg-black-dark']};
 `
 
 export const focusedInputWrapperStyle = css`
-  box-shadow: 0 0 0 3px ${({ foundation }) => foundation?.theme?.['bgtxt-blue-lighter']},
+  box-shadow: 0 0 0 3px ${({ foundation }) => foundation?.theme?.['bgtxt-blue-light']},
     inset 0 0 0 1px ${({ foundation }) => foundation?.theme?.['bgtxt-blue-normal']};
 `
 
 export const erroredInputWrapperStyle = css`
-  box-shadow: 0 0 0 3px ${({ foundation }) => foundation?.theme?.['bgtxt-orange-lighter']},
+  box-shadow: 0 0 0 3px ${({ foundation }) => foundation?.theme?.['bgtxt-orange-light']},
     inset 0 0 0 1px ${({ foundation }) => foundation?.theme?.['bgtxt-orange-normal']};
 `
