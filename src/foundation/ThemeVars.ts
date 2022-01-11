f61be4fb599ff0f79513c38df3e89f6996bb1e82
Ed Sungik Choi <sungik.dev@gmail.com>
2022-01-11 21:13:35 +0900

    feat(theme-vars): add inverted theme css vars

--- src/foundation/ThemeVars.ts
+++ src/foundation/ThemeVars.ts
@@ -1,22 +1,25 @@
 /* External dependencies */
 import { createGlobalStyle } from 'styled-components'
+import { isEmpty } from 'lodash-es'
 
 /* Internal dependencies */
 import { Foundation } from './Foundation'
 
 type ThemeRecord = Record<string, string>
 
-function generateCSSVar(theme?: ThemeRecord) {
+function generateCSSVar(theme?: ThemeRecord, prefix?: string) {
   if (!theme) { return undefined }
+  const prefixString = !isEmpty(prefix) ? `${prefix}-` : ''
   return Object.entries(theme).reduce((varObj, [key, color]) => ({
     ...varObj,
-    [`--${key}`]: color,
+    [`--${prefixString}${key}`]: color,
   }), {} as ThemeRecord)
 }
 
 const ThemeVars = createGlobalStyle<{ foundation?: Foundation }>`
   :root {
     ${({ foundation }) => generateCSSVar(foundation?.theme)}
+    ${({ foundation }) => generateCSSVar(foundation?.subTheme, 'inverted')}
   }
 `
 
