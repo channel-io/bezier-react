87cdaffa0ee381c5120fc79829edf4ca1dbf9516
Ed Sungik Choi <sungik.dev@gmail.com>
2022-01-19 18:43:41 +0900

    test(form-group): add snapshot test

--- src/components/Forms/FormGroup/FormGroup.tsx
+++ src/components/Forms/FormGroup/FormGroup.tsx
@@ -6,7 +6,10 @@ import useFormControlContext from 'Components/Forms/useFormControlContext'
 import type FormGroupProps from './FormGroup.types'
 import * as Styled from './FormGroup.styled'
 
+export const FORM_GROUP_TEST_ID = 'bezier-react-form-group'
+
 function FormGroup({
+  testId = FORM_GROUP_TEST_ID,
   gap = 6,
   direction = 'column',
   children,
@@ -31,6 +34,7 @@ forwardedRef: React.Ref<HTMLDivElement>,
   return (
     <Styled.Stack
       {...ownProps}
+      data-testid={testId}
       ref={forwardedRef}
       gap={gap}
       direction={direction}
