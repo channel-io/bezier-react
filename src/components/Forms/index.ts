22bb48bbc23b57e63738f1738522776a32e4a512
Ed Sungik Choi <sungik.dev@gmail.com>
2022-01-26 13:34:44 +0900

    refactor: change the export directory from direct to the forms index file

---
+++ src/components/Forms/index.ts
@@ -0,0 +1,6 @@
+import type { FormComponentProps } from './Form.types'
+import useFormControlContext from './useFormControlContext'
+import useFormFieldProps from './useFormFieldProps'
+
+export type { FormComponentProps }
+export { useFormControlContext, useFormFieldProps }
