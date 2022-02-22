a666739040363a0929a7a78024b00d2e41679fc7
Ed Sungik Choi <sungik.dev@gmail.com>
2022-02-22 18:31:25 +0900

    build(svgr): update icon index template script
    Change the generated Icon component name, but make IconName the same.

--- scripts/icon-index-template.js
+++ scripts/icon-index-template.js
@@ -1,15 +1,32 @@
 const _ = require('lodash')
 const path = require('path')
 
+const ICON_POSTFIX = "Icon"
+const POSTFIX_REGEX = new RegExp(`(${ICON_POSTFIX})`, 'g')
+const POSTFIX_LENGTH = ICON_POSTFIX.length
+
+function hasValidFileName(basename) {
+  return basename.search(POSTFIX_REGEX) !== -1
+}
+
+function deleteIconPostfix(basename) {
+  return basename.slice(0, basename.length - POSTFIX_LENGTH)
+}
+
 function defaultIndexTemplate(filePaths) {
   const importEntries = []
   const mappedFies = []
 
   filePaths.forEach(filePath => {
     const basename = path.basename(filePath, path.extname(filePath))
+
+    if (!hasValidFileName(basename)) {
+      throw new Error('The svg icon file name must have a "-icon" suffix.')
+    }
+
     const exportName = /^\d/.test(basename) ? `Svg${basename}` : basename
     importEntries.push(`import ${exportName} from './${basename}'`)
-    mappedFies.push(`  '${_.kebabCase(basename)}': ${exportName},`)
+    mappedFies.push(`  '${_.kebabCase(deleteIconPostfix(basename))}': ${exportName},`)
   })
 
   const icons = `/* eslint-disable */
