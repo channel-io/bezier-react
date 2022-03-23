72a769c17c34ebaefb1ff104334653cf3b75256e
guswnsxodlf <guswnsxodlf@gmail.com>
2022-03-23 18:19:30 +0900

    fix: bezier provider always injects theme vars at root

--- src/providers/BezierProvider.tsx
+++ src/providers/BezierProvider.tsx
@@ -1,5 +1,6 @@
 /* External dependencies */
 import React from 'react'
+import type { AnyStyledComponent } from 'styled-components'
 
 /* Internal dependencies */
 import { Foundation, FoundationProvider, GlobalStyle, GlobalStyleProp, ThemeVars } from 'Foundation'
@@ -8,18 +9,23 @@ import EnableCSSHoudini from 'Worklets/EnableCSSHoudini'
 interface BezierProviderProps {
   foundation: Foundation & GlobalStyleProp
   children: React.ReactNode
+  themeVarsScope?: AnyStyledComponent
 }
 
 function BezierProvider({
   foundation,
   children,
+  themeVarsScope,
 }: BezierProviderProps) {
   EnableCSSHoudini({ smoothCorners: true })
 
   return (
     <FoundationProvider foundation={foundation}>
       <GlobalStyle foundation={foundation} />
-      <ThemeVars foundation={foundation} />
+      <ThemeVars
+        foundation={foundation}
+        themeVarsScope={themeVarsScope}
+      />
       { children }
     </FoundationProvider>
   )
