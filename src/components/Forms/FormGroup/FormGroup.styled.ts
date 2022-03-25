9c79cb5a24f6bbb4b5b9ca0248ce59f152d47fd4
Sungjun Lee <sungjunlee@npbgstrategy.com>
2022-03-25 17:34:23 +0900

    refactor(formgroup): formGroup 의 Stack 에도 gap 유틸함수 적용

--- src/components/Forms/FormGroup/FormGroup.styled.ts
+++ src/components/Forms/FormGroup/FormGroup.styled.ts
@@ -1,24 +1,7 @@
 /* Internal dependencies */
-import { styled, css } from 'Foundation'
-import { InjectedInterpolation } from 'Types/Foundation'
+import { styled } from 'Foundation'
+import { gap } from 'Utils/styleUtils'
 import type FormGroupProps from './FormGroup.types'
-
-// TODO: Mixin으로 옮기기보다, Stack 컴포넌트가 prop을 통해 이 믹스인을 내부적으로 사용하도록 하기
-function gap(spacing: number): InjectedInterpolation {
-  return css`
-    gap: ${spacing}px;
-
-    @supports not(gap: ${spacing}px) {
-      margin-top: ${-spacing}px;
-      margin-left: ${-spacing}px;
-
-      > * {
-        margin-top: ${spacing}px;
-        margin-left: ${spacing}px;
-      }
-    }
-  `
-}
 
 type StackProps = Required<Pick<FormGroupProps, 'gap' | 'direction' | 'interpolation'>>
 
