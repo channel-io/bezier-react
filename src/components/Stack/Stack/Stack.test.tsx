978d4522b321f9bc4a0dd32366cd161fd4f76e45
dugi <dugi@channel.io>
2022-04-28 13:56:44 +0900

    test(stack): add simple tests for Stack and StackItem

--- src/components/Stack/Stack/Stack.test.tsx
+++ src/components/Stack/Stack/Stack.test.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 
 /* Internal dependencies */
+import { css } from 'Foundation'
 import { render } from 'Utils/testUtils'
 import Stack from './Stack'
 
@@ -23,6 +24,32 @@ describe('Stack', () => {
       const { getByTestId } = render(<Stack direction="vertical" testId="stack" />)
 
       expect(getByTestId('stack')).toHaveStyle('flex-direction: column')
+    })
+  })
+
+  describe('Supports BezierComponentProps interface', () => {
+    it('supports as prop', () => {
+      const { getByTestId } = render(<Stack direction="horizontal" testId="stack" as="main" />)
+
+      expect(getByTestId('stack').tagName).toBe('MAIN')
+    })
+
+    it('supports style prop', () => {
+      const { getByTestId } = render(<Stack direction="horizontal" testId="stack" style={{ backgroundColor: 'red' }} />)
+
+      expect(getByTestId('stack')).toHaveStyle({ 'background-color': 'red' })
+    })
+
+    it('supports className prop', () => {
+      const { getByTestId } = render(<Stack direction="horizontal" testId="stack" className="foo" />)
+
+      expect(getByTestId('stack')).toHaveClass('foo')
+    })
+
+    it('supports interpolation prop', () => {
+      const { getByTestId } = render(<Stack direction="horizontal" testId="stack" interpolation={css`background-color: red;`} />)
+
+      expect(getByTestId('stack')).toHaveStyle({ 'background-color': 'red' })
     })
   })
 })
