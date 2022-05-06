50aef711f4fa9c202d180a759951130d65e4f2ae
Ed Sungik Choi <sungik.dev@gmail.com>
2022-05-06 12:41:31 +0900

    refactor(eslint): eslint 패키지를 제거하고, 루트 eslint로 통합

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,68 @@
 module.exports = {
   root: true,
-  // This tells ESLint to load the config from the package `eslint-config-bezier`
-  extends: ["bezier"],
+  extends: [
+    '@channel.io/eslint-config', 
+    'plugin:storybook/recommended'
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    'react/jsx-props-no-spreading': 'off',
+    'max-classes-per-file': 'off',
+    // FIXME: Delete the below rules after migration
+    'react/destructuring-assignment': 'off',
+    "react/jsx-key": 'off',
+    '@typescript-eslint/consistent-type-imports': 'off',
+  },
+  overrides: [
+    {
+      files: ["bezier-react/**/*"],
+      rules: {
+        'import/order': [
+          'error',
+          {
+            groups: ['external', 'builtin', 'internal', 'parent', 'sibling', 'index'],
+            pathGroups: [
+              {
+                pattern: 'Foundation',
+                group: 'internal',
+                position: 'before',
+              },
+              {
+                pattern: 'Foundation/**',
+                group: 'internal',
+                position: 'before',
+              },
+              {
+                pattern: 'Providers/**',
+                group: 'internal',
+                position: 'before',
+              },
+              {
+                pattern: 'Hooks/**',
+                group: 'internal',
+                position: 'before',
+              },
+              {
+                pattern: 'Worklets/**',
+                group: 'internal',
+                position: 'before',
+              },
+              {
+                pattern: '@(Constants|Types|Utils)/**',
+                group: 'internal',
+                position: 'before',
+              },
+              {
+                pattern: '@(Components|Layout)/**',
+                group: 'internal',
+                position: 'before',
+              },
+            ],
+          },
+        ],
+      },
+    },
+  ],
 };
