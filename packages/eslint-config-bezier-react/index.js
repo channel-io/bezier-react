5c5e774bb9387bc975fbb330db34ec72814dd033
Ed Sungik Choi <sungik.dev@gmail.com>
2022-05-14 17:26:38 +0900

    refactor(eslint-config): 불필요한 설정 제거, 설정 마이그레이션 관련 FIXME 주석 제거

--- packages/eslint-config-bezier-react/index.js
+++ packages/eslint-config-bezier-react/index.js
@@ -1,5 +1,4 @@
 module.exports = {
-  root: true,
   extends: [
     '@channel.io/eslint-config',
     'plugin:storybook/recommended',
@@ -8,9 +7,5 @@ module.exports = {
     'react/jsx-props-no-spreading': 'off',
     'max-classes-per-file': 'off',
     'no-restricted-imports': 'off',
-    // FIXME: Delete the below rules after migration
-    'react/destructuring-assignment': 'off',
-    'react/jsx-key': 'off',
-    '@typescript-eslint/consistent-type-imports': 'off',
   },
 }
