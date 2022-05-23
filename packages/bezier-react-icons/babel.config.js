7b19574a603042be4d93cc3fa576b52b60f9b721
Ed Sungik Choi <sungik.dev@gmail.com>
2022-05-23 16:08:22 +0900

    build(bezier-react-icons): duplicate bezier-react packages's build config

---
+++ packages/bezier-react-icons/babel.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    test: {
+      presets: [
+        ['@babel/preset-env', { targets: { node: 'current' } }],
+        '@babel/preset-react',
+        ['@babel/preset-typescript', { isTSX: true, allExtensions: true }],
+      ],
+      plugins: [
+        '@babel/plugin-transform-runtime',
+        ['@babel/plugin-proposal-private-property-in-object', { loose: false }],
+        ['@babel/plugin-proposal-class-properties', { loose: false }],
+      ],
+    },
+    build: {
+      presets: [
+        ['@babel/preset-env'],
+        '@babel/preset-react',
+        ['@babel/preset-typescript', { isTSX: true, allExtensions: true }],
+      ],
+      plugins: [
+        '@babel/plugin-transform-runtime',
+        ['@babel/plugin-proposal-private-property-in-object', { loose: false }],
+        ['@babel/plugin-proposal-class-properties', { loose: false }],
+      ],
+    },
+  },
+}
