8b0d45105dc9040c3f2eaf7763e23cf374b1b947
Hyunseung Jeon <dogdriip@gmail.com>
2022-05-27 20:14:52 +0900

    feat(progressbar): size, variant props 받아서 스타일 바뀌도록 styled.ts 작성

--- packages/bezier-react/src/components/ProgressBar/ProgressBar.styled.ts
+++ packages/bezier-react/src/components/ProgressBar/ProgressBar.styled.ts
@@ -1,8 +1,77 @@
 /* Internal dependencies */
-import { styled } from 'Foundation'
+import {
+  styled,
+  css,
+  type Foundation,
+} from 'Foundation'
+import type ProgressBarProps from './ProgressBar.types'
+import { ProgressBarSize, ProgressBarVariant } from './ProgressBar.types'
 
-export const StyledProgressBarWrapper = styled.div`
-  display: flex;
-  align-items: flex-start;
+const PROGRESS_BAR_BORDER_RADIUS = 3
+
+const PROGRESS_BAR_HEIGHT: Record<ProgressBarSize, number> = {
+  [ProgressBarSize.S]: 4,
+  [ProgressBarSize.M]: 6,
+}
+
+interface GetProgressBarActiveGradientProps {
+  foundation?: Foundation
+  variant: ProgressBarVariant
+}
+
+const getProgressBarActiveGradient = ({
+  foundation,
+  variant,
+}: GetProgressBarActiveGradientProps) => {
+  switch (variant) {
+    case ProgressBarVariant.Green: {
+      return css`
+        background: linear-gradient(
+          90deg,
+          ${foundation?.theme?.['bgtxt-green-normal']} 0%,
+          ${foundation?.subTheme?.['bgtxt-green-normal']} 100%
+        );
+      `
+    }
+    case ProgressBarVariant.Monochrome:
+    default: {
+      return css`
+        background: linear-gradient(
+          90deg,
+          ${foundation?.theme?.['bg-black-light']} 0%,
+          ${foundation?.theme?.['bg-black-dark']} 100%
+        );
+      `
+    }
+  }
+}
+
+interface StyledProgressBarWrapperProps extends ProgressBarProps {
+  size: NonNullable<ProgressBarProps['size']>
+}
+
+// FIXME: width
+export const StyledProgressBarWrapper = styled.div<StyledProgressBarWrapperProps>`
+  width: 50px;
+  height: ${({ size }) => PROGRESS_BAR_HEIGHT[size]}px;
+
   background-color: ${({ foundation }) => foundation?.theme?.['bg-black-light']};
+  border-radius: ${PROGRESS_BAR_BORDER_RADIUS}px;
+
+  ${({ interpolation }) => interpolation}
+`
+
+// FIXME: width
+interface StyledProgressBarActiveProps extends ProgressBarProps {
+  variant: NonNullable<ProgressBarProps['variant']>
+}
+
+export const StyledProgressBarActive = styled.div<StyledProgressBarActiveProps>`
+  width: 30px;
+  height: 100%;
+
+  ${getProgressBarActiveGradient}
+  border-radius: ${PROGRESS_BAR_BORDER_RADIUS}px;
+
+  ${({ interpolation }) => interpolation}
 `
--- packages/bezier-react/src/components/ProgressBar/ProgressBar.tsx
+++ packages/bezier-react/src/components/ProgressBar/ProgressBar.tsx
@@ -4,21 +4,22 @@ import React, { memo } from 'react'
 /* Internal dependencies */
 import type ProgressBarProps from './ProgressBar.types'
 import { ProgressBarSize, ProgressBarVariant } from './ProgressBar.types'
-import { StyledProgressBarWrapper } from './ProgressBar.styled'
+import { StyledProgressBarWrapper, StyledProgressBarActive } from './ProgressBar.styled'
 
 const PROGRESS_BAR_TEST_ID = 'bezier-react-progress-bar'
 
 function ProgressBar({
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   size = ProgressBarSize.M,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   variant = ProgressBarVariant.Green,
   testId = PROGRESS_BAR_TEST_ID,
 }: ProgressBarProps) {
   return (
     <StyledProgressBarWrapper
+      size={size}
       data-testid={testId}
-    />
+    >
+      <StyledProgressBarActive variant={variant} />
+    </StyledProgressBarWrapper>
   )
 }
 
