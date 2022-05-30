62f30949523ea3ae4aa75737bcdbf01bce31ec84
Lloyd Junbong Lee <koliie9039@gmail.com>
2022-05-30 19:49:33 +0900

    feat: add Crescent something to StatusType of Status component

--- packages/bezier-react/src/components/Status/Status.tsx
+++ packages/bezier-react/src/components/Status/Status.tsx
@@ -1,29 +1,49 @@
 /* External dependencies */
-import React from 'react'
+import React, { memo } from 'react'
 
-/* Internal denpendencies */
+/* Internal dependencies */
 import { IconSize } from 'Components/Icon'
-import { StatusType, StatusSize, StatusProps } from './Status.types'
-import { StatusCircle, LockIcon } from './Status.styled'
+import { StatusProps, StatusSize, StatusType } from './Status.types'
+import { LockIcon, MoonFilledIcon, StatusCircle } from './Status.styled'
 
 // TODO: 테스트 코드 작성
 const STATUS_TEST_ID = 'bezier-react-status'
+
+const statusWithIcon: Readonly<StatusType[]> = [
+  StatusType.OnlineCrescent,
+  StatusType.OfflineCrescent,
+  StatusType.Lock,
+]
 
 function Status({
   type,
   size = StatusSize.M,
 }: StatusProps) {
-  if (type === StatusType.Lock) {
+  if (statusWithIcon.includes(type)) {
+    const iconSize = (size <= StatusSize.M) ? IconSize.XXS : IconSize.XS
+
     return (
       <StatusCircle
         data-testid={STATUS_TEST_ID}
         color="bg-white-normal"
         size={size}
       >
-        <LockIcon
-          size={IconSize.XXS}
-          color="txt-black-darker"
-        />
+        { (() => {
+          if (type === StatusType.Lock) {
+            return (
+              <LockIcon
+                size={iconSize}
+                color="txt-black-darker"
+              />
+            )
+          }
+          return (
+            <MoonFilledIcon
+              size={iconSize}
+              color={type === StatusType.OnlineCrescent ? 'bgtxt-green-normal' : 'bgtxt-orange-normal'}
+            />
+          )
+        })() }
       </StatusCircle>
     )
   }
@@ -37,4 +57,4 @@ function Status({
   )
 }
 
-export default Status
+export default memo(Status)
