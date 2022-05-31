62968e559d328cf6d027541c8688554504c4cd58
dugi <dugi@channel.io>
2022-05-31 15:57:58 +0900

    docs(button): add jsdoc for ButtonProps interface

--- packages/bezier-react/src/components/Button/Button.types.ts
+++ packages/bezier-react/src/components/Button/Button.types.ts
@@ -38,15 +38,71 @@ export type SideContent = IconName | React.ReactNode
 type MouseEventHandler = React.MouseEventHandler
 
 interface ButtonOptions {
+  /**
+   * `type` attribute of typical HTML button.
+   *
+   * You may want to set `type` to `submit` to the button
+   * which is used as a submit button in `<form>` component.
+   *
+   * @default 'button'
+   */
   type?: HTMLButtonElement['type']
+
+  /**
+   * The text content in the button.
+   *
+   * Do not pass `text` prop if it is an icon-only button.
+   */
   text?: string
+
+  /**
+   * If `loading` is true, spinner will be shown, replacing the content.
+   *
+   * @default false
+   */
   loading?: boolean
+
+  /**
+   * If `active` is true, the button will be styled as if it is hovered.
+   *
+   * You may want to use this prop for a button which opens dropdown, etc.
+   *
+   * @default false
+   */
   active?: boolean
+
+  /**
+   * The style variant.
+   *
+   * @default ButtonStyleVariant.Primary
+   */
   styleVariant?: ButtonStyleVariant
+
+  /**
+   * The color variant.
+   *
+   * @default ButtonColorVariant.Blue
+   */
   colorVariant?: ButtonColorVariant
+
+  /**
+   * The handler to be executed when the button is clicked.
+   */
   onClick?: MouseEventHandler
+
+  /**
+   * The handler to be executed when the mouse enters the button.
+   */
   onMouseEnter?: MouseEventHandler
+
+  /**
+   * The handler to be executed when the mouse leaves the button.
+   */
   onMouseLeave?: MouseEventHandler
+
+  /**
+   * The handler to be executed when the button is unfocused.
+   */
   onBlur?: MouseEventHandler
 }
 
