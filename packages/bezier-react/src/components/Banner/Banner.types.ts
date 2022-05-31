5ab2c429242dc2ee5f381eb1cd58fd9ae6c7244d
dugi <dugi@channel.io>
2022-05-31 11:59:51 +0900

    docs(banner): add jsdoc for BannerProps interface

--- packages/bezier-react/src/components/Banner/Banner.types.ts
+++ packages/bezier-react/src/components/Banner/Banner.types.ts
@@ -22,17 +22,59 @@ export type RenderLinkFunc = (props: {
 }) => JSX.Element
 
 interface BannerOptions {
+  /**
+   * The name of icon to display at the top left of the banner.
+   *
+   * If `null` is given, no icon will be displayed.
+   */
   icon: IconName | null
 
+  /**
+   * Whether to display link at the end of banner content.
+   *
+   * @default false
+   */
   hasLink?: boolean
+
+  /**
+   * The link content.
+   *
+   * This will be displayed as bold, underline text at the end of content.
+   *
+   * @remarks
+   * `hasLink` props should be given `true` to enable the link.
+   */
   linkText?: string
+
+  /**
+   * The location (href) of the link.
+   *
+   * By default, the link will be opened in a new tab. (`target="_blank"`)
+   * To specify a different behavior, use `renderLink` prop to render the link as a custom component.
+   *
+   * @remarks
+   * `hasLink` props should be given `true` to enable the link.
+   */
   linkTo?: string
+
+  /**
+   * Specifies how to render the link.
+   *
+   * @default
+   * renders link as an `<a>` tag with `target="_blank"` attribute.
+   */
   renderLink?: RenderLinkFunc
 
   /**
+   * Specifies which icon button to display at the top right of the banner.
+   *
    * FIXME(@ed): 새로운 아이콘 방식으로 변경
    */
   actionIcon?: IconName
+
+  /**
+   * Handler to be executed when the action icon button is clicked.
+   */
   onClickAction?: ButtonProps['onClick']
 }
 
