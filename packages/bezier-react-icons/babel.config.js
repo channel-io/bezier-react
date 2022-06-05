4924ff5b26b48bc54d18f975f164913338adc194
Ed Sungik Choi <sungik.dev@gmail.com>
2022-06-05 16:08:41 +0900

    refactor(bezier-react-icons): build config and rename build-icon script

--- packages/bezier-react-icons/babel.config.js
+++ packages/bezier-react-icons/babel.config.js
@@ -1,28 +1,10 @@
 module.exports = {
-  env: {
-    test: {
-      presets: [
-        ['@babel/preset-env', { targets: { node: 'current' } }],
-        '@babel/preset-react',
-        ['@babel/preset-typescript', { isTSX: true, allExtensions: true }],
-      ],
-      plugins: [
-        '@babel/plugin-transform-runtime',
-        ['@babel/plugin-proposal-private-property-in-object', { loose: false }],
-        ['@babel/plugin-proposal-class-properties', { loose: false }],
-      ],
-    },
-    build: {
-      presets: [
-        ['@babel/preset-env'],
-        '@babel/preset-react',
-        ['@babel/preset-typescript', { isTSX: true, allExtensions: true }],
-      ],
-      plugins: [
-        '@babel/plugin-transform-runtime',
-        ['@babel/plugin-proposal-private-property-in-object', { loose: false }],
-        ['@babel/plugin-proposal-class-properties', { loose: false }],
-      ],
-    },
-  },
+  presets: [
+    ['@babel/preset-env'],
+    '@babel/preset-react',
+    ['@babel/preset-typescript', { isTSX: true, allExtensions: true }],
+  ],
+  plugins: [
+    '@babel/plugin-transform-runtime',
+  ],
 }
--- packages/bezier-react-icons/rollup.config.js
+++ packages/bezier-react-icons/rollup.config.js
@@ -6,46 +6,14 @@ import commonjs from '@rollup/plugin-commonjs'
 import babel from '@rollup/plugin-babel'
 import { visualizer } from 'rollup-plugin-visualizer'
 import typescript from 'rollup-plugin-typescript2'
-import createStyledComponentsTransformer from 'typescript-plugin-styled-components'
 
 import packageJson from './package.json'
 
 const extensions = DEFAULT_EXTENSIONS.concat(['.ts', '.tsx'])
 
-const styledComponentsTransformer = createStyledComponentsTransformer({
-  displayName: true,
-})
-
-// Order Matters, must after rollup-plugin-node-resolve
-// See Also: https://www.npmjs.com/package/rollup-plugin-typescript2
 const typescriptPlugin = typescript({
-  typescript: require('ttypescript'),
   tsconfig: './tsconfig.json',
   useTsconfigDeclarationDir: true,
-  transformers: [
-    () => ({
-      before: [styledComponentsTransformer],
-    }),
-  ],
-  tsconfigDefaults: {
-    noEmit: false,
-    emitDeclarationOnly: true,
-    compilerOptions: {
-      plugins: [
-        { transform: 'typescript-transform-paths' },
-        { transform: 'typescript-transform-paths', afterDeclarations: true },
-      ],
-    },
-    exclude: [
-      './src/layout/stories/**/*',
-      '**/__mocks__/*',
-      '**/*.stories.tsx',
-      '**/*.test.ts',
-      '**/*.test.tsx',
-      './src/utils/storyUtils.ts',
-      './src/utils/testUtils.tsx',
-    ],
-  },
 })
 
 const commonPlugins = [
