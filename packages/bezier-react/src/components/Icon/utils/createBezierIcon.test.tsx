b0d62532bc7354036aad7ec56cc65a93d057be6a
Ed Sungik Choi <sungik.dev@gmail.com>
2022-06-10 16:46:50 +0900

    test(icons): add the util func test

---
+++ packages/bezier-react/src/components/Icon/utils/createBezierIcon.test.tsx
@@ -0,0 +1,26 @@
+/* External dependencies */
+import React from 'react'
+
+/* Internal dependencies */
+// eslint-disable-next-line no-restricted-imports
+import { AllIcon } from '../generated'
+import { createBezierIcon, isBezierIcon } from './createBezierIcon'
+
+describe('createBezierIcon utils >', () => {
+  it('isBezierIcon returns true for BezierIcon', () => {
+    expect(isBezierIcon(AllIcon)).toBe(true)
+  })
+
+  it('isBezierIcon returns false for non BezierIcon', () => {
+    const source = () => (<svg />)
+
+    expect(isBezierIcon(source)).toBe(false)
+  })
+
+  it('createBezierIcon returns a BezierIcon', () => {
+    const source = () => (<svg />)
+    const mockBezierIcon = createBezierIcon(source)
+
+    expect(isBezierIcon(mockBezierIcon)).toBe(true)
+  })
+})
