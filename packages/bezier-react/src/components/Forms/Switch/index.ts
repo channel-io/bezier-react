b2c4b163c79eaa33fca8cd60efcb84fc9a8a6898
Hyunseung Jeon <dogdriip@gmail.com>
2022-07-04 15:48:59 +0900

    fix(switch/index): enum SwitchSize 값을 export하도록 수정 (feedback @dugi)

--- packages/bezier-react/src/components/Forms/Switch/index.ts
+++ packages/bezier-react/src/components/Forms/Switch/index.ts
@@ -1,9 +1,9 @@
 import Switch from './Switch'
 import type SwitchProps from './Switch.types'
-import type { SwitchSize } from './Switch.types'
+import { SwitchSize } from './Switch.types'
 
 export {
   Switch,
   type SwitchProps,
-  type SwitchSize,
+  SwitchSize,
 }
