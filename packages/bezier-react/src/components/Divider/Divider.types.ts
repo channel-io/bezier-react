0a1eb01ac13d69d217f788747ec1e5a26d9490c9
Hyunseung Jeon <dogdriip@gmail.com>
2022-07-14 14:00:47 +0900

    docs(divider): add jsdoc for DividerOptions

--- packages/bezier-react/src/components/Divider/Divider.types.ts
+++ packages/bezier-react/src/components/Divider/Divider.types.ts
@@ -5,7 +5,22 @@ import type { SeparatorProps as SeparatorPrimitiveProps } from '@radix-ui/react-
 import type { BezierComponentProps } from 'Types/ComponentProps'
 
 interface DividerOptions {
+  /**
+   * If true, the divider will be rendered without side padding,
+   * which stands for the padding on the left and right sides of the horizontal divider,
+   * and the top and bottom sides of the vertical divider.
+   *
+   * @default false
+   */
   withoutSideIndent?: boolean
+
+  /**
+   * If true, the divider will be rendered without parallel padding,
+   * which stands for the padding on the top and bottom sides of the horizontal divider,
+   * and the left and right sides of the vertical divider.
+   *
+   * @default false
+   */
   withoutParallelIndent?: boolean
 }
 
