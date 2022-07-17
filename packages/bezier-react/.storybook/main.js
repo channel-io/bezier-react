ba2859839e6feee5cce135101fb6b47d4db77b95
dugi <dugi@channel.io>
2022-07-17 23:24:59 +0900

    chore(bezier-react/storybook): remove style-loader, css-loader, scss-loader
    scss is not used in this project

--- packages/bezier-react/.storybook/main.js
+++ packages/bezier-react/.storybook/main.js
@@ -28,12 +28,6 @@ module.exports = {
     ]
 
     config.module.rules.push({
-      test: /\.scss$/,
-      use: ['style-loader', 'css-loader', 'sass-loader'],
-      include: path.resolve(__dirname, '../'),
-    })
-
-    config.module.rules.push({
       test: /\.(ts|tsx)$/,
       loader: require.resolve('babel-loader'),
       options: {
