feff06ec748ad682e49f4e76e0b97bbd483facf6
Dugi (Dongwook Hwang) <dugi@channel.io>
2022-09-05 17:18:44 +0900

    Write mdx documentation for Typography (#910)
    * docs(@channel.io/bezier-react): write mdx documentation for Foundation/Typography
    
    * docs(@channel.io/bezier-react): add Typography.Size17 to documentation
    
    * docs(@channel.io/bezier-react): apply themed text color to story (andrew)
    
    * docs(@channel.io/bezier-react): add guide for interpolation usecase of Typography (ed)
    
    * doc(@channel.io/bezier-react): update suggestion style (aru)
    
    Co-authored-by: Aru Hyunseung Jeon <dogdriip@gmail.com>
    
    * doc(@channel.io/bezier-react): specify physical unit on global control section (aru)
    
    Co-authored-by: Aru Hyunseung Jeon <dogdriip@gmail.com>
    
    Co-authored-by: Aru Hyunseung Jeon <dogdriip@gmail.com>

---
+++ packages/bezier-react/src/foundation/Typography.stories.tsx
@@ -0,0 +1,171 @@
+/* External dependencies */
+import React from 'react'
+import {
+  type Meta,
+  type Story,
+} from '@storybook/react'
+
+/* Internal dependencies */
+import {
+  styled,
+} from 'Foundation'
+import {
+  StackItem,
+  VStack,
+} from 'Components/Stack'
+import {
+  Text,
+} from 'Components/Text'
+import {
+  Typography,
+} from './Typography'
+import mdx from './Typography.mdx'
+
+export default {
+  title: 'Foundation/Typography',
+  parameters: {
+    docs: {
+      page: mdx,
+    },
+  },
+  argTypes: {
+    typography: {
+      control: {
+        type: 'radio',
+        options: Object.keys(Typography),
+      },
+    },
+  },
+} as Meta
+
+const Span = styled.span<{ typography: keyof typeof Typography }>`
+  ${({ typography }) => Typography[typography]}
+`
+
+export const Primary: Story<{
+  typography: keyof typeof Typography
+}> = ({ typography }) => (
+  <Span typography={typography}>
+    Channel.io, an all-in-one business solution.
+  </Span>
+)
+
+Primary.args = {
+  typography: 'Size13',
+}
+
+export const Overview: Story<{}> = () => (
+  <VStack spacing={16}>
+    <StackItem>
+      <Text
+        typo={Typography.Size11}
+        color="txt-black-darkest"
+      >
+        11Aa한글 <b>11Aa한글</b>
+      </Text>
+    </StackItem>
+    <StackItem>
+      <Text
+        typo={Typography.Size12}
+        color="txt-black-darkest"
+      >
+        12Aa한글 <b>12Aa한글</b>
+      </Text>
+    </StackItem>
+    <StackItem>
+      <Text
+        typo={Typography.Size13}
+        color="txt-black-darkest"
+      >
+        13Aa한글 <b>13Aa한글</b>
+      </Text>
+    </StackItem>
+    <StackItem>
+      <Text
+        typo={Typography.Size14}
+        color="txt-black-darkest"
+      >
+        14Aa한글 <b>14Aa한글</b>
+      </Text>
+    </StackItem>
+    <StackItem>
+      <Text
+        typo={Typography.Size15}
+        color="txt-black-darkest"
+      >
+        15Aa한글 <b>15Aa한글</b>
+      </Text>
+    </StackItem>
+    <StackItem>
+      <Text
+        typo={Typography.Size16}
+        color="txt-black-darkest"
+      >
+        16Aa한글 <b>16Aa한글</b>
+      </Text>
+    </StackItem>
+    <StackItem>
+      <Text
+        typo={Typography.Size17}
+        color="txt-black-darkest"
+      >
+        17Aa한글 <b>17Aa한글</b>
+      </Text>
+    </StackItem>
+    <StackItem>
+      <Text
+        typo={Typography.Size18}
+        color="txt-black-darkest"
+      >
+        18Aa한글 <b>18Aa한글</b>
+      </Text>
+    </StackItem>
+    <StackItem>
+      <Text
+        typo={Typography.Size22}
+        color="txt-black-darkest"
+      >
+        22Aa한글 <b>22Aa한글</b>
+      </Text>
+    </StackItem>
+    <StackItem>
+      <Text
+        typo={Typography.Size24}
+        color="txt-black-darkest"
+      >
+        24Aa한글 <b>24Aa한글</b>
+      </Text>
+    </StackItem>
+  </VStack>
+)
+
+/* eslint-disable max-len */
+export const UsageText: Story<{}> = () => (
+  <Text
+    as="p"
+    typo={Typography.Size15}
+    color="txt-black-darkest"
+  >
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent bibendum elit erat, eu euismod odio tincidunt vel. Nulla non consequat ligula. Nulla quis justo at enim sodales aliquet. Proin placerat varius elit, et molestie erat ornare ut. Ut pharetra ultrices lacus. Morbi vehicula auctor lectus ac convallis. Etiam scelerisque feugiat lectus, nec gravida enim mollis eu. Cras ut urna imperdiet, blandit nisi nec, auctor ipsum. Aliquam eu nisl tellus. Phasellus non porta dui. Pellentesque fringilla enim ut volutpat commodo. In hac habitasse platea dictumst.
+  </Text>
+)
+/* eslint-enable max-len */
+
+UsageText.storyName = 'Usage (via Text)'
+
+const TypographicCode = styled.code`
+  ${Typography.Size15}
+  color: ${({ foundation }) => foundation?.theme['txt-black-darkest']};
+`
+
+export const UsageInterpolation: Story<{}> = () => (
+  <pre>
+    <TypographicCode>
+      { `function main() {
+  console.log('hello, world!');
+}` }
+    </TypographicCode>
+  </pre>
+)
+
+UsageInterpolation.storyName = 'Usage (via interpolation)'
