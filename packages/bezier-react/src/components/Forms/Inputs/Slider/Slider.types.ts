0ddcc1978785f2657a1870ea5c9bb5a24b9ca11a
Hyunseung Jeon <dogdriip@gmail.com>
2022-09-15 20:28:29 +0900

    docs(silder.types): add jsdoc for SliderOptions

--- packages/bezier-react/src/components/Forms/Inputs/Slider/Slider.types.ts
+++ packages/bezier-react/src/components/Forms/Inputs/Slider/Slider.types.ts
@@ -5,9 +5,25 @@ import type { SliderProps as SliderPrimitiveProps } from '@radix-ui/react-slider
 import type { BezierComponentProps } from 'Types/ComponentProps'
 
 interface SliderOptions {
-  // TODO (@aru): add jsdoc
+  /**
+   * CSS Width of entier slider.
+   * If given value is number or doesn't end with proper unit, `defaultUnit` is suffixed to given value.
+   *
+   * @see src/utils/styleUtils.ts
+   * @default 36
+   */
   width?: number | string
+
+  /**
+   * When array of number `guide` is provided, guide mark will be shown above track regard to given value(s).
+   */
   guide?: number[]
+
+  /**
+   * If `showTooltip` is true, the tooltip with current value will be shown above thumb when dragging.
+   *
+   * @default true
+   */
   showTooltip?: boolean
 }
 
