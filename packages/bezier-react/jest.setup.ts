559b45a5e7c53b7cd083d19f6627d4f66fcf786f
Hyunseung Jeon <dogdriip@gmail.com>
2022-09-16 20:46:02 +0900

    test(jest.setup): add ResizeObserver class mock

--- packages/bezier-react/jest.setup.ts
+++ packages/bezier-react/jest.setup.ts
@@ -1,6 +1,22 @@
 import '@testing-library/jest-dom'
 import 'jest-styled-components'
 
+global.ResizeObserver = class ResizeObserver {
+  cb: any
+
+  constructor(cb: any) {
+    this.cb = cb
+  }
+
+  observe() {
+    this.cb([{ borderBoxSize: { inlineSize: 0, blockSize: 0 } }])
+  }
+
+  unobserve() {}
+
+  disconnect() {}
+}
+
 beforeEach(() => {
   // @ts-ignore
   jest.spyOn(window, 'requestAnimationFrame').mockImplementation(cb => cb())
