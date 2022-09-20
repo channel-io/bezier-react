82c8d17b589bc1ed308da847f8c1a5f59e62ab1d
Aru Hyunseung Jeon <dogdriip@gmail.com>
2022-09-20 13:08:46 +0900

    Bump @radix-ui/react-separator to 1.0.0, Add ResizeObserver jest mock (#928)
    * chore(deps): bump @radix-ui/react-separator to 1.0.0
    
    * test(jest.setup): add ResizeObserver class mock
    
    * docs(jest.setup): add original source link of ResizeObserver mock
    
    * chore(changesets): add changeset

--- packages/bezier-react/jest.setup.ts
+++ packages/bezier-react/jest.setup.ts
@@ -1,6 +1,25 @@
 import '@testing-library/jest-dom'
 import 'jest-styled-components'
 
+/**
+ * @see https://github.com/radix-ui/primitives/blob/83a8c13bf66f3d9f17d77caeb187a69eb146930b/scripts/setup-tests.ts
+ */
+global.ResizeObserver = class ResizeObserver {
+  cb: any
+
+  constructor(cb: any) {
+    this.cb = cb
+  }
+
+  observe() {
+    this.cb([{ borderBoxSize: { inlineSize: 0, blockSize: 0 } }])
+  }
+
+  unobserve() {}
+
+  disconnect() {}
+}
+
 beforeEach(() => {
   // @ts-ignore
   jest.spyOn(window, 'requestAnimationFrame').mockImplementation(cb => cb())
