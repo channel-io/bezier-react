a1adf5a83e7f10b80f6070a0ba21a23fc0e2a6d0
dinohan <dinohan.dev@gmail.com>
2022-10-12 11:48:20 +0900

    fix(form-label-help): remove unnecessary type, fix interface name

--- packages/bezier-react/src/components/Forms/FormLabelHelp/FormLabelHelp.types.ts
+++ packages/bezier-react/src/components/Forms/FormLabelHelp/FormLabelHelp.types.ts
@@ -2,12 +2,7 @@
 import { BezierComponentProps, ChildrenProps } from 'Types/ComponentProps'
 import { TooltipProps } from 'Components/Tooltip'
 
-interface FormLabelOptions {
-  htmlFor?: string
-}
-
-export default interface FormLabelProps extends
+export default interface FormLabelHelpProps extends
   BezierComponentProps,
   ChildrenProps,
-  Omit<TooltipProps, 'content'>,
-  FormLabelOptions {}
+  Omit<TooltipProps, 'content'> {}
