47da720ae4af467c826ee8a1c381ffcbfd46e959
Hyunseung Jeon <dogdriip@gmail.com>
2022-11-10 16:24:05 +0900

    feat(switch): apply @radix-ui/react-switch primitives

--- packages/bezier-react/src/components/Forms/Switch/Switch.tsx
+++ packages/bezier-react/src/components/Forms/Switch/Switch.tsx
@@ -1,11 +1,10 @@
 /* External dependencies */
 import React, {
   forwardRef,
-  useCallback,
   type ReactElement,
-  type MouseEvent,
   type Ref,
 } from 'react'
+import * as SwitchPrimitive from '@radix-ui/react-switch'
 
 /* Internal dependencies */
 import useFormFieldProps from 'Components/Forms/useFormFieldProps'
@@ -21,43 +20,44 @@ function Switch(
     testId = SWITCH_TEST_ID,
     handleTestId = SWITCH_HANDLE_TEST_ID,
     checked = false,
+    onCheckedChange,
     size = SwitchSize.M,
-    onClick,
     ...rest
   }: SwitchProps,
   forwardedRef: Ref<HTMLDivElement>,
 ): ReactElement {
   const {
     disabled,
+    required,
     ...ownProps
   } = useFormFieldProps(rest)
 
-  const handleClick = useCallback((event: MouseEvent) => {
-    if (!disabled && onClick) {
-      onClick(!checked, event)
-    }
-  }, [
-    onClick,
-    checked,
-    disabled,
-  ])
-
   return (
-    <Styled.Wrapper
-      {...ownProps}
-      ref={forwardedRef}
-      size={size}
+    <SwitchPrimitive.Root
+      asChild
       checked={checked}
+      onCheckedChange={onCheckedChange}
       disabled={disabled}
-      onClick={handleClick}
-      data-testid={testId}
+      required={required}
+      {...rest}
     >
-      <Styled.Content
+      <Styled.Wrapper
+        {...ownProps}
+        ref={forwardedRef}
         size={size}
         checked={checked}
-        data-testid={handleTestId}
-      />
-    </Styled.Wrapper>
+        disabled={disabled}
+        data-testid={testId}
+      >
+        <SwitchPrimitive.Thumb asChild>
+          <Styled.Content
+            size={size}
+            checked={checked}
+            data-testid={handleTestId}
+          />
+        </SwitchPrimitive.Thumb>
+      </Styled.Wrapper>
+    </SwitchPrimitive.Root>
   )
 }
 
