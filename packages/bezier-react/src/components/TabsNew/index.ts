d3373d3e003010fcbc4224f63bb659b7920e0350
yangwooseong <andrew@channel.io>
2022-12-05 15:56:33 +0900

    chore(tabs): add index.ts to resolve typecheck failed

---
+++ packages/bezier-react/src/components/TabsNew/index.ts
@@ -0,0 +1,17 @@
+import { Tabs } from './Tabs'
+import { TabsSize } from './Tabs.types'
+import type {
+  TabsProps,
+  TabItemProps,
+  TabsListProps,
+  TabActionProps,
+} from './Tabs.types'
+
+export {
+  Tabs as TabsNew,
+  TabsSize as TabsNewSize,
+  TabsProps as TabsNewProps,
+  TabItemProps as TabItemNewProps,
+  TabsListProps as TabsListNewProps,
+  TabActionProps as TabActionNewProps,
+}
