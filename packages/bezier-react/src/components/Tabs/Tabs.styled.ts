06e911bcdb394a734053433dbd9ac862be1ced86
yangwooseong <andrew@channel.io>
2022-12-09 15:33:49 +0900

    refactor(tabs): apply style to TabsPrimitive.Root directly

--- packages/bezier-react/src/components/Tabs/Tabs.styled.ts
+++ packages/bezier-react/src/components/Tabs/Tabs.styled.ts
@@ -1,6 +1,10 @@
+/* External dependencies */
+import * as TabsPrimitive from '@radix-ui/react-tabs'
+
+/* Internal dependencies */
 import { styled } from 'Foundation'
 
-export const Tabs = styled.div`
+export const Tabs = styled(TabsPrimitive.Root)`
   display: flex;
   flex-direction: column;
   width: 100%;
--- packages/bezier-react/src/components/Tabs/Tabs.tsx
+++ packages/bezier-react/src/components/Tabs/Tabs.tsx
@@ -1,7 +1,5 @@
 /* External dependencies */
 import React from 'react'
-import * as TabsPrimitive from '@radix-ui/react-tabs'
-import { noop } from 'lodash-es'
 
 /* Internal dependencies */
 import { TabsProps } from 'Components/Tabs/Tabs.types'
@@ -9,10 +7,6 @@ import * as Styled from './Tabs.styled'
 
 /**
  * `Tabs` is a set of layered section of content
- *
- * `Tabs` is a context of the Tabs-related components.
- * It does not render any DOM node, but it provides handlers and accessibility property to
- * its Tabs-related children components.
  *
  * @example
  *
@@ -29,21 +23,12 @@ import * as Styled from './Tabs.styled'
 
 export function Tabs({
   children,
-  defaultValue,
-  value,
-  onValueChange = noop,
+  ...rest
 }: TabsProps) {
   return (
-    <TabsPrimitive.Root
-      defaultValue={defaultValue}
-      value={value}
-      onValueChange={onValueChange}
-      asChild
-    >
-      <Styled.Tabs>
-        { children }
-      </Styled.Tabs>
-    </TabsPrimitive.Root>
+    <Styled.Tabs {...rest}>
+      { children }
+    </Styled.Tabs>
   )
 }
 
