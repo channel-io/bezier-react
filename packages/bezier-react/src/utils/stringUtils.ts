0b1ced0eb99269eceb4bd98efec830cbfe040bf7
Bora <annie_1229@naver.com>
2022-12-12 11:40:08 +0900

    refactor(map): migration lodash-es map -> es6 map

--- packages/bezier-react/src/utils/stringUtils.ts
+++ packages/bezier-react/src/utils/stringUtils.ts
@@ -1,7 +1,6 @@
 /* External dependencies */
 import {
   isEmpty,
-  map,
   trim,
 } from 'lodash-es'
 
@@ -14,7 +13,9 @@ import {
 export function mergeClassNames(className?: string, ...otherClassNames: Array<string | undefined>): string | undefined {
   if (!isEmpty(className) || !isEmpty(otherClassNames)) {
     const result: string[] = []
-    map([className, ...otherClassNames], (cn) => trim(cn)).forEach((cn) => result.push(cn))
+    const classNames = [className, ...otherClassNames]
+    classNames.map((cn) => trim(cn)).forEach((cn) => result.push(cn))
+    
     const joinedResult = result.filter((cn) => !isEmpty(cn)).join(' ')
     if (!isEmpty(joinedResult)) { return joinedResult }
   }
