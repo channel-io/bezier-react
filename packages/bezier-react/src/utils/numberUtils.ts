04160a92b990008b2812ae4619678935526d92a0
Bora <annie_1229@naver.com>
2022-12-15 16:51:43 +0900

    test(numberutils): add test numberUtils and fix range function

--- packages/bezier-react/src/utils/numberUtils.ts
+++ packages/bezier-react/src/utils/numberUtils.ts
@@ -1,11 +1,14 @@
 export function range(start: number, end?: number, _step?: number): number[] {
-  if (end === undefined) { return Array.from({ length: start }, (_, i) => i) }
+  if (end === undefined) {
+    if (start < 0) { return Array.from({ length: -1 * start }, (_, i) => (i ? -1 * i : 0)) }
+    return Array.from({ length: start }, (_, i) => i)
+  }
   const step = _step ?? 1
   const length = step === 0 ? (end - start) : ((end - start) / step)
   return Array.from({ length }, (_, i) => start + (i * step))
 }
 
-export function clamp(target: number, boundOne: number, boundTwo: number): number {
+export function clamp(target: number, boundOne: number, boundTwo?: number): number {
   if (!boundTwo) { return Math.max(target, boundOne) === boundOne ? target : boundOne }
   if (Math.min(target, boundOne) === target) { return boundOne }
   if (Math.max(target, boundTwo) === target) { return boundTwo }
