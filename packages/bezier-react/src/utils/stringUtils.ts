08086720215fe396a9cb9d45ed8841ff95c9f570
Bora <annie_1229@naver.com>
2022-12-22 10:28:47 +0900

    refactor(stringutils): refactor kebabCase RegExp

--- packages/bezier-react/src/utils/stringUtils.ts
+++ packages/bezier-react/src/utils/stringUtils.ts
@@ -82,10 +82,8 @@ export function toString(value: unknown): string {
 
 export const kebabCase = (value: string): string => value
   .trim()
-  .replace(/([a-z])([A-Z])/g, '$1-$2')
-  .replace(/([a-z])([0-9])/g, '$1-$2')
-  .replace(/([0-9])([A-Z])/g, '$1-$2')
-  .replace(/([0-9])([a-z])/g, '$1-$2')
+  .replace(/([a-z])([A-Z0-9])/g, '$1-$2')
+  .replace(/([0-9])([a-zA-Z])/g, '$1-$2')
   .replace(/[\s_]+/g, '-')
   .toLowerCase()
 
