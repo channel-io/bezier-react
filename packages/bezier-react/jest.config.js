5240f7207b3687bcb8cf717ba527ae610daceba4
Hyunseung Jeon <dogdriip@gmail.com>
2023-01-05 19:47:18 +0900

    fix(jest.config): add tilde alias to moduleNameMapper

--- packages/bezier-react/jest.config.js
+++ packages/bezier-react/jest.config.js
@@ -15,15 +15,7 @@ module.exports = {
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
     'identity-obj-proxy',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    '~/src/components/(.*)$': '<rootDir>/src/components/$1',
-    '~/src/constants/(.*)$': '<rootDir>/src/constants/$1',
-    '~/src/foundation/(.*)$': '<rootDir>/src/foundation/$1',
-    '~/src/hooks/(.*)$': '<rootDir>/src/hooks/$1',
-    '~/src/layout/(.*)$': '<rootDir>/src/layout/$1',
-    '~/src/providers/(.*)$': '<rootDir>/src/providers/$1',
-    '~/src/types/(.*)$': '<rootDir>/src/types/$1',
-    '~/src/utils/(.*)$': '<rootDir>/src/utils/$1',
-    '~/src/worklets/(.*)$': '<rootDir>/src/worklets/$1',
+    '~(.*)$': '<rootDir>$1',
   },
   collectCoverage: true,
   coveragePathIgnorePatterns: [
