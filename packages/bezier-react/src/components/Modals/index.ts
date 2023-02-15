60fd9dbf324f96399b8f63ed90fdca516201c428
Ed Sungik Choi <sungik.dev@gmail.com>
2023-02-15 17:26:44 +0900

    feat(legacy-modals): delete legacy components

--- packages/bezier-react/src/components/Modals/index.ts
+++ packages/bezier-react/src/components/Modals/index.ts
@@ -1,14 +1,2 @@
-/**
- * @deprecated
- */
-export * from './BaseModal'
-/**
- * @deprecated
- */
-export * as LegacyConfirmModal from './LegacyConfirmModal'
-/**
- * @deprecated
- */
-export * as LegacyModal from './LegacyModal'
 export * from './Modal'
 export * from './ConfirmModal'
