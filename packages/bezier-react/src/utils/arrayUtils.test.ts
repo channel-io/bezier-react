e6e86fbd758595dc716271996988d24ac59cfdf0
annie1229 <annie_1229@naver.com>
2023-02-24 16:21:28 +0900

    test(arrayutils): rebase and delete unnecessary code

--- packages/bezier-react/src/utils/arrayUtils.test.ts
+++ packages/bezier-react/src/utils/arrayUtils.test.ts
@@ -1,23 +1,7 @@
 /* Internal dependencies */
-import { removeItem, compact, flattenDeep } from './arrayUtils'
+import { compact, flattenDeep } from './arrayUtils'
 
 describe('arrayUtils', () => {
-  describe('removeItem', () => {
-    it('should remove item of given index', () => {
-      const array = [1, 2, 3, 4, 5]
-
-      expect(removeItem(array, 0)).toEqual([2, 3, 4, 5])
-      expect(removeItem(array, 2)).toEqual([1, 2, 4, 5])
-    })
-
-    it('should return given array if index is out-of-bound', () => {
-      const array = [1, 2, 3, 4, 5]
-
-      expect(removeItem(array, -1)).toEqual(array)
-      expect(removeItem(array, 5)).toEqual(array)
-    })
-  })
-
   describe('compact', () => {
     it('should remove falsy item', () => {
       const array = [0, 1, false, 2, '', 3]
