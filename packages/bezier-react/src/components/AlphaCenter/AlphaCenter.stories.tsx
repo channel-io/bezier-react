380aa43b95169a48b9bf0588f5819c558119f88c
Ed Sungik Choi <sungik.dev@gmail.com>
2023-03-13 14:36:52 +0900

    test(alpha-center): add story

---
+++ packages/bezier-react/src/components/AlphaCenter/AlphaCenter.stories.tsx
@@ -0,0 +1,33 @@
+/* External dependencies */
+import React from 'react'
+import base from 'paths.macro'
+import { Story, Meta } from '@storybook/react'
+
+/* Internal dependencies */
+import { getTitle } from 'Utils/storyUtils'
+import { Text } from 'Components/Text'
+import { AlphaCenter } from './AlphaCenter'
+import { AlphaCenterProps } from './AlphaCenter.types'
+
+export default {
+  title: getTitle(base),
+  component: AlphaCenter,
+} as Meta<AlphaCenterProps>
+
+const Template: Story<AlphaCenterProps> = ({ children, ...rest }) => (
+  <AlphaCenter {...rest}>
+    <Text color="txt-black-darkest">
+      { children }
+    </Text>
+  </AlphaCenter>
+)
+
+export const Primary = Template.bind({})
+
+Primary.args = {
+  style: {
+    width: '200px',
+    height: '200px',
+  },
+  children: 'Centered content',
+}
