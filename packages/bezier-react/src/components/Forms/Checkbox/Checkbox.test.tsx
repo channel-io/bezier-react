3b49298efd3e74250d94f433ba15ddb5ea7366b8
Ed Sungik Choi <sungik.dev@gmail.com>
2023-03-15 18:15:44 +0900

    test(checkbox): add unit test

---
+++ packages/bezier-react/src/components/Forms/Checkbox/Checkbox.test.tsx
@@ -0,0 +1,130 @@
+/* External dependencies */
+import React from 'react'
+import { isInaccessible } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+/* Internal dependencies */
+import { render } from 'Utils/testUtils'
+import { Checkbox } from './Checkbox'
+import { CheckboxProps } from './Checkbox.types'
+
+const VALUES = ['0', '1', '2']
+
+describe('Checkbox', () => {
+  const renderCheckbox = ({
+    children,
+    ...rest
+  }: CheckboxProps = {}) => render(
+    <Checkbox {...rest}>
+      { children }
+    </Checkbox>,
+  )
+
+  const renderCheckboxes = (
+    props: Omit<CheckboxProps, 'children'> = {},
+  ) => render(
+    <div role="group">
+      { VALUES.map(value => (
+        <Checkbox
+          key={value}
+          value={value}
+          {...props}
+        >
+          { value }
+        </Checkbox>
+      )) }
+    </div>,
+  )
+
+  let user: ReturnType<typeof userEvent.setup>
+
+  beforeEach(() => {
+    user = userEvent.setup()
+  })
+
+  describe('ARIA', () => {
+    it('should be accessible', () => {
+      const { container } = renderCheckbox()
+      expect(isInaccessible(container)).toBe(false)
+    })
+
+    it('should have \'role="checkbox"\' attribute', () => {
+      const { getByRole } = renderCheckbox()
+      expect(getByRole('checkbox')).toBeInTheDocument()
+    })
+
+    it('should be disabled when disabled prop is true', () => {
+      const { getByRole } = renderCheckbox({ disabled: true })
+      expect(getByRole('checkbox')).toHaveAttribute('aria-disabled', 'true')
+    })
+
+    it('should be required when required prop is true', () => {
+      const { getByRole } = renderCheckbox({ required: true })
+      expect(getByRole('checkbox')).toHaveAttribute('aria-required', 'true')
+    })
+
+    it('should be invalid when hasError prop is true', () => {
+      const { getByRole } = renderCheckbox({ hasError: true })
+      expect(getByRole('checkbox')).toHaveAttribute('aria-invalid', 'true')
+    })
+
+    it('should have \'aria-checked="mixed"\' attribute If it is in indeterminate state', () => {
+      const { getByRole } = renderCheckbox({ checked: 'indeterminate' })
+      expect(getByRole('checkbox')).toHaveAttribute('aria-checked', 'mixed')
+    })
+  })
+
+  describe('User Interactions', () => {
+    it('should focus and check checkbox when user clicks on a checkbox', async () => {
+      const { getByRole } = renderCheckbox()
+      const checkbox = getByRole('checkbox')
+      await user.click(checkbox)
+      expect(checkbox).toHaveFocus()
+      expect(checkbox).toBeChecked()
+    })
+
+    it('should focus and check checkbox when user clicks on a label of checkbox', async () => {
+      const { getByText, getByRole } = renderCheckbox({ children: 'Label' })
+      const checkbox = getByRole('checkbox')
+      const label = getByText('Label')
+      await user.click(label)
+      expect(checkbox).toHaveFocus()
+      expect(checkbox).toBeChecked()
+    })
+
+    it('should call the checked change event handler when user clicks on a checkbox', async () => {
+      const onCheckedChange = jest.fn()
+      const { getByRole } = renderCheckbox({ onCheckedChange })
+      const checkbox = getByRole('checkbox')
+      await user.click(checkbox)
+      expect(onCheckedChange).toBeCalledTimes(1)
+    })
+
+    it('should focus on the first checkbox item when user presses tab key', async () => {
+      const { getByRole } = renderCheckboxes()
+      const checkbox = getByRole('checkbox', { name: VALUES[0] })
+      await user.tab()
+      expect(checkbox).toHaveFocus()
+    })
+
+    it('should check checkbox when user presses space key on a focused checkbox', async () => {
+      const { getByRole } = renderCheckboxes()
+      const checkbox = getByRole('checkbox', { name: VALUES[0] })
+      await user.tab()
+      expect(checkbox).toHaveFocus()
+      await user.keyboard('{ }')
+      expect(checkbox).toHaveFocus()
+      expect(checkbox).toBeChecked()
+    })
+
+    it('should call the checked change event handler user presses space key on a focused checkbox', async () => {
+      const onCheckedChange = jest.fn()
+      const { getByRole } = renderCheckboxes({ onCheckedChange })
+      const checkbox = getByRole('checkbox', { name: VALUES[0] })
+      await user.tab()
+      expect(checkbox).toHaveFocus()
+      await user.keyboard('{ }')
+      expect(onCheckedChange).toBeCalledTimes(1)
+    })
+  })
+})
