eed61b3ce839b22900a7ef1cb51e65bdc0da6584
Ed Sungik Choi <sungik.dev@gmail.com>
2023-03-17 17:34:32 +0900

    chore(eslint): install import-newlines plugin and refine rules

--- supports/eslint-config-bezier/index.js
+++ supports/eslint-config-bezier/index.js
@@ -1,5 +1,15 @@
 module.exports = {
+  plugins: [
+    "import-newlines"
+  ],
   extends: [
     '@channel.io/eslint-config',
   ],
+  rules: {
+    'import/no-deprecated': 'warn',
+    'import/no-duplicates': ['error', { 'prefer-inline': true }],
+    'import-newlines/enforce': ['error', { items: 1 }],
+    '@typescript-eslint/consistent-type-imports': ['error', { fixStyle: 'inline-type-imports' }],
+    '@typescript-eslint/consistent-type-exports': 'error',
+  }
 }
