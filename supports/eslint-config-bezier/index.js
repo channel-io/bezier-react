3f448d98e31da0af6552498a389e92c1fcb6d648
Ed Sungik Choi <sungik.dev@gmail.com>
2023-03-21 14:39:53 +0900

    chore(eslintrc): enable a11y-specific rules

--- supports/eslint-config-bezier/index.js
+++ supports/eslint-config-bezier/index.js
@@ -6,9 +6,12 @@ module.exports = {
     '@channel.io/eslint-config',
   ],
   rules: {
-    'import/no-duplicates': ['error', { 'prefer-inline': true }],
     'import-newlines/enforce': ['error', { items: 1 }],
+    'import/no-duplicates': ['error', { 'prefer-inline': true }],
     '@typescript-eslint/consistent-type-imports': ['error', { fixStyle: 'inline-type-imports' }],
     '@typescript-eslint/consistent-type-exports': 'error',
+    'jsx-a11y/alt-text': 'warn',
+    'jsx-a11y/click-events-have-key-events': 'warn',
+    'jsx-a11y/no-static-element-interactions': 'warn',
   }
 }
