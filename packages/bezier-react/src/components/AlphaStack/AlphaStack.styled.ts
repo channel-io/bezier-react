26d52766d9674388d33f61189837761941ee8ce6
John <john@Johnui-MacBookPro.local>
2023-03-24 17:28:21 +0900

    fix: lint style

--- packages/bezier-react/src/components/AlphaStack/AlphaStack.styled.ts
+++ packages/bezier-react/src/components/AlphaStack/AlphaStack.styled.ts
@@ -9,13 +9,12 @@ interface ContainerProps extends
 
 export const Container = styled.div<ContainerProps>`
   display: flex;
+  flex-direction: var(--bezier-alpha-stack-direction);
+  gap: var(--bezier-alpha-stack-spacing);
+  align-items: var(--bezier-alpha-stack-align);
+  justify-content: var(--bezier-alpha-stack-justify);
   width: 100%;
   height: 100%;
-
-  flex-direction: var(--bezier-alpha-stack-direction);
-  justify-content: var(--bezier-alpha-stack-justify);
-  align-items: var(--bezier-alpha-stack-align);
-  gap: var(--bezier-alpha-stack-spacing);
 
   ${({ interpolation }) => interpolation}
 `
