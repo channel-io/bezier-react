347b3cd0d9cc6dfeddad9cb9de14c2445c3abb8a
Ed Sungik Choi <sungik.dev@gmail.com>
2023-03-24 16:18:49 +0900

    chore(github): fix the prerelease workflow to work

--- .github/create-ch-desk-bullets-blocks.sh
+++ .github/create-ch-desk-bullets-blocks.sh
@@ -16,4 +16,4 @@
 #     "value": "@xx/xy@0.8.9: <link type=\"url\" value=\"https://github.com/channel-io/bezier-react/releases/tag/@xx/xy@0.8.9\">Release Note</link>"
 #   }
 # ]
-jq --raw-output '[ .[] | (.name + "@" + .version) | {"type": "text", "value": (. + ": <link type=\"url\" value=\"https://github.com/channel-io/bezier-react/releases/tag/" + . + "\">Release Note</link>") } ]'
+jq --compact-output '[ .[] | (.name + "@" + .version) | {"type": "text", "value": (. + ": <link type=\"url\" value=\"https://github.com/channel-io/bezier-react/releases/tag/" + . + "\">Release Note</link>") } ]'
