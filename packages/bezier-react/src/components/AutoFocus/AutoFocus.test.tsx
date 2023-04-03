23f3cad6c56ac974b63d49823b45aeaca194d979
Ed Sungik Choi <sungik.dev@gmail.com>
2023-04-03 15:08:10 +0900

    test(auto-focus): add test cases related to when prop

--- packages/bezier-react/src/components/AutoFocus/AutoFocus.test.tsx
+++ packages/bezier-react/src/components/AutoFocus/AutoFocus.test.tsx
@@ -7,8 +7,13 @@ import { AutoFocus } from './AutoFocus'
 import { type AutoFocusProps } from './AutoFocus.types'
 
 describe('AutoFocus', () => {
-  const renderComponent = ({ children }: AutoFocusProps) => render(
-    <AutoFocus>{ children }</AutoFocus>,
+  const renderComponent = ({
+    children,
+    ...rest
+  }: AutoFocusProps) => render(
+    <AutoFocus {...rest}>
+      { children }
+    </AutoFocus>,
   )
 
   it('should focus on the child element - HTMLDivElement', () => {
@@ -33,5 +38,23 @@ describe('AutoFocus', () => {
     })
     const rendered = getByRole('textbox')
     expect(rendered).toHaveFocus()
+  })
+
+  it('should focus on the child element when the `when` condition prop is true', () => {
+    const { getByText } = renderComponent({
+      when: true,
+      children: <button type="button">Close</button>,
+    })
+    const rendered = getByText('Close')
+    expect(rendered).toHaveFocus()
+  })
+
+  it('should not focus on the child element when the `when` condition prop is false', () => {
+    const { getByText } = renderComponent({
+      when: false,
+      children: <button type="button">Close</button>,
+    })
+    const rendered = getByText('Close')
+    expect(rendered).not.toHaveFocus()
   })
 })
