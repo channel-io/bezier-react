264fadb2ded3fac2c490c0c42eb816ee53c4ac43
Ed Sungik Choi <sungik.dev@gmail.com>
2023-04-06 17:50:18 +0900

    chore(eslintrc): enhance import/order rule

--- packages/bezier-react/.eslintrc.js
+++ packages/bezier-react/.eslintrc.js
@@ -6,59 +6,57 @@ module.exports = {
     project: './tsconfig.eslint.json',
   },
   rules: {
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: true,
+        ignoreDeclarationSort: true,
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc' },
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        pathGroupsExcludedImportTypes: ['react', 'react-dom'],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: 'react-dom',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '~/src/foundation',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '~/src/foundation/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '~/src/components/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: './**/*.styled',
+            group: 'sibling',
+            position: 'after',
+          },
+        ],
+      },
+    ],
     'max-classes-per-file': 'off',
     'react/jsx-props-no-spreading': 'off',
     'react/no-array-index-key': 'warn',
     '@typescript-eslint/naming-convention': 'off',
   },
-  overrides: [
-    {
-      files: ['./packages/**/src/**/*'],
-      rules: {
-        'import/order': [
-          'error',
-          {
-            groups: ['external', 'builtin', 'internal', 'parent', 'sibling', 'index'],
-            pathGroups: [
-              {
-                pattern: '~/src/foundation',
-                group: 'internal',
-                position: 'before',
-              },
-              {
-                pattern: '~/src/foundation/**',
-                group: 'internal',
-                position: 'before',
-              },
-              {
-                pattern: '~/src/providers/**',
-                group: 'internal',
-                position: 'before',
-              },
-              {
-                pattern: '~/src/hooks/**',
-                group: 'internal',
-                position: 'before',
-              },
-              {
-                pattern: '~/src/worklets/**',
-                group: 'internal',
-                position: 'before',
-              },
-              {
-                pattern: '~/src/@(constants|types|utils|assets)/**',
-                group: 'internal',
-                position: 'before',
-              },
-              {
-                pattern: '~/src/@(components|layout)/**',
-                group: 'internal',
-                position: 'before',
-              },
-            ],
-          },
-        ],
-      },
-    },
-  ],
 }
