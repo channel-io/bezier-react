5c9ba2d8ffcae49fa63f7076f866a8709b183bed
Ed Sungik Choi <sungik.dev@gmail.com>
2023-04-07 16:23:21 +0900

    feat(segmented-control/item): implement basic logic to consume context

--- packages/bezier-react/src/components/AlphaSegmentedControl/SegmentedControlItem.tsx
+++ packages/bezier-react/src/components/AlphaSegmentedControl/SegmentedControlItem.tsx
@@ -1,22 +1,37 @@
 import React, { forwardRef } from 'react'
 
 import * as RadioGroupPrimitive from '@radix-ui/react-radio-group'
+import * as TabsPrimitive from '@radix-ui/react-tabs'
 
 import { type SegmentedControlItemProps } from './SegmentedControl.types'
+import { useSegmentedControlContext } from './SegmentedControlContext'
 
 function SegmentedControlItemImpl<Value extends string>({
   value,
   children,
   ...rest
 }: SegmentedControlItemProps<Value>, forwardedRef: React.Ref<HTMLButtonElement>) {
+  const contextValue = useSegmentedControlContext()
+
+  if (!contextValue) {
+    throw new Error('SegmentedControlItem must be used within SegmentedControl')
+  }
+
+  // TODO: Implement size
+  const { type, size } = contextValue
+
+  const SegmentedControlItem = type === 'radiogroup'
+    ? RadioGroupPrimitive.Item
+    : TabsPrimitive.Trigger
+
   return (
-    <RadioGroupPrimitive.Item
+    <SegmentedControlItem
       ref={forwardedRef}
       value={value}
       {...rest}
     >
       { children }
-    </RadioGroupPrimitive.Item>
+    </SegmentedControlItem>
   )
 }
 
