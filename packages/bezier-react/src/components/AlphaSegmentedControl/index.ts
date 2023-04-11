9d842905f375374fa65d7b4f6cc3105636292881
Ed Sungik Choi <sungik.dev@gmail.com>
2023-04-11 16:12:31 +0900

    feat(segmented-control): export modules

---
+++ packages/bezier-react/src/components/AlphaSegmentedControl/index.ts
@@ -0,0 +1,14 @@
+export {
+  SegmentedControl,
+  SegmentedControlTabList,
+  SegmentedControlTabContent,
+} from './SegmentedControl'
+
+export { SegmentedControlItem } from './SegmentedControlItem'
+
+export type {
+  SegmentedControlProps,
+  SegmentedControlItemProps,
+  SegmentedControlTabListProps,
+  SegmentedControlTabContentProps,
+} from './SegmentedControl.types'
