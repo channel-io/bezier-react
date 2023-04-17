4dcf60d7437c10c761989c27df4c64f9caadf7eb
Jamie Choi <toonation@gmail.com>
2023-04-17 17:11:00 +0900

    Extract the icons from Figma (#1284)
    * feat(icons): update the icons
    
    * feat(icons): generate icons
    
    * chore(changeset): add changeset
    
    ---------
    
    Co-authored-by: ch-builder <eng@channel.io>
    Co-authored-by: Ed Sungik Choi <sungik.dev@gmail.com>

---
+++ packages/bezier-react/src/components/Icon/generated/DragableAlt.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react'
+import { SVGProps } from 'react'
+import { createBezierIcon } from '../utils'
+function SvgDragableAlt(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="M17 8.25a2.25 2.25 0 1 0 4.5 0 2.25 2.25 0 0 0-4.5 0ZM17 15.75a2.25 2.25 0 1 0 4.5 0 2.25 2.25 0 0 0-4.5 0ZM12 10.5A2.25 2.25 0 1 1 12 6a2.25 2.25 0 0 1 0 4.5ZM9.75 15.75a2.25 2.25 0 1 0 4.5 0 2.25 2.25 0 0 0-4.5 0ZM4.75 10.5a2.25 2.25 0 1 1 0-4.5 2.25 2.25 0 0 1 0 4.5ZM2.5 15.75a2.25 2.25 0 1 0 4.5 0 2.25 2.25 0 0 0-4.5 0Z"
+      />
+    </svg>
+  )
+}
+export default createBezierIcon(SvgDragableAlt)
