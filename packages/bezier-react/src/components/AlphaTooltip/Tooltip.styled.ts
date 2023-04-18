5115e00e7478aed61d5e0969d568a6435ed89cc7
Ed Sungik Choi <sungik.dev@gmail.com>
2023-04-18 17:45:43 +0900

    feat(alpha-tooltip): add slide animation

--- packages/bezier-react/src/components/AlphaTooltip/Tooltip.styled.ts
+++ packages/bezier-react/src/components/AlphaTooltip/Tooltip.styled.ts
@@ -2,14 +2,75 @@ import * as TooltipPrimitive from '@radix-ui/react-tooltip'
 
 import {
   LineHeightAbsoluteNumber,
+  TransitionDuration,
   Typography,
+  css,
   ellipsis,
+  keyframes,
   styled,
 } from '~/src/foundation'
 
 import { ZIndex } from '~/src/constants/ZIndex'
 
 import { Text } from '~/src/components/Text'
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+`
+
+const SLIDE_OFFSET = 2
+
+const slideUp = keyframes`
+  from {
+    transform: translateY(${SLIDE_OFFSET}px);
+  }
+
+  to {
+    transform: translateY(0);
+  }
+`
+
+const slideRight = keyframes`
+  from {
+    transform: translateX(-${SLIDE_OFFSET}px);
+  }
+
+  to {
+    transform: translateX(0);
+  }
+`
+
+const slideDown = keyframes`
+  from {
+    transform: translateY(-${SLIDE_OFFSET}px);
+  }
+
+  to {
+    transform: translateY(0);
+  }
+`
+
+const slideLeft = keyframes`
+  from {
+    transform: translateX(${SLIDE_OFFSET}px);
+  }
+
+  to {
+    transform: translateX(0);
+  }
+`
+
+const getSlideAnimation = (frames: ReturnType<typeof keyframes>) => css`
+  animation-name: ${() => frames}, ${() => fadeIn};
+  animation-duration: ${TransitionDuration.M}ms;
+  animation-timing-function: cubic-bezier(0.3, 0, 0, 1);
+`
 
 export const TooltipContent = styled(TooltipPrimitive.Content)`
   z-index: ${ZIndex.Tooltip};
@@ -25,6 +86,23 @@ export const TooltipContent = styled(TooltipPrimitive.Content)`
 
   ${({ foundation }) => foundation?.elevation?.ev3(true)};
   ${({ foundation }) => foundation?.rounding?.round8};
+
+  &[data-side="top"] {
+    ${getSlideAnimation(slideUp)}
+  }
+
+  &[data-side="right"] {
+    ${getSlideAnimation(slideRight)}
+  }
+
+  &[data-side="bottom"] {
+    ${getSlideAnimation(slideDown)}
+  }
+
+  &[data-side="left"] {
+    ${getSlideAnimation(slideLeft)}
+  }
+
 
   /* FIXME: type */
   ${({ interpolation }) => interpolation}
