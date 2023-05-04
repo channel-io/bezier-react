8a9dc1fdd98523ee659a1de93b1bd72e623dbfd1
Ed Sungik Choi <sungik.dev@gmail.com>
2023-05-04 18:20:55 +0900

    test(checkable-avatar): rewrite story

--- packages/bezier-react/src/components/Avatars/CheckableAvatar/CheckableAvatar.stories.tsx
+++ packages/bezier-react/src/components/Avatars/CheckableAvatar/CheckableAvatar.stories.tsx
@@ -1,7 +1,4 @@
-import React, {
-  useCallback,
-  useState,
-} from 'react'
+import React from 'react'
 
 import {
   type Meta,
@@ -9,195 +6,47 @@ import {
 } from '@storybook/react'
 import { base } from 'paths.macro'
 
-import { styled } from '~/src/foundation'
-
-import { getTitle } from '~/src/utils/storyUtils'
-import { isNaN } from '~/src/utils/typeUtils'
-
 import {
-  Avatar,
-  AvatarSize,
-} from '~/src/components/Avatars/Avatar'
+  getObjectFromEnum,
+  getTitle,
+} from '~/src/utils/storyUtils'
+
+import { AvatarSize } from '~/src/components/Avatars/Avatar'
 
 import { CheckableAvatar } from './CheckableAvatar'
 import type { CheckableAvatarProps } from './CheckableAvatar.types'
-
-const MOCK_AVATAR_URL = 'https://cf.channel.io/thumb/200x200/pub-file/1/606d87d059a6093594c0/ch-symbol-filled-smiley-bg.png'
-
-const MOCK_AVATAR_LIST = [
-  {
-    id: 1,
-    avatarUrl: 'https://bit.ly/code-beast',
-    name: 'Christian Nwamba',
-  },
-  {
-    id: 2,
-    avatarUrl: 'https://bit.ly/tioluwani-kolawole',
-    name: 'Kola Tioluwani',
-  },
-  {
-    id: 3,
-    avatarUrl: 'https://bit.ly/kent-c-dodds',
-    name: 'Kent Dodds',
-  },
-  {
-    id: 4,
-    avatarUrl: 'https://bit.ly/ryan-florence',
-    name: 'Ryan Florence',
-  },
-  {
-    id: 5,
-    avatarUrl: 'https://bit.ly/dan-abramov',
-    name: 'Dan Abrahmov',
-  },
-  {
-    id: 6,
-    avatarUrl: 'https://bit.ly/prosper-baba',
-    name: 'Prosper Otemuyiwa',
-  },
-  {
-    id: 7,
-    avatarUrl: 'https://bit.ly/sage-adebayo',
-    name: 'Segun Adebayo',
-  },
-]
-
-const avatarSizeList = Object.keys(AvatarSize)
-  .filter(value => isNaN(Number(value)) === true)
-  .map(key => AvatarSize[key])
 
 export default {
   title: getTitle(base),
   component: CheckableAvatar,
   argTypes: {
-    size: {
+    checked: {
       control: {
-        type: 'radio',
-        options: avatarSizeList,
+        type: 'boolean',
       },
     },
-    checkedBackgroundColor: {
+    size: {
       control: {
-        type: 'radio',
-        options: [
-          'bgtxt-green-normal',
-          'bgtxt-cobalt-normal',
-        ],
+        type: 'select',
+        options: getObjectFromEnum(AvatarSize),
       },
     },
   },
-} as Meta
+} as Meta<CheckableAvatarProps>
 
-// NOTE: (@ed) border 색상을 명확하게 보여주기 위해 회색의 Wrapper를 추가했습니다
-const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 200px;
-  height: 200px;
-  background-color: ${({ foundation }) => foundation?.theme?.['bg-grey-light']};
-`
+const Template: Story<CheckableAvatarProps> = ({ children, ...rest }) => (
+  <CheckableAvatar {...rest}>
+    { children }
+  </CheckableAvatar>
+)
 
-type CheckableAvatarPropsWithAdditionalAvatar = CheckableAvatarProps & { withAdditionalAvatar: boolean }
-
-const Template: Story<CheckableAvatarPropsWithAdditionalAvatar> = ({ withAdditionalAvatar, ...args }) => {
-  const [checked, setChecked] = useState(false)
-
-  const handleClick = useCallback(() => {
-    setChecked(!checked)
-  }, [checked])
-
-  return (
-    <Wrapper>
-      <CheckableAvatar
-        {...args}
-        checked={checked}
-        onClick={handleClick}
-      >
-        { withAdditionalAvatar && (
-          <Avatar
-            size={AvatarSize.Size20}
-            avatarUrl={args.avatarUrl}
-            name="additionalAvatar"
-            showBorder
-          />
-        ) }
-      </CheckableAvatar>
-    </Wrapper>
-  )
-}
-
-export const Primary: Story<CheckableAvatarPropsWithAdditionalAvatar> = Template.bind({})
+export const Primary: Story<CheckableAvatarProps> = Template.bind({})
 Primary.args = {
-  avatarUrl: MOCK_AVATAR_URL,
+  avatarUrl: 'https://cf.channel.io/thumb/200x200/pub-file/1/606d87d059a6093594c0/ch-symbol-filled-smiley-bg.png',
   name: 'Channel',
   size: AvatarSize.Size24,
-  showBorder: false,
+  checked: undefined,
+  defaultChecked: false,
   disabled: false,
-  checked: false,
-  withAdditionalAvatar: false,
-}
-
-const List = styled.li`
-  display: flex;
-  align-items: center;
-  padding: 4px;
-  cursor: pointer;
-
-  &:hover {
-    background-color: ${({ foundation }) => foundation?.theme?.['bg-grey-light']};
-  }
-`
-
-const Name = styled.span`
-  margin-left: 4px;
-`
-
-const TemplateCheckableAvatarList: Story<CheckableAvatarProps> = (args) => {
-  const [checkedList, setCheckedList] = useState<{ [id: number]: boolean }>({})
-
-  const handleClickList = useCallback((id: number) => {
-    setCheckedList({
-      ...checkedList,
-      [id]: !checkedList[id],
-    })
-  }, [checkedList])
-
-  return (
-    <ul>
-      { MOCK_AVATAR_LIST.map(({ id, avatarUrl, name }) => (
-        <List
-          key={id}
-        >
-          <CheckableAvatar
-            {...args}
-            avatarUrl={avatarUrl}
-            name={name}
-            checked={checkedList[id]}
-            onClick={() => handleClickList(id)}
-          />
-          <Name>{ name }</Name>
-        </List>
-      )) }
-      <List>
-        <CheckableAvatar
-          avatarUrl="https://bit.ly/dan-abramov"
-          name="Dan Abramov"
-          disabled
-        />
-        <Name>Disabled Dan</Name>
-      </List>
-    </ul>
-  )
-}
-
-export const CheckableAvatarList: Story<CheckableAvatarProps> = TemplateCheckableAvatarList.bind({})
-CheckableAvatarList.args = {
-  avatarUrl: MOCK_AVATAR_URL,
-  name: 'Channel',
-  size: AvatarSize.Size24,
   showBorder: false,
-  disabled: false,
-  checked: false,
 }
-
