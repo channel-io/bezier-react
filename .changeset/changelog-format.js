d6f35d4b48c74a60a070861a70ab7c07f78a55e8
Ed <sungik.dev@gmail.com>
2023-05-30 18:20:57 +0900

    Add a changelog custom format script (#1374)
    * chore(deps): add @changsets/types package
    
    * chore(changeset): add a changelog custom format script
    
    * chore(deps): remove unused deps
    
    * chore(changeset): remove unnecessary changeset
    
    * chore(changeset): refactor format script

---
+++ .changeset/changelog-format.js
@@ -0,0 +1,55 @@
+const { getInfo } = require('@changesets/get-github-info')
+
+function optionChecker(options) {
+  if (!options || !options.repo) {
+    throw new Error(
+      'Please provide a repo to this changelog generator like this:\n"changelog": ["./changelog-format.js", { "repo": "org/repo" }]',
+    )
+  }
+}
+
+/** @type {import("@changesets/types").ChangelogFunctions} */
+const changelogFunctions = {
+  getDependencyReleaseLine: async (_, dependenciesUpdated, options) => {
+    optionChecker(options)
+
+    if (dependenciesUpdated.length === 0) {
+      return ""
+    }
+
+    const updatedDependenciesList = dependenciesUpdated.map((dependency) =>
+      `  - ${dependency.name}@${dependency.newVersion}`
+    )
+
+    return `\n- Updated dependencies\n${updatedDependenciesList.join("\n")}`
+  },
+  getReleaseLine: async (changeset, _, options) => {
+    optionChecker(options)
+
+    const [firstLine, ...nextLines] = changeset.summary
+      .split("\n")
+      .map((l) => l.trimEnd())
+
+    let releaseLine = `\n\n- ${firstLine}`
+
+    if (changeset.commit) {
+      const { links, user } = await getInfo({
+        repo: options.repo,
+        commit: changeset.commit,
+      })
+
+      const pullRequestLink = links.pull === null ? '' : links.pull
+      const userMention = user === null ? '' : `@${user}`
+
+      releaseLine += ` (${pullRequestLink}) by ${userMention}`
+    }
+
+    if (nextLines.length > 0) {
+      releaseLine += `\n${nextLines.map((l) => `  ${l}`).join("\n")}`
+    }
+
+    return releaseLine
+  },
+}
+
+module.exports = changelogFunctions
