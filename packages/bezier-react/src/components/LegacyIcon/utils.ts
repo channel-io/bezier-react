c6f4bf12e98823c35299216716d748c25ded5d28
Ed Sungik Choi <sungik.dev@gmail.com>
2023-06-02 14:44:10 +0900

    docs(icons): update jsdoc

--- packages/bezier-react/src/components/LegacyIcon/utils.ts
+++ packages/bezier-react/src/components/LegacyIcon/utils.ts
@@ -6,12 +6,13 @@ import {
 /**
  * @deprecated Please import and use individual icons.
  * @example
- * import { Icon, AllIcon, type IconProps } from '@channel.io/bezier-react'
+ * import { AllIcon } from '@channel.io/bezier-icons'
+ * import { Icon, type IconProps } from '@channel.io/bezier-react'
  * <Icon source={AllIcon} color="bg-black-dark" />
  * @example <caption>How to validate the bezier icon source</caption>
- * import { isBezierIcon, AllIcon } from '@channel.io/bezier-react'
+ * import { isBezierIcon, AllIcon } from '@channel.io/bezier-icons'
  * isBezierIcon(AllIcon) // true
- * isBezierIcon(<svg>...</svg>) // false
+ * isBezierIcon(() => <svg>...</svg>) // false
  * @example <caption>Legacy icon component is still available. but it will be removed in future versions!</caption>
  * import { LegacyIcon, type LegacyIconProps } from '@channel.io/bezier-react'
  * <LegacyIcon name="all" color="bg-black-dark" />
