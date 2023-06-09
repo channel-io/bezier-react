1b97b7a2bb05299b8cf1d743656ec3ba81b40d64
Ed Sungik Choi <sungik.dev@gmail.com>
2023-06-09 12:22:27 +0900

    fix: change the identifier of the bezier icon to a string literal instead of a symbol

--- packages/bezier-icons/utils/index.ts
+++ packages/bezier-icons/utils/index.ts
@@ -3,17 +3,17 @@
  * so set the type inside this module to 'any'.
  */
 
-const BEZIER_ICON_SYMBOL = Symbol('bezier.icon')
+const BEZIER_ICON_ID = '__bezier__icon'
 
 export function isBezierIcon(arg: any) {
   return (
     typeof arg === 'function' &&
-    arg[BEZIER_ICON_SYMBOL] === true
+    arg[BEZIER_ICON_ID] === true
   )
 }
 
 export function createBezierIcon(source: any) {
   const clone = source
-  clone[BEZIER_ICON_SYMBOL] = true
+  clone[BEZIER_ICON_ID] = true
   return clone
 }
