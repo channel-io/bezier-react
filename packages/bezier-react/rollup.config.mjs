73755af5cdda70f18445ecec65a816a64ff88ed0
Ed Sungik Choi <sungik.dev@gmail.com>
2023-06-15 17:21:06 +0900

    build(rollup): changes to rollup settings and TypeScript type generation process (WIP)

--- packages/bezier-react/rollup.config.mjs
+++ packages/bezier-react/rollup.config.mjs
@@ -1,89 +1,72 @@
-import { exec } from 'child_process'
+import { readFileSync } from 'fs'
+import * as path from 'path'
+import { fileURLToPath } from 'url'
 
 import { DEFAULT_EXTENSIONS } from '@babel/core'
+import alias from '@rollup/plugin-alias'
 import babel from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
-import typescript from '@rollup/plugin-typescript'
 import url from '@rollup/plugin-url'
 import { defineConfig } from 'rollup'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import { visualizer } from 'rollup-plugin-visualizer'
 
+const pkg = JSON.parse(
+  readFileSync(fileURLToPath(new URL('./package.json', import.meta.url))),
+)
+
+const rootDir = fileURLToPath(new URL('.', import.meta.url))
+
 const extensions = [...DEFAULT_EXTENSIONS, '.ts', '.tsx']
 
-/**
- * @type {import('rollup').PluginImpl}
- */
-function tscAlias() {
-  return {
-    name: 'tscAlias',
-    writeBundle: () => new Promise((resolve, reject) => {
-      exec('tsc-alias', function callback(error, stdout, stderr) {
-        if (stderr || error) {
-          reject(stderr || error)
-        } else {
-          resolve(stdout)
-        }
-      })
-    }),
-  }
-}
-
-const commonPlugins = [
-  commonjs(),
-  nodeResolve({ extensions }),
-  typescript({
-    tsconfig: './tsconfig.json',
-    exclude: [
-      '**/*.stories.tsx',
-      '**/*.test.ts',
-      '**/*.test.tsx',
-      './src/utils/storyUtils.ts',
-      './src/utils/testUtils.tsx',
-    ],
-  }),
-  babel({
-    babelHelpers: 'runtime',
-    skipPreflightCheck: true,
-    exclude: 'node_modules/**',
-    extensions,
-  }),
-  peerDepsExternal(),
-  url(),
-  tscAlias(),
-  visualizer({ filename: 'stats.html' }),
-]
-
 const generateConfig = ({
-  output: _output,
-  plugins: _plugins,
+  output = [],
+  plugins = [],
 }) => defineConfig({
   input: 'src/index.ts',
-  output: {
-    ..._output,
-    sourcemap: true,
-  },
-  plugins: [
-    ..._plugins,
-    ...commonPlugins,
-  ],
+  output,
   external: [/@babel\/runtime/],
+  plugins: [
+    alias({
+      entries: [{
+        find: '~',
+        replacement: rootDir,
+      }],
+    }),
+    peerDepsExternal(),
+    nodeResolve({ extensions }),
+    commonjs(),
+    babel({
+      babelHelpers: 'runtime',
+      skipPreflightCheck: true,
+      exclude: 'node_modules/**',
+      extensions,
+    }),
+    url(),
+    visualizer({ filename: 'stats.html' }),
+    ...plugins,
+  ],
 })
 
 export default defineConfig([
   generateConfig({
-    output: {
-      file: 'dist/index.cjs.js',
+    output: [{
       format: 'cjs',
-    },
-  }),
-  generateConfig({
-    output: {
-      dir: 'dist',
-      format: 'esm',
+      dir: path.dirname(pkg.main),
+      sourcemap: true,
       preserveModules: true,
-      preserveModulesRoot: '.',
+      preserveModulesRoot: 'src',
+      entryFileNames: '[name].js',
+      exports: 'named',
     },
+    {
+      format: 'esm',
+      dir: path.dirname(pkg.module),
+      sourcemap: true,
+      preserveModules: true,
+      preserveModulesRoot: 'src',
+      entryFileNames: '[name].mjs',
+    }],
   }),
 ])
