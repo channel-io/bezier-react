ed994faca99f60e02489fdccb53e0fb8332891e1
Ed Sungik Choi <sungik.dev@gmail.com>
2023-06-19 13:33:09 +0900

    build(babel): remove unnecessary plugins embedded in preset-env

--- packages/bezier-react/babel.config.js
+++ packages/bezier-react/babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   presets: [
-    '@babel/preset-env',
+    ['@babel/preset-env', { useBuiltIns: 'entry', corejs: '3.31.0', bugfixes: true }],
     /**
      * Will use the native built-in instead of trying to polyfill behavior for any plugins that require one.
      * Remove the helper function for Object.assign.
@@ -11,8 +11,6 @@ module.exports = {
   ],
   plugins: [
     '@babel/plugin-transform-runtime',
-    ['@babel/plugin-proposal-private-property-in-object', { loose: false }],
-    ['@babel/plugin-proposal-class-properties', { loose: false }],
     ['babel-plugin-styled-components', {
       minify: true,
       pure: true,
