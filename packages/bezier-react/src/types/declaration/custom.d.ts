c62cace9bbe23ee8048ecf40137771c317e9eeee
Ed Sungik Choi <sungik.dev@gmail.com>
2023-07-11 21:10:10 +0900

    refactor(d.ts): change target module pattern

--- packages/bezier-react/src/types/declaration/custom.d.ts
+++ packages/bezier-react/src/types/declaration/custom.d.ts
@@ -1,6 +1,6 @@
-declare module '*.scss' {
-  const content: { [className: string]: string }
-  export = content
+declare module '*.module.scss' {
+  const classes: { [className: string]: string }
+  export default classes
 }
 
 declare module '*.svg' {
