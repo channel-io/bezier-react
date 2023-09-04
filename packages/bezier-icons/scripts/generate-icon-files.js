3b7e3631b71a141049f5c4a727ac86e53a5eadd8
andrew <andrew@channel.io>
2023-09-04 16:13:47 +0900

    feat(bezier-icons): add script generating icon files

--- packages/bezier-icons/scripts/generate-icon-files.js
+++ packages/bezier-icons/scripts/generate-icon-files.js
@@ -1,1 +1,43 @@
-console.log("Hello world!")
+const fs = require('fs')
+const path = require('path')
+
+const svgo = require('svgo')
+
+const bezierIconsDirectory = path.resolve(__dirname, '..')
+const iconsJson = path.resolve(bezierIconsDirectory, 'icons.json')
+const iconsDir = path.join(bezierIconsDirectory, 'icons')
+const svgByName = JSON.parse(fs.readFileSync(iconsJson, 'utf-8'))
+
+const flushAndMakeIconsDirectory = () => {
+  if (fs.existsSync(iconsDir)) {
+    fs.rmSync(iconsDir, { recursive: true, force: true })
+    fs.mkdirSync(iconsDir)
+  }
+}
+
+const makeSvgFiles = ([iconName, svgObject]) => {
+  const svgPath = path.resolve(iconsDir, `${iconName}.svg`)
+  const { svg } = svgObject
+  const svgElement = svgo.optimize(svg, {
+    js2svg: {
+      indent: 0,
+      pretty: true,
+    },
+    plugins: [],
+  }).data
+
+  fs.writeFileSync(svgPath, svgElement, 'utf-8')
+}
+
+const generateSVGFilesFromMap = () => {
+  Object.entries(svgByName)
+    .sort(([iconName1], [iconName2]) => iconName1.localeCompare(iconName2))
+    .map(makeSvgFiles)
+}
+
+const generateIconFiles = () => {
+  flushAndMakeIconsDirectory()
+  generateSVGFilesFromMap()
+}
+
+generateIconFiles()
