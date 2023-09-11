772d763d5a4424efd36383fa5aefc96825528174
Ed <sungik.dev@gmail.com>
2023-09-11 15:26:02 +0900

    Upgrade dependencies (#1609)
    <!--
      How to write a good PR title:
    - Follow [the Conventional Commits
    specification](https://www.conventionalcommits.org/en/v1.0.0/).
      - Give as much context as necessary and as little as possible
      - Prefix it with [WIP] while it’s a work in progress
    -->
    
    ## Self Checklist
    
    - [x] I wrote a PR title in **English** and added an appropriate
    **label** to the PR.
    - [x] I wrote the commit message in **English** and to follow [**the
    Conventional Commits
    specification**](https://www.conventionalcommits.org/en/v1.0.0/).
    - [x] I [added the
    **changeset**](https://github.com/changesets/changesets/blob/main/docs/adding-a-changeset.md)
    about the changes that needed to be released. (or didn't have to)
    - [x] I wrote or updated **documentation** related to the changes. (or
    didn't have to)
    - [x] I wrote or updated **tests** related to the changes. (or didn't
    have to)
    - [x] I tested the changes in various browsers. (or didn't have to)
      - Windows: Chrome, Edge, (Optional) Firefox
      - macOS: Chrome, Edge, Safari, (Optional) Firefox
    
    ## Summary
    <!-- Please brief explanation of the changes made -->
    
    `yarn upgrade-interactive` 명령어를 통해 의존성을 업데이트합니다. (마이너 버전 범위까지)
    
    ## Details
    <!-- Please elaborate description of the changes -->
    
    - 5509f35a9e045132a52bae562844329c89e2a927 : 린트 규칙이 충돌하는 현상이 발생하여 비활성화
    처리합니다. 채널 eslint 패키지 최신화 및 TS 5.0 업데이트 이후 다시 살펴보려고 합니다.
    
    ### Breaking change? (Yes/No)
    <!-- If Yes, please describe the impact and migration path for users -->
    
    No

--- supports/eslint-config-bezier/index.js
+++ supports/eslint-config-bezier/index.js
@@ -4,7 +4,8 @@ module.exports = {
   rules: {
     "no-multiple-empty-lines": ['error', { max: 1, maxEOF: 1, maxBOF: 0 }],
     'import-newlines/enforce': ['error', { items: 1 }],
-    'import/no-duplicates': ['error', { 'prefer-inline': true }],
+    // FIXME: Conflict with consistent-type-imports rule. Try testing again after TS version 5 update.
+    // 'import/no-duplicates': ['error', { 'prefer-inline': true }],
     '@typescript-eslint/consistent-type-imports': ['error', { fixStyle: 'inline-type-imports' }],
     '@typescript-eslint/consistent-type-exports': 'error',
     'jsx-a11y/alt-text': 'warn',
