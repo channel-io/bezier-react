1c5eca2998e109f3ba2e66222715d1c80e39e7cf
Ed Sungik Choi <sungik.dev@gmail.com>
2023-09-13 21:37:02 +0900

    feat(smooth-corners): enhance to inject window as argument when activating smooth corners feature

--- packages/bezier-react/src/features/SmoothCornersFeature/SmoothCornersFeature.ts
+++ packages/bezier-react/src/features/SmoothCornersFeature/SmoothCornersFeature.ts
@@ -7,42 +7,71 @@ import {
 import { smoothCornersScript } from './smoothCornersScript'
 
 /**
+ * @see {@link https://fetch.spec.whatwg.org/#requestcredentials}
+ */
+type RequestCredentials = 'omit' | 'same-origin' | 'include'
+
+type WorkletOptions = {
+  credentials: RequestCredentials
+}
+
+/**
+ * @see {@link https://html.spec.whatwg.org/multipage/worklets.html#worklets-worklet}
+ */
+interface Worklet {
+  addModule: (moduleURL: string, options?: WorkletOptions) => Promise<void>
+}
+
+declare global {
+  namespace CSS {
+    export const paintWorklet: Worklet
+  }
+}
+
+/**
  * 🚨 This is an experimental feature! It may not be suitable for use in production.
  *
  * Instead of CSS border-radius, Use *Superellipse* masking using the CSS Houdini API.
  * When enabled, the feature will be applied to components with the `smoothCorners` property set to `true`.
  */
 class SmoothCornersFeature extends Feature {
+  private globalObject: typeof globalThis | null = null
+
   /**
    * @deprecated (@ed) Upcoming improvements
    * Changed to private property, and will be referenced via the useFeatureFlag context rather than directly externally.
    */
-  activated = false
+  activated: WeakMap<typeof globalThis, boolean> = new WeakMap([[globalThis, false]])
 
-  async activate() {
-    if (
-      !this.activated
-      && typeof CSS !== 'undefined'
-      && 'paintWorklet' in CSS
-    ) {
+  private updateCurrentGlobalObject(globalObject: typeof globalThis) {
+    this.globalObject = globalObject
+  }
+
+  private isGlobalObjectIdentical(globalObject: typeof globalThis) {
+    return Object.is(this.globalObject, globalObject)
+  }
+
+  private supportCSSPaintWorklet(globalObject: typeof globalThis) {
+    return typeof globalObject.CSS !== 'undefined' && 'paintWorklet' in globalObject.CSS
+  }
+
+  async activate(globalObject: typeof globalThis = globalThis) {
+    if (!this.isGlobalObjectIdentical(globalObject) && this.supportCSSPaintWorklet(globalObject)) {
       const workletURL = URL.createObjectURL(
         new Blob([smoothCornersScript], { type: 'application/javascript' }),
       )
 
-      // @ts-ignore
-      const promise = CSS.paintWorklet.addModule(workletURL) as Promise<void>
-
-      return promise
-        .then(() => {
-          this.activated = true
-          return this.activated
-        }).catch(() => {
-          this.activated = false
-          return this.activated
-        })
+      try {
+        await globalObject.CSS.paintWorklet.addModule(workletURL)
+        this.activated.set(globalObject, true)
+      } catch {
+        this.activated.set(globalObject, false)
+      }
     }
 
-    return this.activated
+    this.updateCurrentGlobalObject(globalObject)
+
+    return this.activated.get(globalObject) as boolean
   }
 }
 
