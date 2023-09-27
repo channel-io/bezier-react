604d224c366b73b8b10a4f27d1220b78802485ad
andrew <andrew@channel.io>
2023-09-27 15:35:19 +0900

    refactor(bezier-icons): make utils directory for utility function

--- packages/bezier-icons/scripts/add-pr-description.js
+++ packages/bezier-icons/scripts/add-pr-description.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-import { getDescription } from './getPrDescription'
+import { getDescription } from './utils/getPrDescription'
 
 const { exec } = require('child_process')
 
--- packages/bezier-icons/scripts/generate-icon-files.js
+++ packages/bezier-icons/scripts/generate-icon-files.js
@@ -1,6 +1,6 @@
 const path = require('path')
 
-const { generateIconFiles } = require('./generateIconFilesFromJson')
+const { generateIconFiles } = require('./utils/generateIconFilesFromJson')
 
 const bezierIconsDirectory = path.resolve(__dirname, '..')
 const iconsJson = path.resolve(bezierIconsDirectory, 'icons.json')
