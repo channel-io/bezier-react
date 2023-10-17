f3945b933d083f5de9bbb7e87e4dda1e61563591
Ed <sungik.dev@gmail.com>
2023-10-17 18:01:10 +0900

    Add build job with turbo cache to CI (#1615)
    <!--
      How to write a good PR title:
    - Follow [the Conventional Commits
    specification](https://www.conventionalcommits.org/en/v1.0.0/).
      - Give as much context as necessary and as little as possible
      - Prefix it with [WIP] while it’s a work in progress
    -->
    
    ## Self Checklist
    
    - [x] I wrote a PR title in **English** and added an appropriate
    **label** to the PR.
    - [x] I wrote the commit message in **English** and to follow [**the
    Conventional Commits
    specification**](https://www.conventionalcommits.org/en/v1.0.0/).
    - [x] I [added the
    **changeset**](https://github.com/changesets/changesets/blob/main/docs/adding-a-changeset.md)
    about the changes that needed to be released. (or didn't have to)
    - [x] I wrote or updated **documentation** related to the changes. (or
    didn't have to)
    - [x] I wrote or updated **tests** related to the changes. (or didn't
    have to)
    - [x] I tested the changes in various browsers. (or didn't have to)
      - Windows: Chrome, Edge, (Optional) Firefox
      - macOS: Chrome, Edge, Safari, (Optional) Firefox
    
    ## Related Issue
    <!-- Please link to issue if one exists -->
    
    Fixes #1005 
    
    ## Summary
    <!-- Please brief explanation of the changes made -->
    
    - 빌드 잡을 CI에 추가합니다.
    - 빌드 잡에 캐시를 추가합니다.
    
    ## Details
    <!-- Please elaborate description of the changes -->
    
    기존에 `--filter=bezier-icons` 를 통해 아이콘 패키지만 빌드하던 것에서 필터를 제거하고, 모든 패키지를
    빌드하도록 변경합니다. 의존성 업데이트 등 변경이 있었을 경우 라이브러리가 정상적으로 빌드되는 지 검증하기 위해서입니다.
    #1424 에서 빌드 시간이 많이 단축되었지만, 필터가 제거되었으므로 총 빌드 시간도 유의미하게 증가하게 될 거라 생각했습니다.
    이를 방지하고자 빌드 잡에 캐시를 추가했습니다.
    
    ### 빌드 잡에 캐시 추가
    
    - 기존엔 jest의 캐시가 없었습니다. 캐시 관련 설정을 추가합니다.
    - 마이너: 캐시 설정을 추가하며 jest 설정을 레포지토리 전반적으로 통일합니다. jest 관련 패키지를 루트로 옮기고,
    `@swc/jest` 를 모든 패키지에 적용하여 테스트 수행시간을 줄이고자 했습니다. 기본값과 동일하거나 불필요한 jest 설정
    옵션은 제거했습니다.
    - `test:ci` 스크립트를 제거하고 기존 `test: jest --onlyChanged` 스크립트를 `test:ci` 와
    동일한 스크립트로 변경합니다. 기존에 only changed 플래그가 추가되었던 이유가 pre-commit 훅을 빠르게 수행하기
    위해서였는데, pre-commit 훅이 제거되었기때문에 불필요해졌다고 판단했습니다.
    
    ### Breaking change? (Yes/No)
    <!-- If Yes, please describe the impact and migration path for users -->
    
    No

--- packages/bezier-react/jest.config.js
+++ packages/bezier-react/jest.config.js
@@ -1,18 +1,14 @@
 /** @type {import('jest').Config} */
 module.exports = {
-  roots: ['./src'],
+  cacheDirectory: '.jestcache',
   testEnvironment: 'jsdom',
+  roots: ['./src'],
   setupFilesAfterEnv: ['./jest.setup.ts'],
-  moduleFileExtensions: ['ts', 'tsx', 'js'],
   transformIgnorePatterns: [],
   transform: {
     '^.+\\.[t|j]sx?$': ['@swc/jest'],
   },
   testMatch: ['**/*.test.(ts|tsx)'],
-  moduleDirectories: [
-    'node_modules',
-    'src',
-  ],
   moduleNameMapper: {
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
