6708abf56d429c2d7afb992db924587b3ac96e37
Ed Sungik Choi <sungik.dev@gmail.com>
2023-11-16 20:18:39 +0900

    refactor(utils): implement css utils

---
+++ packages/bezier-react/src/utils/css.ts
@@ -0,0 +1,20 @@
+export const px = <Value extends number | undefined>(value: Value) => (value ? `${value}px` as const : undefined)
+
+export const cssVarName = <ComponentName extends string>(componentName: ComponentName) => <PropertyName extends string>(propertyName: PropertyName) => `--bezier-${componentName}-${propertyName}` as const
+
+export function cssVarValue<
+  PropertyName extends string | undefined,
+  DeclarationValue extends string | number | undefined,
+>(propertyName: PropertyName, declarationValue?: DeclarationValue) {
+  /* eslint-disable no-nested-ternary */
+  return propertyName
+    ? declarationValue
+      ? `var(--${propertyName}, ${declarationValue})` as const
+      : `var(--${propertyName})` as const
+    : undefined
+  /* eslint-enable no-nested-ternary */
+}
+
+export function cssUrl(url: string | undefined) {
+  return url ? `url(${url})` : undefined
+}
