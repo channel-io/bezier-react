581e07674b8fd7afdeec8f242a8bcf6662f2c716
Ed Sungik Choi <sungik.dev@gmail.com>
2023-12-01 16:24:07 +0900

    fix(tokens/transform): change using the $type property

--- packages/bezier-tokens/scripts/lib/transform.ts
+++ packages/bezier-tokens/scripts/lib/transform.ts
@@ -12,7 +12,7 @@ export const customFontRem: CustomTransform = {
   type: 'value',
   transitive: true,
   matcher: ({ attributes: { category, type } = {} }) =>
-    category === 'font' && (type === 'size' || type === 'lineHeight'),
+    category === 'font' && (type === 'size' || type === 'line-height'),
   transformer: ({ value }: { value: string }, options) =>
     `${parseFloat(extractNumber(value) ?? '') / ((options && options.basePxFontSize) || 16)}rem`,
 }
@@ -21,8 +21,7 @@ export const customFontFamily: CustomTransform = {
   name: 'custom/font/family',
   type: 'value',
   transitive: true,
-  matcher: ({ attributes: { category, type } = {} }) =>
-    category === 'font' && type === 'family',
+  matcher: ({ original: { $type } }) => $type === 'fontFamily',
   transformer: ({ value }: { value: string[] }) =>
     /**
      * @see {@link https://stackoverflow.com/questions/13751412/why-would-font-names-need-quotes}
@@ -34,8 +33,7 @@ export const customCubicBezier: CustomTransform = {
   name: 'custom/cubicBezier',
   type: 'value',
   transitive: true,
-  matcher: ({ attributes: { category } = {} }) =>
-    category === 'cubicBezier',
+  matcher: ({ original: { $type } }) => $type === 'cubicBezier',
   transformer: ({ value: [x1, y1, x2, y2] }: { value: [number, number, number, number] }) =>
     `cubic-bezier(${x1}, ${y1}, ${x2}, ${y2})`,
 }
