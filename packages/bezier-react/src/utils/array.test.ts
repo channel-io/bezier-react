1db1a0fd2769620ae70e923b9912f7e559cdf146
김석진 <sj01048260279@gmail.com>
2023-12-04 20:23:31 +0900

    feat(bezier-react): add test case to array

--- packages/bezier-react/src/utils/array.test.ts
+++ packages/bezier-react/src/utils/array.test.ts
@@ -1,20 +1,37 @@
 import {
   compact,
   flattenDeep,
+  isLastArrayIndex,
 } from './array'
 
-describe('arrayUtils', () => {
-  describe('compact', () => {
-    it('should remove falsy item', () => {
-      const array = [0, 1, false, 2, '', 3]
-      expect(compact(array)).toEqual([1, 2, 3])
-    })
+describe('isLastArrayIndex', () => {
+  it('should return true when the last array index', () => {
+    const arr = [0, 1, 2, 3, 4]
+
+    const result = isLastArrayIndex(arr, 4)
+
+    expect(result).toBe(true)
   })
 
-  describe('flattenDeep', () => {
-    it('should return flatten array', () => {
-      const array = [1, [2, [3, [4]], 5]]
-      expect(flattenDeep(array)).toEqual([1, 2, 3, 4, 5])
-    })
+  it('should return false when not the last array index', () => {
+    const arr = [0, 1, 2, 3, 4]
+
+    const result = isLastArrayIndex(arr, 1)
+
+    expect(result).toBe(false)
+  })
+})
+
+describe('compact', () => {
+  it('should remove falsy item', () => {
+    const array = [0, 1, false, 2, '', 3]
+    expect(compact(array)).toEqual([1, 2, 3])
+  })
+})
+
+describe('flattenDeep', () => {
+  it('should return flatten array', () => {
+    const array = [1, [2, [3, [4]], 5]]
+    expect(flattenDeep(array)).toEqual([1, 2, 3, 4, 5])
   })
 })
