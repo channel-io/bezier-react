afabf3eb8e2a7f1ae0c78974030db421bf9b8266
김석진 <102217654+SEOKKAMONI@users.noreply.github.com>
2023-12-05 16:26:49 +0900

    Update packages/bezier-react/src/hooks/useIsMounted.test.ts
    Co-authored-by: Yang Wooseong (Andrew) <dntjddid1@gmail.com>

--- packages/bezier-react/src/hooks/useIsMounted.test.ts
+++ packages/bezier-react/src/hooks/useIsMounted.test.ts
@@ -3,7 +3,7 @@ import { renderHook } from '@testing-library/react'
 import useIsMounted from './useIsMounted'
 
 describe('useIsMounted', () => {
-  it('should return when mounted', () => {
+  it('should return true when mounted', () => {
     const { result } = renderHook(() => useIsMounted())
 
     expect(result.current).toBe(true)
