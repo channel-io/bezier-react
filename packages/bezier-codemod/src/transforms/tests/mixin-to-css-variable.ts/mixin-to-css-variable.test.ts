29b0e14842e9772723687b59b9c6c6dd5c57ed19
andrew <andrew@channel.io>
2023-12-13 11:09:59 +0900

    chore(bezier-codemod): typo fix

--- packages/bezier-codemod/src/transforms/tests/mixin-to-css-variable.ts/mixin-to-css-variable.test.ts
+++ packages/bezier-codemod/src/transforms/tests/mixin-to-css-variable.ts/mixin-to-css-variable.test.ts
@@ -2,7 +2,7 @@ import { testTransformFunction } from '../../../utils/test.js'
 import mixinTransform from '../../mixin-to-css-variable.js'
 
 describe('mixin transform', () => {
-  it('should transform foundation to css variable', () => {
+  it('should transform mixin interpolation to css variable', () => {
     testTransformFunction(__dirname, 'mixin1', mixinTransform)
   })
 })
