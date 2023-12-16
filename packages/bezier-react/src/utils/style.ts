79d20428d55a48f273f777bc2837a3d4bf60de52
Ed Sungik Choi <sungik.dev@gmail.com>
2023-12-16 17:03:06 +0900

    feat(style): enhance to not attach px unit to 0

--- packages/bezier-react/src/utils/style.ts
+++ packages/bezier-react/src/utils/style.ts
@@ -41,9 +41,13 @@ export function touchableHover(interpolation: InjectedInterpolation): InjectedIn
 }
 
 export function px<Value extends number>(value?: Value) {
-  return isNil(value)
-    ? undefined
-    : `${value}px` as const
+  if (isNil(value)) {
+    return undefined
+  }
+  if (value !== 0) {
+    return `${value}px` as const
+  }
+  return value as 0
 }
 
 export function cssDimension<Value extends number | string>(value?: Value) {
