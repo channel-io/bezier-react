a8bd96fd37b95aec747d18720a28fd38950409a3
Ed Sungik Choi <sungik.dev@gmail.com>
2023-12-19 11:57:00 +0900

    feat: migrating type changes in text props

--- packages/bezier-react/src/components/Forms/FormLabel/FormLabel.types.ts
+++ packages/bezier-react/src/components/Forms/FormLabel/FormLabel.types.ts
@@ -14,6 +14,6 @@ interface FormLabelOptions {
 export default interface FormLabelProps extends
   BezierComponentProps,
   ChildrenProps,
-  TextProps,
+  Omit<TextProps, 'as'>,
   Partial<IdentifierProps>,
   FormLabelOptions {}
