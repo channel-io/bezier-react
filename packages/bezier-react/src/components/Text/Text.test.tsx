bdb2c038ba8a2a27df915c26df67e513c7f1481c
Ed Sungik Choi <sungik.dev@gmail.com>
2023-12-19 11:10:31 +0900

    test(text): update test cases

--- packages/bezier-react/src/components/Text/Text.test.tsx
+++ packages/bezier-react/src/components/Text/Text.test.tsx
@@ -1,75 +1,64 @@
 import React from 'react'
 
-import { Typography } from '~/src/foundation'
-
 import { render } from '~/src/utils/test'
 
-import Text, { TEXT_TEST_ID } from './Text'
-import type TextProps from './Text.types'
+import { Text } from './Text'
+import { type TextProps } from './Text.types'
 
-describe('Text test >', () => {
+const TEXT = 'Hello, Channel!'
+
+describe('Text', () => {
   let props: TextProps
 
   beforeEach(() => {
     props = {
-      children: 'hello, world',
+      children: TEXT,
     }
   })
 
   const renderComponent = (optionProps?: TextProps) => render(<Text {...props} {...optionProps} />)
 
-  it('Text have default style', () => {
-    const { getByTestId } = renderComponent()
-
-    const renderedText = getByTestId(TEXT_TEST_ID)
-
-    expect(renderedText).toHaveStyle('font-size: 1.5rem;')
-    expect(renderedText).toHaveStyle('font-weight: normal;')
-    expect(renderedText).toHaveStyle('font-style: normal;')
-    expect(renderedText).toHaveStyle('color: inherit;')
+  it('should have default style', () => {
+    const { getByText } = renderComponent()
+    const rendered = getByText(TEXT)
+    expect(rendered).toHaveStyle('color: var(--b-text-color, inherit);')
+    expect(rendered).toHaveStyle('font-style: normal;')
+    expect(rendered).toHaveStyle('font-weight: var(--font-weight-400);')
+    expect(rendered).toHaveStyle('transition: color var(--transition-s);')
   })
 
-  it('Text receives bold style', () => {
-    const { getByTestId } = renderComponent({ bold: true })
-
-    const renderedText = getByTestId(TEXT_TEST_ID)
-
-    expect(renderedText).toHaveStyle('font-weight: bold;')
+  it('should receives bold style', () => {
+    const { getByText } = renderComponent({ bold: true })
+    const rendered = getByText(TEXT)
+    expect(rendered).toHaveStyle('font-weight: var(--font-weight-700);')
   })
 
-  it('Text receives italic style', () => {
-    const { getByTestId } = renderComponent({ italic: true })
-
-    const renderedText = getByTestId(TEXT_TEST_ID)
-
-    expect(renderedText).toHaveStyle('font-style: italic;')
+  it('should receives italic style', () => {
+    const { getByText } = renderComponent({ italic: true })
+    const rendered = getByText(TEXT)
+    expect(rendered).toHaveStyle('font-style: italic;')
   })
 
-  it('Text receives truncated style', () => {
-    const { getByTestId } = renderComponent({ truncated: true })
-
-    const renderedText = getByTestId(TEXT_TEST_ID)
-
-    expect(renderedText).toHaveStyle('display: block;')
-    expect(renderedText).toHaveStyle('overflow: hidden;')
-    expect(renderedText).toHaveStyle('text-overflow: ellipsis;')
-    expect(renderedText).toHaveStyle('white-space: nowrap;')
+  it('should receives truncated style', () => {
+    const { getByText } = renderComponent({ truncated: true })
+    const rendered = getByText(TEXT)
+    expect(rendered).toHaveStyle('display: block;')
+    expect(rendered).toHaveStyle('overflow: hidden;')
+    expect(rendered).toHaveStyle('text-overflow: ellipsis;')
+    expect(rendered).toHaveStyle('white-space: nowrap;')
   })
 
-  it('Text receives style object', () => {
-    const { getByTestId } = renderComponent({ style: { color: 'skyblue' } })
-
-    const renderedText = getByTestId(TEXT_TEST_ID)
-
-    expect(renderedText).toHaveStyle('color: skyblue;')
+  it('should receives style object', () => {
+    const { getByText } = renderComponent({ style: { color: 'skyblue' } })
+    const rendered = getByText(TEXT)
+    expect(rendered).toHaveStyle('color: skyblue;')
   })
 
-  it('Text receives typo from styled component css', () => {
-    const { getByTestId } = renderComponent({ typo: Typography.Size24 })
-
-    const renderedText = getByTestId(TEXT_TEST_ID)
-
-    expect(renderedText).toHaveStyle('font-size: 2.4rem;')
-    expect(renderedText).toHaveStyle('line-height: 3.2rem;')
+  it('should receives typo', () => {
+    const { getByText } = renderComponent({ typo: '24' })
+    const rendered = getByText(TEXT)
+    expect(rendered).toHaveStyle('font-size: var(--typography-size-24-font-size);')
+    expect(rendered).toHaveStyle('line-height: var(--typography-size-24-line-height);')
+    expect(rendered).toHaveStyle('letter-spacing: var(--typography-size-24-letter-spacing);')
   })
 })
