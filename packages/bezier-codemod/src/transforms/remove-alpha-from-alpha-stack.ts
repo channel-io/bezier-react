3ad654db9dcdd7538dfc69960a5f7ff84ddf0b40
andrew <andrew@channel.io>
2023-12-19 15:44:05 +0900

    chore(bezier-codemod): add new line

--- packages/bezier-codemod/src/transforms/remove-alpha-from-alpha-stack.ts
+++ packages/bezier-codemod/src/transforms/remove-alpha-from-alpha-stack.ts
@@ -3,33 +3,12 @@ import {
   SyntaxKind,
 } from 'ts-morph'
 
-import {
-  getNamedImport,
-  hasNamedImport,
-} from '../utils/import.js'
+import { renameNamedImport } from '../utils/import.js'
 
 const LEGACY_STACKS = ['Stack', 'HStack', 'VStack', 'StackItem', 'Spacer']
 const ALPHA_STACKS = ['AlphaStack']
 const getNameWithLegacyPrefix = (name: string) => `Legacy${name}`
 const getNameWithoutAlphaPrefix = (name: string) => name.slice('ALPHA'.length)
-
-const transformInImport = (sourceFile: SourceFile, stacks: string[], convertFn: (name: string) => string) => {
-  stacks.forEach((stack) => {
-    if (hasNamedImport(sourceFile, stack)) {
-      const importSpecifier = getNamedImport(sourceFile, stack)
-      const alias = importSpecifier?.getAliasNode()
-      if (alias) {
-        importSpecifier?.replaceWithText(`${convertFn(stack)} as ${alias.getText()}`)
-      } else {
-        importSpecifier?.replaceWithText(convertFn(stack))
-        sourceFile
-          .getDescendantsOfKind(SyntaxKind.Identifier)
-          .filter((v) => v.getText() === stack)
-          .forEach((v) => v.replaceWithText(convertFn(stack)))
-      }
-    }
-  })
-}
 
 const transformInJsx = (sourceFile: SourceFile, stacks: string[], convertFn: (name: string) => string) => {
   for (const syntaxKind of [SyntaxKind.JsxOpeningElement, SyntaxKind.JsxClosingElement, SyntaxKind.JsxSelfClosingElement] as const) {
@@ -45,12 +24,12 @@ const transformInJsx = (sourceFile: SourceFile, stacks: string[], convertFn: (na
 }
 
 const transformLegacyStack = (sourceFile: SourceFile) => {
-  transformInImport(sourceFile, LEGACY_STACKS, getNameWithLegacyPrefix)
+  renameNamedImport(sourceFile, LEGACY_STACKS, getNameWithLegacyPrefix)
   transformInJsx(sourceFile, LEGACY_STACKS, getNameWithLegacyPrefix)
 }
 
 const transformAlphaStack = (sourceFile: SourceFile) => {
-  transformInImport(sourceFile, ALPHA_STACKS, getNameWithoutAlphaPrefix)
+  renameNamedImport(sourceFile, ALPHA_STACKS, getNameWithoutAlphaPrefix)
   transformInJsx(sourceFile, ALPHA_STACKS, getNameWithoutAlphaPrefix)
 }
 
--- packages/bezier-codemod/src/utils/import.ts
+++ packages/bezier-codemod/src/utils/import.ts
@@ -35,3 +35,21 @@ export const removeUnusedNamedImport = (sourceFile: SourceFile) => {
     .filter((v) => (sourceFile.getDescendantsOfKind(SyntaxKind.Identifier).filter((_v) => _v.getText() === v.getText()).length === 1))
     .forEach((v) => v.remove())
 }
+
+export const renameNamedImport = (sourceFile: SourceFile, targets: string[], renameFn: (name: string) => string) => {
+  targets.forEach((target) => {
+    if (hasNamedImport(sourceFile, target)) {
+      const importSpecifier = getNamedImport(sourceFile, target)
+      const alias = importSpecifier?.getAliasNode()
+      if (alias) {
+        importSpecifier?.replaceWithText(`${renameFn(target)} as ${alias.getText()}`)
+      } else {
+        importSpecifier?.replaceWithText(renameFn(target))
+        sourceFile
+          .getDescendantsOfKind(SyntaxKind.Identifier)
+          .filter((v) => v.getText() === target)
+          .forEach((v) => v.replaceWithText(renameFn(target)))
+      }
+    }
+  })
+}
