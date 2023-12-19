4759f1104e03a49ebcefca967ee9e28a160c272c
andrew <andrew@channel.io>
2023-12-19 15:07:48 +0900

    refactor(bezier-codemod): change comment style from // to /* */

--- packages/bezier-codemod/src/transforms/tests/mixin-to-css-variable.ts/fixtures/interpolation1.output.tsx
+++ packages/bezier-codemod/src/transforms/tests/mixin-to-css-variable.ts/fixtures/interpolation1.output.tsx
@@ -32,13 +32,13 @@ const Wrapper = styled.div`
 `
 
 const Wrapper = styled.div`
-  // NOTE: Do not use font-related css variables below separately, use Text component instead
+  /* NOTE: Do not use font-related css variables below separately, use Text component instead */
   font-size: var(--typography-size-11-font-size);
   line-height: var(--typography-size-11-line-height);
 `
 
 const Wrapper = styled.div`
-  // NOTE: Do not use font-related css variables below separately, use Text component instead
+  /* NOTE: Do not use font-related css variables below separately, use Text component instead */
   font-size: var(--typography-size-17-font-size);
   line-height: var(--typography-size-17-line-height);
   letter-spacing: var(--typography-size-17-letter-spacing);
