16c3f47da0f014afd77f72c02ead70e5380fd9e3
Ed Sungik Choi <sungik.dev@gmail.com>
2023-12-20 14:13:29 +0900

    refactor(form-group): remove wrap from prop type

--- packages/bezier-react/src/components/Forms/FormGroup/FormGroup.types.ts
+++ packages/bezier-react/src/components/Forms/FormGroup/FormGroup.types.ts
@@ -8,7 +8,7 @@ import type { StackProps } from '~/src/components/Stack'
 interface FormGroupProps extends
   AlphaBezierComponentProps,
   ChildrenProps,
-  Partial<Pick<StackProps, 'direction' | 'spacing' | 'wrap'>>,
+  Partial<Pick<StackProps, 'direction' | 'spacing'>>,
   React.HTMLAttributes<HTMLDivElement> {}
 
 export default FormGroupProps
