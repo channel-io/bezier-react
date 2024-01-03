a6f5c1a2069f1b93b7992f2723314a7476c16030
Yang Wooseong (Andrew) <dntjddid1@gmail.com>
2024-01-03 11:59:46 +0900

    Use `:where` pseudo class to `Text`, `Stack` components (#1869)
    <!--
      How to write a good PR title:
    - Follow [the Conventional Commits
    specification](https://www.conventionalcommits.org/en/v1.0.0/).
      - Give as much context as necessary and as little as possible
      - Prefix it with [WIP] while it’s a work in progress
    -->
    
    ## Self Checklist
    
    - [x] I wrote a PR title in **English** and added an appropriate
    **label** to the PR.
    - [x] I wrote the commit message in **English** and to follow [**the
    Conventional Commits
    specification**](https://www.conventionalcommits.org/en/v1.0.0/).
    - [x] I [added the
    **changeset**](https://github.com/changesets/changesets/blob/main/docs/adding-a-changeset.md)
    about the changes that needed to be released. (or didn't have to)
    - [x] I wrote or updated **documentation** related to the changes. (or
    didn't have to)
    - [x] I wrote or updated **tests** related to the changes. (or didn't
    have to)
    - [x] I tested the changes in various browsers. (or didn't have to)
      - Windows: Chrome, Edge, (Optional) Firefox
      - macOS: Chrome, Edge, Safari, (Optional) Firefox
    
    ## Related Issue
    <!-- Please link to issue if one exists -->
    
    - https://github.com/channel-io/bezier-react/issues/1865
    
    ## Summary
    <!-- Please brief explanation of the changes made -->
    
    - `Text`, `Stack` 컴포넌트 스타일링의 css 우선순위를 낮추기 위해 :where 셀렉터를 사용합니다.
    - 컴포넌트 스토리북에 args 를 추가합니다. 
    
    ### Breaking change? (Yes/No)
    <!-- If Yes, please describe the impact and migration path for users -->
    
    - No

--- packages/bezier-react/src/components/Stack/Stack.stories.tsx
+++ packages/bezier-react/src/components/Stack/Stack.stories.tsx
@@ -9,6 +9,7 @@ import {
 import { range } from '~/src/utils/number'
 
 import { Box } from '~/src/components/Box'
+import { Text } from '~/src/components/Text'
 
 import { Stack } from './Stack'
 
@@ -18,7 +19,7 @@ const meta = {
 
 type Story = StoryObj<typeof meta>
 
-function DecorativeBox() {
+function DecorativeBox({ children }) {
   return (
     <Box
       width={50}
@@ -27,14 +28,22 @@ function DecorativeBox() {
       borderRadius="8"
       borderWidth={1}
       borderColor="bdr-black-light"
-    />
+    >
+      <Text>
+        { children }
+      </Text>
+    </Box>
   )
 }
 
 const Template: StoryFn<typeof Stack> = (args) => (
-  <Stack {...args}>
+  <Stack
+    {...args}
+    borderColor="bdr-black-dark"
+    borderWidth={1}
+  >
     { range(4).map((i) => (
-      <DecorativeBox key={`item-${i}`} />
+      <DecorativeBox key={`item-${i}`}>{ i + 1 }</DecorativeBox>
     )) }
   </Stack>
 )
@@ -43,6 +52,12 @@ export const Primary: Story = {
   render: Template,
   args: {
     direction: 'horizontal',
+    justify: 'start',
+    align: 'start',
+    reverse: false,
+    wrap: true,
+    width: 300,
+    height: 300,
     spacing: 6,
   },
 }
