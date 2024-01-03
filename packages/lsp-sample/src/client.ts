ff0657571afcee56e21aaf15adf64407d91018c7
andrew <andrew@channel.io>
2024-01-03 23:01:52 +0900

    feat(vs-code): activate server log - https://stackoverflow.com/questions/77346575/vs-code-language-server-extension-guide-lacks-attach-launch-configuration

--- packages/lsp-sample/src/client.ts
+++ packages/lsp-sample/src/client.ts
@@ -21,6 +21,10 @@ export function activate(context: ExtensionContext) {
 		path.join('out', 'server.js')
 	);
 
+	// The debug options for the server
+	// --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
+	const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+
 	// If the extension is launched in debug mode then the debug server options are used
 	// Otherwise the run options are used
 	const serverOptions: ServerOptions = {
@@ -28,6 +32,7 @@ export function activate(context: ExtensionContext) {
 		debug: {
 			module: serverModule,
 			transport: TransportKind.ipc,
+			options: debugOptions,
 		}
 	};
 
