9ad710062e5e1188a3c73492cfcdcd9c7c770e67
andrew <andrew@channel.io>
2024-01-04 14:58:56 +0900

    feat(badge): use classname for static styling

--- packages/bezier-react/src/components/TagBadge/Badge/Badge.tsx
+++ packages/bezier-react/src/components/TagBadge/Badge/Badge.tsx
@@ -10,7 +10,6 @@ import {
   getMarginStyle,
   splitByMarginProps,
 } from '~/src/utils/props'
-import { cssVar } from '~/src/utils/style'
 import { isEmpty } from '~/src/utils/type'
 
 import { Icon } from '~/src/components/Icon'
@@ -18,14 +17,14 @@ import {
   TAG_BADGE_ICON_SIZE,
   TagBadgeSize,
   TagBadgeVariant,
-  getProperBadgeTextColor,
-  getProperTagBadgeBgColor,
   getProperTagBadgeTypo,
 } from '~/src/components/TagBadge/TagBadgeCommon'
 import commonStyles from '~/src/components/TagBadge/TagBadgeCommon/TagBadge.module.scss'
 import { Text } from '~/src/components/Text'
 
 import type BadgeProps from './Badge.types'
+
+import styles from './Badge.module.scss'
 
 export const BADGE_TEST_ID = 'bezier-react-badge'
 
@@ -57,38 +56,33 @@ export const Badge = memo(forwardRef<HTMLDivElement, BadgeProps>(function Badge(
     ...rest
   } = marginRest
 
-  const bgColor = getProperTagBadgeBgColor(variant)
-  const textColor = getProperBadgeTextColor(variant)
-
   return (
     <div
       style={{
         ...style,
         ...marginStyle.style,
-        '--b-tag-badge-background-color': cssVar(bgColor),
       } as CSSProperties}
       ref={forwardedRef}
       className={classNames(
+        styles.Badge,
+        styles[`variant-${variant}`],
         commonStyles.TagBadge,
         commonStyles[`size-${size}`],
         marginStyle.className,
         className,
       )}
       data-testid={testId}
-      color={textColor}
       {...rest}
     >
       { icon && (
         <Icon
           source={icon}
           size={TAG_BADGE_ICON_SIZE}
-          color={textColor}
         />
       ) }
 
       { !isEmpty(children) && (
         <Text
-          color={textColor}
           typo={getProperTagBadgeTypo(size)}
           mx={3}
         >
