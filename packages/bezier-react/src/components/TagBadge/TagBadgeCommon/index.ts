7807cfb43d0ca97940ee1a085a29963b509da7b9
andrew <andrew@channel.io>
2024-01-04 15:36:28 +0900

    fix(tag): resolve typecheck error

--- packages/bezier-react/src/components/TagBadge/TagBadgeCommon/index.ts
+++ packages/bezier-react/src/components/TagBadge/TagBadgeCommon/index.ts
@@ -11,7 +11,6 @@ export {
 
 /* Utils */
 export {
-  getProperBadgeTextColor,
   getProperTagBadgeBgColor,
   getProperTagBadgeTypo,
 } from './utils'
--- packages/bezier-react/src/components/TagBadge/TagBadgeCommon/utils.ts
+++ packages/bezier-react/src/components/TagBadge/TagBadgeCommon/utils.ts
@@ -2,10 +2,7 @@ import { type SemanticNames } from '~/src/foundation'
 
 import TagBadgeSize from './constants/TagBadgeSize'
 import type TagBadgeVariant from './constants/TagBadgeVariant'
-import {
-  BadgeColorPreset,
-  TagBadgeBgColorPreset,
-} from './constants/TagBadgeVariant'
+import { TagBadgeBgColorPreset } from './constants/TagBadgeVariant'
 
 export function getProperTagBadgeTypo(size: TagBadgeSize) {
   return {
@@ -18,8 +15,4 @@ export function getProperTagBadgeTypo(size: TagBadgeSize) {
 
 export function getProperTagBadgeBgColor(color: TagBadgeVariant): SemanticNames {
   return TagBadgeBgColorPreset[color]
-}
-
-export function getProperBadgeTextColor(color: TagBadgeVariant): SemanticNames {
-  return BadgeColorPreset[color]
 }
