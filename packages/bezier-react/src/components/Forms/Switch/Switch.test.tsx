e8dc6ac537dbceff2d5d0ba4296e37f5b95838c8
andrew <andrew@channel.io>
2024-01-08 20:39:02 +0900

    feat(switch): remove unnecessary test code related to css style

--- packages/bezier-react/src/components/Forms/Switch/Switch.test.tsx
+++ packages/bezier-react/src/components/Forms/Switch/Switch.test.tsx
@@ -2,120 +2,18 @@ import React from 'react'
 
 import userEvent from '@testing-library/user-event'
 
-import { LightFoundation } from '~/src/foundation'
-
 import { render } from '~/src/utils/test'
 
 import {
-  SWITCH_HANDLE_TEST_ID,
   SWITCH_TEST_ID,
   Switch,
 } from './Switch'
 import type SwitchProps from './Switch.types'
-import { SwitchSize } from './Switch.types'
 
 describe('Switch', () => {
   const renderComponent = (props?: Partial<SwitchProps>) => render(
     <Switch {...props} />,
   )
-
-  describe('no props specified', () => {
-    it('should render default Switch', () => {
-      const { getByTestId } = renderComponent()
-      const switchComponent = getByTestId(SWITCH_TEST_ID)
-      const switchHandleComponent = getByTestId(SWITCH_HANDLE_TEST_ID)
-
-      expect(switchComponent).toHaveStyle('position: relative')
-      expect(switchComponent).toHaveStyle('width: 36px')
-      expect(switchComponent).toHaveStyle('height: 24px')
-      expect(switchComponent).toHaveStyle('cursor: pointer')
-      expect(switchComponent).toHaveStyle(`background-color: ${LightFoundation.theme['bg-black-dark']}`)
-      expect(switchComponent).toHaveStyle('border-radius: 12px')
-      expect(switchComponent).toHaveStyle('opacity: initial')
-      expect(switchHandleComponent).toHaveStyle('position: absolute')
-      expect(switchHandleComponent).toHaveStyle('top: 3px')
-      expect(switchHandleComponent).toHaveStyle('left: 3px')
-      expect(switchHandleComponent).toHaveStyle('width: 18px')
-      expect(switchHandleComponent).toHaveStyle('height: 18px')
-      expect(switchHandleComponent).toHaveStyle(`background-color: ${LightFoundation.theme['bgtxt-absolute-white-dark']}`)
-      expect(switchHandleComponent).toHaveStyle('border-radius: 12px')
-      expect(switchHandleComponent).toHaveStyle('transform: initial')
-    })
-  })
-
-  describe('specify size props', () => {
-    it('should render Switch with size M', () => {
-      const { getByTestId } = renderComponent({
-        size: SwitchSize.M,
-      })
-      const switchComponent = getByTestId(SWITCH_TEST_ID)
-      const switchHandleComponent = getByTestId(SWITCH_HANDLE_TEST_ID)
-
-      expect(switchComponent).toHaveStyle('width: 36px')
-      expect(switchComponent).toHaveStyle('height: 24px')
-      expect(switchHandleComponent).toHaveStyle('width: 18px')
-      expect(switchHandleComponent).toHaveStyle('height: 18px')
-    })
-
-    it('should render Switch with size S', () => {
-      const { getByTestId } = renderComponent({
-        size: SwitchSize.S,
-      })
-      const switchComponent = getByTestId(SWITCH_TEST_ID)
-      const switchHandleComponent = getByTestId(SWITCH_HANDLE_TEST_ID)
-
-      expect(switchComponent).toHaveStyle('width: 30px')
-      expect(switchComponent).toHaveStyle('height: 20px')
-      expect(switchHandleComponent).toHaveStyle('width: 14px')
-      expect(switchHandleComponent).toHaveStyle('height: 14px')
-    })
-  })
-
-  describe('specify checked props', () => {
-    it('should render Switch with non-checked state when checked is false', () => {
-      const { getByTestId } = renderComponent({
-        checked: false,
-      })
-      const switchComponent = getByTestId(SWITCH_TEST_ID)
-      const switchHandleComponent = getByTestId(SWITCH_HANDLE_TEST_ID)
-
-      expect(switchComponent).toHaveStyle(`background-color: ${LightFoundation.theme['bg-black-dark']}`)
-      expect(switchHandleComponent).toHaveStyle('transform: initial')
-    })
-
-    it('should render Switch with checked state when checked is true', () => {
-      const { getByTestId } = renderComponent({
-        checked: true,
-      })
-      const switchComponent = getByTestId(SWITCH_TEST_ID)
-      const switchHandleComponent = getByTestId(SWITCH_HANDLE_TEST_ID)
-
-      expect(switchComponent).toHaveStyle(`background-color: ${LightFoundation.theme['bgtxt-green-normal']}`)
-      expect(switchHandleComponent).toHaveStyle('transform: translateX(12px)')
-    })
-  })
-
-  describe('specify disabled props', () => {
-    it('should render default Switch when disabled is false', () => {
-      const { getByTestId } = renderComponent({
-        disabled: false,
-      })
-      const switchComponent = getByTestId(SWITCH_TEST_ID)
-
-      expect(switchComponent).toHaveStyle('opacity: initial')
-      expect(switchComponent).toHaveStyle('cursor: pointer')
-    })
-
-    it('should render disabled Switch when disabled is true', () => {
-      const { getByTestId } = renderComponent({
-        disabled: true,
-      })
-      const switchComponent = getByTestId(SWITCH_TEST_ID)
-
-      expect(switchComponent).toHaveStyle('opacity: var(--opacity-disabled)')
-      expect(switchComponent).toHaveStyle('cursor: not-allowed')
-    })
-  })
 
   describe('data attribute', () => {
     describe('data-state', () => {
