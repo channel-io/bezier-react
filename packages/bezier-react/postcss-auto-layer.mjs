564ee19c06b54208390faaf8f79fcd44312c66e7
Ed Sungik Choi <sungik.dev@gmail.com>
2024-01-09 01:55:43 +0900

    build(postcss): implement auto layer plugin

---
+++ packages/bezier-react/postcss-auto-layer.mjs
@@ -0,0 +1,20 @@
+import { minimatch } from 'minimatch'
+
+/**
+ * @type {import('postcss').PluginCreator<{ name: string, path: string }>}
+ */
+const postcssAutoLayer = (opts = {}) => ({
+  postcssPlugin: 'postcss-auto-layer',
+  Once(root, { result, AtRule }) {
+    const filePath = result.opts.from
+    if (filePath && minimatch(filePath, opts.path)) {
+      const layer = new AtRule({ name: 'layer', params: opts.name })
+      root.nodes.forEach(node => { layer.append(node) })
+      root.removeAll().append(layer)
+    }
+  },
+})
+
+postcssAutoLayer.postcss = true
+
+export default postcssAutoLayer
