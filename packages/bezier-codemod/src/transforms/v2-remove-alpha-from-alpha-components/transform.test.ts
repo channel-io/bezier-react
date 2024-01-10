06d9a4888d4af4043b6011a4ceaed569c4884ba1
andrew <andrew@channel.io>
2024-01-10 11:53:50 +0900

    feat(code-mod): rename fixture files

--- packages/bezier-codemod/src/transforms/v2-remove-alpha-from-alpha-components/transform.test.ts
+++ packages/bezier-codemod/src/transforms/v2-remove-alpha-from-alpha-components/transform.test.ts
@@ -4,15 +4,15 @@ import stackTransform from './transform.js'
 
 describe('import transform', () => {
   it('should add legacy prefix to components to be deprecated in tsx file', () => {
-    testTransformFunction(__dirname, 'stack1', stackTransform)
+    testTransformFunction(__dirname, 'legacy1', stackTransform)
   })
 
   it('should add legacy prefix to components to be deprecated in styled component file', () => {
-    testTransformFunction(__dirname, 'stack2', stackTransform)
+    testTransformFunction(__dirname, 'legacy2', stackTransform)
   })
 
   it('should remove alpha prefix from Alpha prefixed components in tsx file', () => {
-    testTransformFunction(__dirname, 'stack3', stackTransform)
+    testTransformFunction(__dirname, 'alpha3', stackTransform)
   })
 
   it('should remove alpha prefix from Alpha prefixed components in styled component file', () => {
