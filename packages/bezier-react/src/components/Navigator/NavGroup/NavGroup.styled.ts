409f8ae6b746b6fc0f76984ffa3c7c2e13757e69
andrew <andrew@channel.io>
2024-01-12 13:41:00 +0900

    feat(nav-group): add width: 100% to common style

--- packages/bezier-react/src/components/Navigator/NavGroup/NavGroup.styled.ts
+++ packages/bezier-react/src/components/Navigator/NavGroup/NavGroup.styled.ts
@@ -1,13 +1,4 @@
-import {
-  css,
-  styled,
-} from '~/src/foundation'
-
-import type { InterpolationProps } from '~/src/types/Foundation'
-
-interface WrapperProps extends InterpolationProps {
-  active: boolean
-}
+import { styled } from '~/src/foundation'
 
 export const ChevronWrapper = styled.div`
   display: flex;
@@ -16,45 +7,6 @@ export const ChevronWrapper = styled.div`
   min-width: 20px;
   max-width: 20px;
   margin-left: 2px;
-`
-
-const activeItemStyle = css`
-  color: ${({ foundation }) => foundation?.theme?.['bgtxt-blue-normal']};
-  background-color: ${({ foundation }) => foundation?.theme?.['bgtxt-blue-lightest']};
-`
-
-const nonActiveItemStyle = css`
-  color: ${({ foundation }) => foundation?.theme?.['txt-black-darkest']};
-
-  &:hover {
-    background-color: ${({ foundation }) => foundation?.theme?.['bg-black-lighter']};
-  }
-
-  /*
-    ':focus-visible' Pseudo class는 Safari 15.4 이후 지원하기 때문에,
-    이를 ':hover' 와 ',' 연산자로 연결할 경우 hover, focus-visible 모두 작동하지 않는 이슈가 있음.
-    https://developer.mozilla.org/en-US/docs/Web/CSS/:focus-visible#browser_compatibility
-  */
-  &:focus-visible {
-    background-color: ${({ foundation }) => foundation?.theme?.['bg-black-lighter']};
-  }
-`
-
-export const Item = styled.button<WrapperProps>`
-  display: flex;
-  align-items: center;
-  width: 100%;
-  height: 28px;
-  padding: 0 6px;
-  cursor: pointer;
-  background-color: transparent;
-  border: 0;
-  outline: 0;
-  ${({ foundation }) => foundation?.rounding?.round6}
-
-  ${({ active }) => (active ? activeItemStyle : nonActiveItemStyle)}
-
-  ${({ interpolation }) => interpolation}
 `
 
 export const ChildrenWrapper = styled.ul`
