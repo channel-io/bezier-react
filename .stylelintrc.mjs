3fca921cbdc13e9d61180001f5ad577068c4016e
Ed Sungik Choi <sungik.dev@gmail.com>
2024-01-13 16:54:55 +0900

    chore(stylelint): refine lint rules

--- .stylelintrc.mjs
+++ .stylelintrc.mjs
@@ -5,7 +5,14 @@ export default {
     "stylelint-config-clean-order/error",
   ],
   "rules": {
-    // NOTE: Turn off a rule because you have a case that depends on a specific enum value(e.g. variants, size).
-    "selector-class-pattern": null,
+    "selector-class-pattern": [
+      // NOTE: Allows Pascal case(components) and Kebab case(states, variants, etc.).
+      "^(?:[A-Z][a-zA-Z0-9]*|[a-z][a-z0-9]*(?:-[a-z0-9]+)*)$",
+      // NOTE: Lowered severity because some class names depend on certain enum values(e.g. variants, size).
+      // TODO: Elevate severity to "error"
+      { "severity": "warning" },
+    ],
+    // NOTE: Set to reduce difficulties caused by selector specificity between components.
+    "selector-max-specificity": ["0,2,0"],
   }
 }
