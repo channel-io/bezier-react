0cfcd8d922fe8954d7fb6f421ad2eaf38c964a65
Ed Sungik Choi <sungik.dev@gmail.com>
2024-01-13 16:04:06 +0900

    chore(deps): add & apply stylelint-config-clean-order

--- .stylelintrc.mjs
+++ .stylelintrc.mjs
@@ -1,6 +1,9 @@
 /** @type {import('stylelint').Config} */
 export default {
-  "extends": ["stylelint-config-standard-scss"],
+  "extends": [
+    "stylelint-config-standard-scss",
+    "stylelint-config-clean-order/error",
+  ],
   "rules": {
     // NOTE: Turn off a rule because you have a case that depends on a specific enum value(e.g. variants, size).
     "selector-class-pattern": null,
