0bb2f9fcbf43d52ebf3c118714b198adca2b1a85
Ed Sungik Choi <sungik.dev@gmail.com>
2024-01-18 14:43:21 +0900

    test(select): update test cases

--- packages/bezier-react/src/components/Forms/Inputs/Select/Select.test.tsx
+++ packages/bezier-react/src/components/Forms/Inputs/Select/Select.test.tsx
@@ -3,24 +3,15 @@ import React from 'react'
 import { fireEvent } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 
-import { LightFoundation } from '~/src/foundation'
-
-import { ZIndex } from '~/src/constants/ZIndex'
 import { render } from '~/src/utils/test'
 
 import {
-  SELECT_CONTAINER_TEST_ID,
   SELECT_DROPDOWN_TEST_ID,
-  SELECT_TRIGGER_TEST_ID,
-  SELECT_TRIGGER_TEXT_TEST_ID,
   Select,
 } from './Select'
-import {
-  type SelectProps,
-  SelectSize,
-} from './Select.types'
+import { type SelectProps } from './Select.types'
 
-describe('Select Test >', () => {
+describe('Select', () => {
   let props: SelectProps
 
   beforeEach(() => {
@@ -35,7 +26,7 @@ describe('Select Test >', () => {
 
   const renderSelect = (optionProps?: Partial<SelectProps>) => render(<Select {...props} {...optionProps} />)
 
-  describe('Event >', () => {
+  describe('Event', () => {
     const renderFormWithSelect = (onSubmit: React.FormEventHandler) => render(
       <form onSubmit={onSubmit}>
         <Select {...props} />
@@ -44,8 +35,8 @@ describe('Select Test >', () => {
 
     it('should fire click event when clicking the trigger button', () => {
       const onClick = jest.fn()
-      const { getByTestId } = renderSelect({ onClickTrigger: onClick })
-      const rendered = getByTestId(SELECT_TRIGGER_TEST_ID)
+      const { getByRole } = renderSelect({ onClickTrigger: onClick })
+      const rendered = getByRole('button')
 
       fireEvent.click(rendered)
       expect(onClick).toHaveBeenCalled()
@@ -53,238 +44,15 @@ describe('Select Test >', () => {
 
     it('should not fire submit event when clicking the trigger button', () => {
       const onSubmit = jest.fn()
-      const { getByTestId } = renderFormWithSelect(onSubmit)
-      const rendered = getByTestId(SELECT_TRIGGER_TEST_ID)
+      const { getByRole } = renderFormWithSelect(onSubmit)
+      const rendered = getByRole('button')
 
       fireEvent.click(rendered)
       expect(onSubmit).not.toHaveBeenCalled()
     })
   })
 
-  describe('Default Style >', () => {
-    it('Snapshot >', () => {
-      const { container } = renderSelect({ text: 'foo' })
-      expect(container.firstChild).toMatchSnapshot()
-    })
-
-    it('Container >', () => {
-      const { getByTestId } = renderSelect()
-      const defaultSelect = getByTestId(SELECT_CONTAINER_TEST_ID)
-
-      expect(defaultSelect).toHaveStyle('position: relative;')
-    })
-
-    it('Trigger >', () => {
-      const { getByTestId } = renderSelect()
-      const defaultSelectTrigger = getByTestId(SELECT_TRIGGER_TEST_ID)
-
-      expect(defaultSelectTrigger).toHaveStyle('display: flex;')
-      expect(defaultSelectTrigger).toHaveStyle('align-items: center;')
-      expect(defaultSelectTrigger).toHaveStyle('width: 100%;')
-      expect(defaultSelectTrigger).toHaveStyle('justify-content: space-between;')
-      expect(defaultSelectTrigger).toHaveStyle('padding: 8px 12px;')
-      expect(defaultSelectTrigger).toHaveStyle('cursor: pointer;')
-    })
-
-    it('Dropdown >', async () => {
-      const user = userEvent.setup()
-      const { getByTestId } = renderSelect()
-
-      const defaultSelectTrigger = getByTestId(SELECT_TRIGGER_TEST_ID)
-      await user.click(defaultSelectTrigger)
-
-      const defaultSelectDropdown = getByTestId(SELECT_DROPDOWN_TEST_ID)
-
-      expect(defaultSelectDropdown).toHaveStyle(`z-index: ${ZIndex.Overlay};`)
-      expect(defaultSelectDropdown).toHaveStyle('min-width: 200px;')
-      expect(defaultSelectDropdown).toHaveStyle('min-height: 42px;')
-      expect(defaultSelectDropdown).toHaveStyle('max-height: 640px;')
-    })
-  })
-
-  describe('SelectSize >', () => {
-    it('Size only effects to trigger style >', async () => {
-      const user = userEvent.setup()
-      const XL_CONTAINER_TEST_ID = `${SELECT_CONTAINER_TEST_ID}_XL`
-      const XL_TRIGGER_TEST_ID = `${SELECT_TRIGGER_TEST_ID}_XL`
-      const XL_DROPDOWN_TEST_ID = `${SELECT_DROPDOWN_TEST_ID}_XL`
-      const L_CONTAINER_TEST_ID = `${SELECT_CONTAINER_TEST_ID}_L`
-      const L_TRIGGER_TEST_ID = `${SELECT_TRIGGER_TEST_ID}_L`
-      const L_DROPDOWN_TEST_ID = `${SELECT_DROPDOWN_TEST_ID}_L`
-
-      const { getByTestId: getByTestIdForXL } = renderSelect({
-        size: SelectSize.XL,
-        testId: XL_CONTAINER_TEST_ID,
-        triggerTestId: XL_TRIGGER_TEST_ID,
-        dropdownTestId: XL_DROPDOWN_TEST_ID,
-      })
-      const { getByTestId: getByTestIdForL } = renderSelect({
-        size: SelectSize.L,
-        testId: L_CONTAINER_TEST_ID,
-        triggerTestId: L_TRIGGER_TEST_ID,
-        dropdownTestId: L_DROPDOWN_TEST_ID,
-      })
-
-      // Select Size.XL
-      const xlSelectContainer = getByTestIdForXL(XL_CONTAINER_TEST_ID)
-      const xlSelectTrigger = getByTestIdForXL(XL_TRIGGER_TEST_ID)
-      // Open Dropdown
-      await user.click(xlSelectTrigger)
-      const xlSelectDropdown = getByTestIdForXL(XL_DROPDOWN_TEST_ID)
-      // Close Dropdown
-      await user.click(xlSelectTrigger)
-      const xlContainerStyle = window.getComputedStyle(xlSelectContainer)
-      const xlDropdownStyle = window.getComputedStyle(xlSelectDropdown)
-
-      // Select Size.L
-      const lSelectContainer = getByTestIdForL(L_CONTAINER_TEST_ID)
-      const lSelectTrigger = getByTestIdForL(L_TRIGGER_TEST_ID)
-      // Open Dropdown
-      await user.click(lSelectTrigger)
-      const lSelectDropdown = getByTestIdForL(L_DROPDOWN_TEST_ID)
-      // Close Dropdown
-      await user.click(lSelectTrigger)
-      const lContainerStyle = window.getComputedStyle(lSelectContainer)
-      const lDropdownStyle = window.getComputedStyle(lSelectDropdown)
-
-      // Container
-      expect(xlContainerStyle.getPropertyValue('display')).toEqual(lContainerStyle.getPropertyValue('display'))
-
-      // Dropdown
-      expect(xlDropdownStyle.getPropertyValue('width')).toEqual(lDropdownStyle.getPropertyValue('width'))
-      expect(xlDropdownStyle.getPropertyValue('min-width')).toEqual(lDropdownStyle.getPropertyValue('min-width'))
-      expect(xlDropdownStyle.getPropertyValue('min-height')).toEqual(lDropdownStyle.getPropertyValue('min-height'))
-      expect(xlDropdownStyle.getPropertyValue('max-height')).toEqual(lDropdownStyle.getPropertyValue('max-height'))
-    })
-
-    it('Size S >', () => {
-      const { getByTestId } = renderSelect({ size: SelectSize.S })
-      const defaultSelectDropdown = getByTestId(SELECT_TRIGGER_TEST_ID)
-
-      expect(defaultSelectDropdown).toHaveStyle('height: 28px;')
-    })
-
-    it('Size M >', () => {
-      const { getByTestId } = renderSelect({ size: SelectSize.M })
-      const defaultSelectDropdown = getByTestId(SELECT_TRIGGER_TEST_ID)
-
-      expect(defaultSelectDropdown).toHaveStyle('height: 36px;')
-    })
-
-    it('Size L >', () => {
-      const { getByTestId } = renderSelect({ size: SelectSize.L })
-      const defaultSelectDropdown = getByTestId(SELECT_TRIGGER_TEST_ID)
-
-      expect(defaultSelectDropdown).toHaveStyle('height: 44px;')
-    })
-
-    it('Size XL >', () => {
-      const { getByTestId } = renderSelect({ size: SelectSize.XL })
-      const defaultSelectDropdown = getByTestId(SELECT_TRIGGER_TEST_ID)
-
-      expect(defaultSelectDropdown).toHaveStyle('height: 54px;')
-    })
-  })
-
-  describe('Trigger text style >', () => {
-    it('shows default text color', () => {
-      const { getByTestId } = renderSelect({ text: 'lorem ipsum' })
-      const triggerText = getByTestId(SELECT_TRIGGER_TEXT_TEST_ID)
-
-      expect(triggerText).toHaveStyle('color: var(--txt-black-darkest);')
-    })
-
-    it('shows given text color', () => {
-      const { getByTestId } = renderSelect({ text: 'lorem ipsum', textColor: 'bgtxt-green-normal' })
-      const triggerText = getByTestId(SELECT_TRIGGER_TEXT_TEST_ID)
-
-      expect(triggerText).toHaveStyle('color: var(--bgtxt-green-normal);')
-    })
-
-    it('shows placeholder text color if content is empty', () => {
-      const { getByTestId } = renderSelect({ text: '', textColor: 'bgtxt-blue-normal' })
-      const triggerText = getByTestId(SELECT_TRIGGER_TEXT_TEST_ID)
-
-      expect(triggerText).toHaveStyle('color: var(--txt-black-dark);')
-    })
-  })
-
-  describe('Disabled style', () => {
-    it('should have dimmed opacity', () => {
-      const { getByTestId } = renderSelect({ disabled: true })
-      const trigger = getByTestId(SELECT_TRIGGER_TEST_ID)
-
-      expect(trigger).toHaveStyle('opacity: var(--opacity-disabled)')
-    })
-
-    it('should have not-allowed cursor style', () => {
-      const { getByTestId } = renderSelect({ disabled: true })
-      const trigger = getByTestId(SELECT_TRIGGER_TEST_ID)
-
-      expect(trigger).toHaveStyle('cursor: not-allowed')
-    })
-
-    it('should not show dropdown when clicked', async () => {
-      const user = userEvent.setup()
-      const { getByTestId } = renderSelect({ disabled: true })
-      const trigger = getByTestId(SELECT_TRIGGER_TEST_ID)
-
-      await user.click(trigger)
-
-      expect(() => getByTestId(SELECT_DROPDOWN_TEST_ID)).toThrow() // element should not exist
-    })
-
-    it('should not update style when hovered', () => {
-      const { getByTestId } = renderSelect({ disabled: true })
-      const trigger = getByTestId(SELECT_TRIGGER_TEST_ID)
-
-      fireEvent.mouseOver(trigger)
-
-      expect(trigger).toHaveStyle(`background-color: ${LightFoundation.theme['bg-grey-lightest']}`)
-    })
-
-    it('overrides read-only style', () => {
-      const { getByTestId } = renderSelect({ readOnly: true, disabled: true })
-      const trigger = getByTestId(SELECT_TRIGGER_TEST_ID)
-
-      expect(trigger).toHaveStyle('opacity: var(--opacity-disabled)')
-    })
-  })
-
-  describe('Read-only style', () => {
-    it('should have regular opacity', () => {
-      const { getByTestId } = renderSelect({ readOnly: true })
-      const trigger = getByTestId(SELECT_TRIGGER_TEST_ID)
-
-      expect(trigger).toHaveStyle('opacity: ;')
-    })
-
-    it('should have initial cursor style', () => {
-      const { getByTestId } = renderSelect({ readOnly: true })
-      const trigger = getByTestId(SELECT_TRIGGER_TEST_ID)
-
-      expect(trigger).toHaveStyle('cursor: initial')
-    })
-
-    it('should not show dropdown when clicked', async () => {
-      const user = userEvent.setup()
-      const { getByTestId } = renderSelect({ readOnly: true })
-      const trigger = getByTestId(SELECT_TRIGGER_TEST_ID)
-
-      await user.click(trigger)
-
-      expect(() => getByTestId(SELECT_DROPDOWN_TEST_ID)).toThrow() // element should not exist
-    })
-
-    it('should have chevron with txt-black-dark color', () => {
-      const { getByTestId } = renderSelect({ readOnly: true })
-      const trigger = getByTestId(SELECT_TRIGGER_TEST_ID)
-
-      expect(trigger.children.item(1)).toHaveStyle('--b-icon-color: var(--txt-black-dark)')
-    })
-  })
-
-  describe('defaultFocus >', () => {
+  describe('defaultFocus', () => {
     it('should not show dropdown when the first time If the defaultFocus is false >', () => {
       const { queryByTestId } = renderSelect({ defaultFocus: false })
       const dropdown = queryByTestId(SELECT_DROPDOWN_TEST_ID)
@@ -309,13 +77,6 @@ describe('Select Test >', () => {
       await user.click(body)
 
       expect(dropdown).not.toBeVisible()
-    })
-  })
-
-  describe('rightContent >', () => {
-    it('Snapshot >', () => {
-      const { container } = renderSelect({ text: 'lorem ipsum', rightContent: (<div>가나다</div>) })
-      expect(container.firstChild).toMatchSnapshot()
     })
   })
 })
--- packages/bezier-react/src/components/Forms/Inputs/Select/Select.tsx
+++ packages/bezier-react/src/components/Forms/Inputs/Select/Select.tsx
@@ -37,9 +37,6 @@ import {
 
 import styles from './Select.module.scss'
 
-export const SELECT_CONTAINER_TEST_ID = 'bezier-react-select-container'
-export const SELECT_TRIGGER_TEST_ID = 'bezier-react-select-trigger'
-export const SELECT_TRIGGER_TEXT_TEST_ID = 'bezier-react-select-trigger-text'
 export const SELECT_DROPDOWN_TEST_ID = 'bezier-react-select-dropdown'
 
 export const Select = forwardRef<SelectRef, SelectProps>(function Select({
@@ -62,9 +59,9 @@ export const Select = forwardRef<SelectRef, SelectProps>(function Select({
   dropdownMarginY = 6,
   dropdownZIndex = 'overlay',
   dropdownPosition = OverlayPosition.BottomLeft,
-  testId = SELECT_CONTAINER_TEST_ID,
-  triggerTestId = SELECT_TRIGGER_TEST_ID,
-  triggerTextTestId = SELECT_TRIGGER_TEXT_TEST_ID,
+  testId = 'bezier-react-select-container',
+  triggerTestId = 'bezier-react-select-trigger',
+  triggerTextTestId = 'bezier-react-select-trigger-text',
   dropdownTestId = SELECT_DROPDOWN_TEST_ID,
   onClickTrigger,
   onHideDropdown,
