a4647d30b62a92a354c8b49c1eeb496879b157f5
Yang Wooseong (Andrew) <dntjddid1@gmail.com>
2024-01-19 15:01:27 +0900

    Add rounding to interpolation transform (#1928)
    <!--
      How to write a good PR title:
    - Follow [the Conventional Commits
    specification](https://www.conventionalcommits.org/en/v1.0.0/).
      - Give as much context as necessary and as little as possible
      - Prefix it with [WIP] while it’s a work in progress
    -->
    
    ## Self Checklist
    
    - [x] I wrote a PR title in **English** and added an appropriate
    **label** to the PR.
    - [x] I wrote the commit message in **English** and to follow [**the
    Conventional Commits
    specification**](https://www.conventionalcommits.org/en/v1.0.0/).
    - [x] I [added the
    **changeset**](https://github.com/changesets/changesets/blob/main/docs/adding-a-changeset.md)
    about the changes that needed to be released. (or didn't have to)
    - [x] I wrote or updated **documentation** related to the changes. (or
    didn't have to)
    - [x] I wrote or updated **tests** related to the changes. (or didn't
    have to)
    - [x] I tested the changes in various browsers. (or didn't have to)
      - Windows: Chrome, Edge, (Optional) Firefox
      - macOS: Chrome, Edge, Safari, (Optional) Firefox
    
    ## Related Issue
    <!-- Please link to issue if one exists -->
    
    <!-- Fixes #0000 -->
    
    - https://github.com/channel-io/bezier-react/issues/1778
    
    ## Summary
    <!-- Please brief explanation of the changes made -->
    
    - 사용처가 매우 적긴 하지만, `${Rounding.round12}` 와 같은 경우가 interpolation 변환
    codemod 에서 누락되어서 이를 추가합니다.
    
    ## Details
    <!-- Please elaborate description of the changes -->
    
    - Foundation 의 경우 theme, border, elevation 등을 한번에 묶어서 변환하는 codemod가
    있습니다. 이것처럼 interpolation 역시 한번에 묶어서 변환하는 codemod 를 추가하고, 다음과 같이 naming 을
    변경합니다.
    - `v2-typography-interpolation-to-css-variable` ->
    `v2-interpolation-to-css-variable-typography`
    - `v2-input-interpolation-to-css-variable` ->
    `v2-interpolation-to-css-variable-input`
    - `v2-z-index-interpolation-to-css-variable` ->
    `v2-interpolation-to-css-variable-z-index`
    - `v2-interpolation-to-css-variable`: 위 transform 들을 모두 실행 
    
    ### Breaking change? (Yes/No)
    <!-- If Yes, please describe the impact and migration path for users -->
    
    - No
    
    ## References
    <!-- Please list any other resources or points the reviewer should be
    aware of -->
    
    - None

--- packages/bezier-codemod/src/transforms/v2-input-interpolation-to-css-variable/transform.test.ts
+++
@@ -1,9 +0,0 @@
-import { testTransformFunction } from '../../utils/test.js'
-
-import interpolationTransform from './transform.js'
-
-describe('input interpolation transform', () => {
-  it('should transform input interpolation to css variable', () => {
-    testTransformFunction(__dirname, 'input-interpolation', interpolationTransform)
-  })
-})
