712eb4a1ef19ec3082ce56e49c2e923fb844ccde
Ed Sungik Choi <sungik.dev@gmail.com>
2024-02-01 20:08:38 +0900

    docs(props): add jsdoc to common props

--- packages/bezier-react/src/types/props.ts
+++ packages/bezier-react/src/types/props.ts
@@ -1,5 +1,8 @@
-import { type CSSProperties } from 'react'
-import type React from 'react'
+import type {
+  CSSProperties,
+  ElementType,
+  ReactNode,
+} from 'react'
 
 import type {
   BackgroundSemanticColor,
@@ -11,70 +14,125 @@ import type {
   ZIndex,
 } from './tokens'
 
-/* Component Base Props */
+/**
+ * Base configuration properties for components, including the HTML element type and test ID.
+ */
 export interface RenderConfigProps {
   /**
    * Specifies which HTML tag should be used to render this component.
-   *
-   * `as` prop directly comes from the polymorphic "as" prop of `styled-components`.
+   * This prop comes directly from the polymorphic "as" prop of `styled-components`.
    * @see https://styled-components.com/docs/api#as-polymorphic-prop
    */
-  as?: React.ElementType
+  as?: ElementType
 
   /**
-   * The test id attribute compatible with `@testing-library/react`.
+   * A test ID attribute compatible with `@testing-library/react`.
    */
   testId?: string
 }
 
+/**
+ * Properties for styling components, including inline CSS and class names.
+ */
 export interface StylableComponentProps {
   /**
-   * Customized inline CSS style for this component.
+   * Custom inline CSS styles for the component.
    */
   style?: CSSProperties
-
   /**
-   * Customized CSS classname for this component.
+   * Custom CSS class name for the component.
    */
   className?: string
 }
 
-/* Component Additional Props */
-export interface ContentProps<Content = React.ReactNode> {
+/**
+ * Props for components that include content.
+ */
+export interface ContentProps<Content = ReactNode> {
+  /**
+   * Content to be rendered within the component.
+   */
   content?: Content
 }
 
-export interface ChildrenProps<Children = React.ReactNode> {
+/**
+ * Props for components that include children elements.
+ */
+export interface ChildrenProps<Children = ReactNode> {
+  /**
+   * Child elements to be rendered within the component.
+   */
   children?: Children
 }
 
+/**
+ * Props for components with variant styles or behaviors.
+ */
 export interface VariantProps<Variant extends string | number> {
+  /**
+   * Variant of the component to be used.
+   */
   variant?: Variant
 }
 
+/**
+ * Props for components with size variations.
+ */
 export interface SizeProps<Size extends string | number> {
+  /**
+   * Size of the component.
+   */
   size?: Size
 }
 
-export interface SideContentProps<LeftContent = React.ReactNode, RightContent = React.ReactNode> {
+/**
+ * Props for components that have content on their sides (left or right).
+ */
+export interface SideContentProps<LeftContent = ReactNode, RightContent = ReactNode> {
+  /**
+   * Content to be displayed on the left side of the component.
+   */
   leftContent?: LeftContent
+  /**
+   * Content to be displayed on the right side of the component.
+   */
   rightContent?: RightContent
 }
 
+/**
+ * Props for components that can be disabled.
+ */
 export interface DisableProps {
+  /**
+   * Whether the component is disabled.
+   */
   disabled?: boolean
 }
 
+/**
+ * Props for components that use color from the design system's palette.
+ */
 export interface ColorProps {
+  /**
+   * Color from the design system's semantic color palette.
+   */
   color?: SemanticColor
 }
 
+/**
+ * Props for components that require an identifier (ID).
+ */
 export interface IdentifierProps {
+  /**
+   * Unique identifier for the component.
+   */
   id: string
 }
 
 type PropNameType = string | string[]
-
+/**
+ * Generic type for defining additional properties with a specific suffix.
+ */
 type AdditionalProps<PropName extends PropNameType, Suffix extends string, PropType> = {
   [Key in `${PropName extends string
     ? PropName
@@ -82,23 +140,47 @@ type AdditionalProps<PropName extends PropNameType, Suffix extends string, PropT
   }${Capitalize<Suffix>}`]?: PropType
 }
 
+/**
+ * Props for adding custom styles to named elements within a component.
+ */
 type AdditionalStyleProps<ElementName extends PropNameType> =
   AdditionalProps<ElementName, 'style', CSSProperties>
 
+/**
+ * Props for adding custom class names to named elements within a component.
+ */
 type AdditionalClassNameProps<ElementName extends PropNameType> =
   AdditionalProps<ElementName, 'className', string>
 
+/**
+ * Props for adding test IDs to named elements within a component, useful for testing purposes.
+ */
 export type AdditionalTestIdProps<ElementName extends PropNameType> =
   AdditionalProps<ElementName, 'testId', string>
 
+/**
+ * Props for adding color properties to named elements within a component.
+ */
 export type AdditionalColorProps<ElementName extends PropNameType> =
   AdditionalProps<ElementName, 'color', SemanticColor>
 
+/**
+ * Props for components that can be activated or deactivated.
+ */
 export interface ActivatableProps {
+  /**
+   * Whether the component is active.
+   */
   active?: boolean
 }
 
+/**
+ * Props for components that act as a link.
+ */
 export interface LinkProps {
+  /**
+   * URL or a URL fragment that the hyperlink points to.
+   */
   href?: string
 }
 
@@ -116,13 +198,20 @@ export interface AlphaBezierComponentProps extends
   Omit<RenderConfigProps, 'as'>,
   Omit<StylableComponentProps, 'interpolation'> {}
 
+/**
+ * Props for polymorphic components that can render as different element types.
+ * @todo Generic type for `as` prop.
+ */
 export interface PolymorphicProps {
   /**
    * Element type to render.
    */
-  as?: React.ElementType
+  as?: ElementType
 }
 
+/**
+ * Props for specifying margins around a component.
+ */
 export interface MarginProps {
   /**
    * the margin area on all four sides of an element.
@@ -164,6 +253,9 @@ export interface MarginProps {
 type Position = 'absolute' | 'fixed' | 'relative' | 'sticky'
 type Overflow = 'auto' | 'hidden' | 'scroll' | 'visible'
 
+/**
+ * Props for defining layout-related properties of a component, such as padding, size, and position.
+ */
 export interface LayoutProps {
   /**
    * the padding area on all four sides of an element.
@@ -337,8 +429,14 @@ export interface LayoutProps {
   overflowY?: Overflow
 }
 
+/**
+ * Enumeration of form field sizes. (TextField, Select)
+ */
 export type FormFieldSize = 'xl' | 'l' | 'm' | 'xs'
 
+/**
+ * Props for form field components, including states like disabled, error, and required.
+ */
 export interface FormFieldProps extends
   DisableProps,
   Partial<IdentifierProps> {
