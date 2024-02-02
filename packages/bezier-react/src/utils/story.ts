c8066bef42bf88f5b69d62791be2b18fc4aae3da
Ed Sungik Choi <sungik.dev@gmail.com>
2024-02-02 18:16:57 +0900

    refactor(story-utils): add generic

--- packages/bezier-react/src/utils/story.ts
+++ packages/bezier-react/src/utils/story.ts
@@ -4,9 +4,9 @@ interface Enum {
   [id: string]: string | number
 }
 
-export const getObjectFromEnum = (input: Enum) => Object.entries(input)
+export const getObjectFromEnum = <T extends Enum>(enumeration: T) => Object.entries(enumeration)
   .reduce((acc, [key, value]) => (
     isNaN(Number(key))
       ? { ...acc, [key]: value }
       : acc
-  ), {})
+  ), {} as { [K in keyof T]: T[K] })
