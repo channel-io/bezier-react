738a4b2e1f3035d7c98202b6dbdb2d187fce2258
Ed Sungik Choi <sungik.dev@gmail.com>
2024-02-02 18:45:11 +0900

    test: update test

--- packages/bezier-react/src/utils/story.test.ts
+++ packages/bezier-react/src/utils/story.test.ts
@@ -1,34 +1,8 @@
-import { type IconName } from '@channel.io/bezier-icons'
+import { getObjectFromEnum } from './story'
 
-import {
-  getObjectFromEnum,
-  iconList,
-} from './story'
-import { isArray } from './type'
-
-describe('storyUtils Test >', () => {
-  describe('iconList Test >', () => {
-    it('iconList는 리스트이다', () => {
-      expect(isArray(iconList)).toBe(true)
-    })
-
-    it('iconList는 IconName type인 string을 가지고 있다', () => {
-      const sampleIconName: IconName = 'arrow-hook-left-down'
-      expect(iconList.includes(sampleIconName)).toBe(true)
-    })
-
-    it('iconList는 IconName이 아닌 것들은 가지고 있지 않다', () => {
-      // @ts-ignore
-      expect(iconList.includes(undefined)).toBe(false)
-      // @ts-ignore
-      expect(iconList.includes(null)).toBe(false)
-      // @ts-ignore
-      expect(iconList.includes('iphone')).toBe(false)
-    })
-  })
-
-  describe('getObejctFromEnum Test >', () => {
-    it('getObjectFromEnum 정상 동작', () => {
+describe('story', () => {
+  describe('getObjectFromEnum', () => {
+    it('converts a non-empty enum to an object with enum members as keys and values', () => {
       enum FooEnum {
         A = 'A',
         B = 'B',
@@ -37,7 +11,7 @@ describe('storyUtils Test >', () => {
       expect(getObjectFromEnum(FooEnum)).toMatchObject({ A: 'A', B: 'B', C: 'C' })
     })
 
-    it('Empty Enum이 들어가면 {} 을 뱉는다', () => {
+    it('converts an empty enum to an empty object', () => {
       enum EmptyEnum {}
       expect(getObjectFromEnum(EmptyEnum)).toMatchObject({})
     })
