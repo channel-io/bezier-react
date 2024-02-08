acf05d63239188acf949430feeb9c74a4a7e81da
andrew <andrew@channel.io>
2024-02-08 10:10:06 +0900

    feat(codemod): fix bug in `v2-import-from-bezier-to-styled-components`

---
+++ packages/bezier-codemod/src/transforms/v2-import-from-bezier-to-styled-components/fixtures/only-named-import.input.tsx
@@ -0,0 +1,3 @@
+import { css } from '@channel.io/bezier-react'
+
+import { type Device } from './device'
---
+++ packages/bezier-codemod/src/transforms/v2-import-from-bezier-to-styled-components/fixtures/only-named-import.output.tsx
@@ -0,0 +1,2 @@
+import { css } from 'styled-components';
+import { type Device } from './device'
