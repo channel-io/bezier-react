b1465757ace7ffa4dcb7a2d15d6125ed6603d14a
Ed Sungik Choi <sungik.dev@gmail.com>
2024-03-18 19:00:45 +0900

    feat: export TooltipPrimitive

---
+++ packages/bezier-react/src/alpha-components/TooltipPrimitive/TooltipPrimitive.types.ts
@@ -0,0 +1,38 @@
+import type {
+  TooltipArrowProps,
+  TooltipContentProps,
+  TooltipPortalProps,
+  TooltipProps,
+  TooltipProviderProps,
+  TooltipTriggerProps,
+} from '@radix-ui/react-tooltip'
+
+/**
+ * @see {@link https://www.radix-ui.com/primitives/docs/components/tooltip#provider}
+ */
+export type TooltipPrimitiveProviderProps = TooltipProviderProps
+
+/**
+ * @see {@link https://www.radix-ui.com/primitives/docs/components/tooltip#root}
+ */
+export type TooltipPrimitiveProps = TooltipProps
+
+/**
+ * @see {@link https://www.radix-ui.com/primitives/docs/components/tooltip#trigger}
+ */
+export type TooltipPrimitiveTriggerProps = TooltipTriggerProps
+
+/**
+ * @see {@link https://www.radix-ui.com/primitives/docs/components/tooltip#portal}
+ */
+export type TooltipPrimitivePortalProps = TooltipPortalProps
+
+/**
+ * @see {@link https://www.radix-ui.com/primitives/docs/components/tooltip#content}
+ */
+export type TooltipPrimitiveContentProps = TooltipContentProps
+
+/**
+ * @see {@link https://www.radix-ui.com/primitives/docs/components/tooltip#arrow}
+ */
+export type TooltipPrimitiveArrowProps = TooltipArrowProps
--- packages/bezier-react/src/alpha-components/index.ts
+++ packages/bezier-react/src/alpha-components/index.ts
@@ -1,1 +1,2 @@
 export * from './DialogPrimitive'
+export * from './TooltipPrimitive'
