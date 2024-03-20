8763bafd540d675d40a387dae02a2ac7a519d518
andrew <andrew@channel.io>
2024-03-20 17:27:04 +0900

    feat(vs-code): set any type explicitly

--- packages/bezier-vscode/src/utils.ts
+++ packages/bezier-vscode/src/utils.ts
@@ -1,5 +1,5 @@
 // Merge a `source` object to a `target` recursively
-export const merge = (target, source) => {
+export const merge = (target: any, source: any) => {
   // Iterate through `source` properties and if an `Object` set property to merge of `target` and `source` properties
   for (const key of Object.keys(source)) {
     if (source[key] instanceof Object) {
