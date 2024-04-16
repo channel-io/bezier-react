a67dbdcf153689e825bf45399033f381cecb6074
andrew <andrew@channel.io>
2024-04-16 17:37:37 +0900

    feat(avatar): add types

---
+++ packages/bezier-react/src/components/AlphaAvatar/AlphaAvatar.types.ts
@@ -0,0 +1,62 @@
+import {
+  type BezierComponentProps,
+  type ChildrenProps,
+  type DisableProps,
+  type SizeProps,
+} from '~/src/types/props'
+
+import { type StatusType } from '~/src/components/Status'
+
+export type AlphaAvatarSize =
+  | '20'
+  | '24'
+  | '30'
+  | '36'
+  | '42'
+  | '48'
+  | '72'
+  | '90'
+  | '120'
+
+interface AlphaAvatarOwnProps {
+  /**
+   * Semantic name of the avatar.
+   */
+  name: string
+  /**
+   * Asset image URL of the avatar.
+   * @default ''
+   */
+  avatarUrl?: string
+  /**
+   * Fallback image URL for the avatar.
+   * @default DEFAULT_AVATAR_URL
+   */
+  fallbackUrl?: string
+  /**
+   * Additional status type for this avatar.
+   * Typically, this is used to indicate the status of the user (online, offline, etc).
+   *
+   * If additional customization of the status component is needed,
+   * pass the custom status component as `children` of the Avatar component.
+   */
+  status?: StatusType
+  /**
+   * Whether to display the border of the avatar.
+   * @default false
+   */
+  showBorder?: boolean
+  /**
+   * Whether to use the smooth corners feature.
+   * Overrides whether the smooth corners feature is applied when it is enabled.
+   * @default true
+   */
+  smoothCorners?: boolean
+}
+
+export interface AvatarProps
+  extends BezierComponentProps<'div'>,
+    SizeProps<AlphaAvatarSize>,
+    DisableProps,
+    ChildrenProps,
+    AlphaAvatarOwnProps {}
