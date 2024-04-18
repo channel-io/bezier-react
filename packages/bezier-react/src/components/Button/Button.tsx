405b9496b14741e4ee6b7aaa6fe150f9ff448a0b
andrew <andrew@channel.io>
2024-04-18 17:14:03 +0900

    chore(button): dirty in bezier-react

--- packages/bezier-react/src/components/Button/Button.tsx
+++ packages/bezier-react/src/components/Button/Button.tsx
@@ -17,7 +17,7 @@ import styles from './Button.module.scss'
 
 export const BUTTON_TEST_ID = 'bezier-button'
 
-function getTypography(size: ButtonSize) {
+function getTypography_dirty(size: ButtonSize) {
   return (
     {
       xs: '13',
@@ -147,7 +147,7 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
           {text && (
             <Text
               className={styles.ButtonText}
-              typo={getTypography(size)}
+              typo={getTypography_dirty(size)}
               bold
             >
               {text}
