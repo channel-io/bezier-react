0d6d0e4b93aea1d642a586296cd294c8a0661a63
Yang Wooseong (Andrew) <dntjddid1@gmail.com>
2024-05-07 16:34:13 +0900

    Add `AlphaFloatingButton` component (#2193)
    <!--
      How to write a good PR title:
    - Follow [the Conventional Commits
    specification](https://www.conventionalcommits.org/en/v1.0.0/).
      - Give as much context as necessary and as little as possible
      - Prefix it with [WIP] while it’s a work in progress
    -->
    
    ## Self Checklist
    
    - [x] I wrote a PR title in **English** and added an appropriate
    **label** to the PR.
    - [x] I wrote the commit message in **English** and to follow [**the
    Conventional Commits
    specification**](https://www.conventionalcommits.org/en/v1.0.0/).
    - [x] I [added the
    **changeset**](https://github.com/changesets/changesets/blob/main/docs/adding-a-changeset.md)
    about the changes that needed to be released. (or didn't have to)
    - [x] I wrote or updated **documentation** related to the changes. (or
    didn't have to)
    - [x] I wrote or updated **tests** related to the changes. (or didn't
    have to)
    - [x] I tested the changes in various browsers. (or didn't have to)
      - Windows: Chrome, Edge, (Optional) Firefox
      - macOS: Chrome, Edge, Safari, (Optional) Firefox
    
    ## Related Issue
    
    <!-- Please link to issue if one exists -->
    
    <!-- Fixes #0000 -->
    
    - Fixes https://github.com/channel-io/bezier-react/issues/2179 
    
    ## Summary
    
    <!-- Please brief explanation of the changes made -->
    
    - `AlphaFloatingButton` 컴포넌트를 구현합니다. `AlphaButton`컴포넌트와 일부 스타일을 제외하고 거의
    동일합니다.
    
    ## Details
    
    <!-- Please elaborate description of the changes -->
    
    - 생략
    
    ### Breaking change? (Yes/No)
    
    <!-- If Yes, please describe the impact and migration path for users -->
    
    - No
    
    ## References
    
    <!-- Please list any other resources or points the reviewer should be
    aware of -->
    
    - [컴포넌트
    스펙(internal)](https://www.notion.so/channelio/6c9950c5805041b8ae27d86f3bdeabd5?v=8e329a647fa84541841528bbe482ebdb&p=182cc8dec41c49fba941055ef9fca738&pm=s)
    -
    [피그마](https://www.figma.com/file/8J76noM3T1Sp5cNPhnYQiG/Action?type=design&node-id=112%3A8172&mode=design&t=G1I09yRJJnWQ2PTF-1)

---
+++ packages/bezier-react/src/components/AlphaFloatingButton/FloatingButton.types.ts
@@ -0,0 +1,71 @@
+import { type BezierIcon } from '@channel.io/bezier-icons'
+
+import {
+  type BezierComponentProps,
+  type DisableProps,
+  type PolymorphicProps,
+  type SizeProps,
+} from '~/src/types/props'
+
+export type FloatingButtonVariant = 'primary' | 'secondary'
+
+export type FloatingButtonColor =
+  | 'blue'
+  | 'cobalt'
+  | 'red'
+  | 'orange'
+  | 'green'
+  | 'pink'
+  | 'purple'
+  | 'dark-grey'
+  | 'light-grey'
+
+export type FloatingButtonSize = 'xs' | 's' | 'm' | 'l' | 'xl'
+
+interface FloatingButtonOwnProps {
+  /**
+   * The text content in the button.
+   */
+  text: string
+
+  /**
+   * If `loading` is true, spinner will be shown, replacing the content.
+   * @default false
+   */
+  loading?: boolean
+
+  /**
+   * If `active` is true, the button will be styled as if it is hovered.
+   * @default false
+   */
+  active?: boolean
+
+  /**
+   * Types of visual styles for button.
+   * @default 'primary'
+   */
+  variant?: FloatingButtonVariant
+
+  /**
+   * Color of the button.
+   * @default 'blue'
+   */
+  color?: FloatingButtonColor
+
+  /**
+   * Icon on the left.
+   */
+  prefixIcon?: BezierIcon
+
+  /**
+   * Icon on the right.
+   */
+  suffixIcon?: BezierIcon
+}
+
+export interface FloatingButtonProps
+  extends Omit<BezierComponentProps<'button'>, 'color'>,
+    PolymorphicProps,
+    SizeProps<FloatingButtonSize>,
+    DisableProps,
+    FloatingButtonOwnProps {}
---
+++ packages/bezier-react/src/components/AlphaFloatingButton/index.ts
@@ -0,0 +1,7 @@
+export { FloatingButton as AlphaFloatingButton } from './FloatingButton'
+export type {
+  FloatingButtonColor as AlphaFloatingButtonColor,
+  FloatingButtonProps as AlphaFloatingButtonProps,
+  FloatingButtonSize as AlphaFloatingButtonSize,
+  FloatingButtonVariant as AlphaFloatingButtonVariant,
+} from './FloatingButton.types'
