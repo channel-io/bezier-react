80bf7c108c5c8be31b4bc6aa7d797ce49ae40efd
andrew <andrew@channel.io>
2024-05-14 16:01:33 +0900

    feat(floating-icon-button): storybook

---
+++ packages/bezier-react/src/components/AlphaFloatingIconButton/AlphaFloatingIconButton.stories.tsx
@@ -0,0 +1,28 @@
+import { PlusIcon } from '@channel.io/bezier-icons'
+import { type Meta, type StoryObj } from '@storybook/react'
+
+import {
+  AlphaFloatingIconButton,
+  type AlphaFloatingIconButtonProps,
+} from '~/src/components/AlphaFloatingIconButton'
+
+const meta: Meta<typeof AlphaFloatingIconButton> = {
+  component: AlphaFloatingIconButton,
+  argTypes: {
+    onClick: { action: 'onClick' },
+  },
+}
+export default meta
+
+export const Playground: StoryObj<AlphaFloatingIconButtonProps> = {
+  args: {
+    disabled: false,
+    active: false,
+    loading: false,
+    icon: PlusIcon,
+    'aria-label': 'invite',
+    size: 'm',
+    variant: 'primary',
+    color: 'blue',
+  },
+}
