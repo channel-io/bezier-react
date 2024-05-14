20819730644caf7e18313a8769e84a2f32312930
andrew <andrew@channel.io>
2024-05-14 15:59:59 +0900

    feat(floating-icon-button): add component

---
+++ packages/bezier-react/src/components/AlphaFloatingIconButton/FloatingIconButton.tsx
@@ -0,0 +1,88 @@
+import React, { forwardRef } from 'react'
+
+import classNames from 'classnames'
+
+import { type AlphaFloatingIconButtonProps } from '~/src/components/AlphaFloatingIconButton'
+import { BaseButton } from '~/src/components/BaseButton'
+import { type ButtonSize } from '~/src/components/Button'
+import { Icon } from '~/src/components/Icon'
+import { Spinner } from '~/src/components/Spinner'
+
+import styles from './FloatingIconButton.module.scss'
+
+function getIconSize(size: ButtonSize) {
+  return (
+    {
+      xs: 'xxs',
+      s: 'xs',
+      m: 's',
+      l: 's',
+      xl: 'm',
+    } as const
+  )[size]
+}
+
+function getSpinnerSize(size: ButtonSize) {
+  return (
+    {
+      xs: 'xs',
+      s: 'xs',
+      m: 's',
+      l: 's',
+      xl: 's',
+    } as const
+  )[size]
+}
+
+export const FloatingIconButton = forwardRef<
+  HTMLButtonElement,
+  AlphaFloatingIconButtonProps
+>(function FloatingIconButton(
+  {
+    as = BaseButton,
+    color = 'blue',
+    variant = 'primary',
+    size = 'm',
+    active,
+    icon,
+    loading,
+    className,
+    ...rest
+  },
+  forwardedRef
+) {
+  const Comp = as as typeof BaseButton
+
+  return (
+    <Comp
+      ref={forwardedRef}
+      className={classNames(
+        styles.FloatingIconButton,
+        styles[`size-${size}`],
+        styles[`variant-${variant}`],
+        styles[`color-${color}`],
+        active && styles.active,
+        className
+      )}
+      {...rest}
+    >
+      <div
+        className={classNames(styles.ButtonContent, loading && styles.loading)}
+      >
+        {icon && (
+          <Icon
+            size={getIconSize(size)}
+            source={icon}
+          />
+        )}
+      </div>
+
+      {/* TODO: use AlphaSpinner */}
+      {loading && (
+        <div className={styles.ButtonLoader}>
+          <Spinner size={getSpinnerSize(size)} />
+        </div>
+      )}
+    </Comp>
+  )
+})
---
+++ packages/bezier-react/src/components/AlphaFloatingIconButton/FloatingIconButton.types.ts
@@ -0,0 +1,62 @@
+import { type BezierIcon } from '@channel.io/bezier-icons'
+
+import {
+  type BezierComponentProps,
+  type DisableProps,
+  type PolymorphicProps,
+  type SizeProps,
+} from '~/src/types/props'
+
+type FloatingIconButtonVariant = 'primary' | 'secondary'
+
+type FloatingIconButtonColor =
+  | 'blue'
+  | 'cobalt'
+  | 'red'
+  | 'orange'
+  | 'green'
+  | 'pink'
+  | 'purple'
+  | 'dark-grey'
+  | 'light-grey'
+
+type FloatingIconButtonSize = 'xs' | 's' | 'm' | 'l' | 'xl'
+
+interface FloatingIconButtonOwnProps {
+  /**
+   * If `loading` is true, spinner will be shown, replacing the content.
+   * @default false
+   */
+  loading?: boolean
+
+  /**
+   * If `active` is true, the button will be styled as if it is hovered.
+   * You may want to use this prop for a button which opens dropdown, etc.
+   * @default false
+   */
+  active?: boolean
+
+  /**
+   * Types of visual styles for button.
+   * @default 'primary'
+   */
+  variant?: FloatingIconButtonVariant
+
+  /**
+   * Color of the button.
+   * @default 'blue'
+   */
+  color?: FloatingIconButtonColor
+
+  /**
+   * Icon in the button.
+   */
+  icon?: BezierIcon
+}
+
+export interface FloatingIconButtonProps
+  extends Omit<BezierComponentProps<'button'>, 'color'>,
+    PolymorphicProps,
+    SizeProps<FloatingIconButtonSize>,
+    DisableProps,
+    FloatingIconButtonOwnProps {}
