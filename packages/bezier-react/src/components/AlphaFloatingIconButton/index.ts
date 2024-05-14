94d16ad3c32afea3ff30afbc41521ec3acd2b082
andrew <andrew@channel.io>
2024-05-14 16:01:22 +0900

    feat(floating-icon-button): export

---
+++ packages/bezier-react/src/components/AlphaFloatingIconButton/index.ts
@@ -0,0 +1,2 @@
+export { FloatingIconButton as AlphaFloatingIconButton } from './FloatingIconButton'
+export type { FloatingIconButtonProps as AlphaFloatingIconButtonProps } from './FloatingIconButton.types'
