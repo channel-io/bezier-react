9e5a180f202532c0ee2e42c8663d0687be818562
nabi.lee <hello@nabi.kim>
2024-05-24 22:34:39 +0900

    fix: fix react forwardRef errors in BaseTagBadgeText

--- packages/bezier-react/src/components/BaseTagBadge/BaseTagBadge.tsx
+++ packages/bezier-react/src/components/BaseTagBadge/BaseTagBadge.tsx
@@ -51,10 +51,11 @@ export const BaseTagBadge = forwardRef<HTMLDivElement, BaseTagBadgeProps>(
 export const BaseTagBadgeText = forwardRef<
   HTMLDivElement,
   BaseTagBadgeTextProps
->(function BaseTagBadgeText({ size, children, ...rest }) {
+>(function BaseTagBadgeText({ size, children, ...rest }, forwardRef) {
   return (
     <Text
       typo={getProperTypo(size)}
+      ref={forwardRef}
       {...rest}
     >
       {children}
