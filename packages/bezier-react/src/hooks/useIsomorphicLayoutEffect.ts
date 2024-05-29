b553a124f0342070341018b71dd08d933c385f4b
nabi.lee <hello@nabi.kim>
2024-05-29 14:46:20 +0900

    fix: fix eslint error
    - remove line-break on jsdoc

--- packages/bezier-react/src/hooks/useIsomorphicLayoutEffect.ts
+++ packages/bezier-react/src/hooks/useIsomorphicLayoutEffect.ts
@@ -3,7 +3,6 @@ import { useEffect, useLayoutEffect } from 'react'
 /**
  * @description This hook is used to handle the layout effect in a way that is compatible with SSR.
  * In Server Environments, this hook will use `useEffect` instead of `useLayoutEffect`.
- * 
  * @see https://react.dev/reference/react/useLayoutEffect#troubleshooting
  */
 export const useIsomorphicLayoutEffect =
