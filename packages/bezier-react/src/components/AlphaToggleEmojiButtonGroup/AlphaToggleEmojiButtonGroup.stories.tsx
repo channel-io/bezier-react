38dc0cc15e9794173fe7555a49e48c44daeb5b4b
andrew <andrew@channel.io>
2024-06-18 17:20:02 +0900

    feat(toggle-emoji-button-group): add storybook

---
+++ packages/bezier-react/src/components/AlphaToggleEmojiButtonGroup/AlphaToggleEmojiButtonGroup.stories.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+
+import type { Meta, StoryFn, StoryObj } from '@storybook/react'
+
+import {
+  ToggleEmojiButtonGroup,
+  ToggleEmojiButtonSource,
+} from './ToggleEmojiButtonGroup'
+import type {
+  ToggleEmojiButtonGroupProps,
+  ToggleEmojiButtonSourceProps,
+} from './ToggleEmojiButtonGroup.types'
+import { Emoji } from '~/src/components/Emoji'
+import { Center } from '~/src/components/Center'
+
+const meta: Meta<typeof ToggleEmojiButtonGroup> = {
+  component: ToggleEmojiButtonGroup,
+}
+export default meta
+
+type ToggleButtonCompositionType = ToggleEmojiButtonGroupProps &
+  Pick<ToggleEmojiButtonSourceProps, 'variant'>
+
+const Template: StoryFn<ToggleButtonCompositionType> = ({
+  fillDirection,
+  variant,
+}) => (
+  <Center
+    width={400}
+    height={100}
+    backgroundColor="bg-lounge"
+  >
+    <ToggleEmojiButtonGroup fillDirection={fillDirection}>
+      <ToggleEmojiButtonSource
+        variant={variant}
+        content={
+          <Emoji
+            name="channel"
+            size="30"
+            imageUrl="https://cf.exp.channel.io/asset/emoji/images/80/blush.png"
+          />
+        }
+      />
+      <ToggleEmojiButtonSource
+        variant={variant}
+        content={
+          <Emoji
+            name="channel"
+            size="30"
+            imageUrl="https://cf.exp.channel.io/asset/emoji/images/80/blush.png"
+          />
+        }
+      />
+    </ToggleEmojiButtonGroup>
+  </Center>
+)
+
+export const Primary = {
+  render: Template,
+
+  args: {
+    fillDirection: 'all',
+    variant: 'primary',
+  },
+} satisfies StoryObj<ToggleButtonCompositionType>
