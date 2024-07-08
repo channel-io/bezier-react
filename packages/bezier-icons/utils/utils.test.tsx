f03c26bf1d27cf9252de7bf07990003e4c88d9ed
andrew <andrew@channel.io>
2024-07-08 10:41:53 +0900

    feat(bezier-icons): resolve test fail

--- packages/bezier-icons/utils/utils.test.tsx
+++ packages/bezier-icons/utils/utils.test.tsx
@@ -1,14 +1,16 @@
+import { forwardRef } from 'react'
+
 import { createBezierIcon, isBezierIcon } from '.'
 
 describe('Icon utils', () => {
   describe('createBezierIcon', () => {
     it('isBezierIcon returns false for non BezierIcon', () => {
-      const source = () => <svg />
+      const source = forwardRef(() => <svg />)
       expect(isBezierIcon(source)).toBe(false)
     })
 
     it('createBezierIcon returns a BezierIcon', () => {
-      const source = () => <svg />
+      const source = forwardRef(() => <svg />)
       const mockBezierIcon = createBezierIcon(source)
       expect(isBezierIcon(mockBezierIcon)).toBe(true)
     })
