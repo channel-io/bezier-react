1f320a081f2b4e90dea811627a6315163beeadf9
andrew <andrew@channel.io>
2024-07-17 10:05:27 +0900

    feat(gh-action): rm unused code

--- packages/bezier-icons/scripts/generate-icon-files.js
+++
@@ -1,8 +0,0 @@
-const path = require('path')
-
-const { generateIconFiles } = require('./utils/generateIconFilesFromJson')
-
-const bezierIconsDirectory = path.resolve(__dirname, '../icons')
-const iconsJson = path.resolve(__dirname, '../icons.json')
-
-generateIconFiles(iconsJson, bezierIconsDirectory)
