3fb6d9ac70fc9bb02c924dd9e0932cf7200c0092
andrew <andrew@channel.io>
2024-10-21 14:21:00 +0900

    feat(button): add storybook for demo

--- packages/bezier-react/src/components/AlphaButton/AlphaButton.stories.tsx
+++ packages/bezier-react/src/components/AlphaButton/AlphaButton.stories.tsx
@@ -1,3 +1,5 @@
+import React, { Fragment } from 'react'
+
 import { ArrowRightIcon, PlusIcon } from '@channel.io/bezier-icons'
 import { type Meta, type StoryObj } from '@storybook/react'
 
@@ -5,6 +7,8 @@ import {
   AlphaButton,
   type AlphaButtonProps,
 } from '~/src/components/AlphaButton'
+import { HStack, VStack } from '~/src/components/Stack'
+import { Text } from '~/src/components/Text'
 
 const meta: Meta<typeof AlphaButton> = {
   component: AlphaButton,
@@ -31,5 +35,69 @@ export const Primary: StoryObj<AlphaButtonProps> = {
       type: 'link',
       url: 'https://www.figma.com/design/8J76noM3T1Sp5cNPhnYQiG/Action?node-id=0-1&t=WwfuBAWI872yYPf8-1',
     },
+  },
+}
+
+export const Demo: StoryObj<AlphaButtonProps> = {
+  render: () => {
+    const VARIANTS: AlphaButtonProps['variant'][] = [
+      'primary',
+      'secondary',
+      'tertiary',
+    ]
+    const COLORS: AlphaButtonProps['color'][] = [
+      'blue',
+      'cobalt',
+      'green',
+      'orange',
+      'red',
+      'purple',
+      'pink',
+      'dark-grey',
+      'light-grey',
+      'white-absolute',
+    ]
+
+    return (
+      <VStack spacing={6}>
+        {VARIANTS.map((variant) => {
+          return (
+            <Fragment key={variant}>
+              <Text>{variant}</Text>
+              <HStack>
+                {COLORS.map((color) => {
+                  return (
+                    <AlphaButton
+                      key={color}
+                      prefixContent={PlusIcon}
+                      suffixContent={ArrowRightIcon}
+                      text="button"
+                      variant={variant}
+                      color={color}
+                    />
+                  )
+                })}
+              </HStack>
+
+              <HStack>
+                {COLORS.map((color) => {
+                  return (
+                    <AlphaButton
+                      key={color}
+                      active
+                      prefixContent={PlusIcon}
+                      suffixContent={ArrowRightIcon}
+                      text="button"
+                      variant={variant}
+                      color={color}
+                    />
+                  )
+                })}
+              </HStack>
+            </Fragment>
+          )
+        })}
+      </VStack>
+    )
   },
 }
