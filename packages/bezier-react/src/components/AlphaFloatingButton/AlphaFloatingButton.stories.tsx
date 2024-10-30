91d94f3d31d7517e79394290c163939d7f577abd
andrew <andrew@channel.io>
2024-10-30 14:19:46 +0900

    feat(floating-button): demo

--- packages/bezier-react/src/components/AlphaFloatingButton/AlphaFloatingButton.stories.tsx
+++ packages/bezier-react/src/components/AlphaFloatingButton/AlphaFloatingButton.stories.tsx
@@ -1,3 +1,5 @@
+import React, { Fragment } from 'react'
+
 import { ArrowRightIcon, PlusIcon } from '@channel.io/bezier-icons'
 import { type Meta, type StoryObj } from '@storybook/react'
 
@@ -5,6 +7,8 @@ import {
   AlphaFloatingButton,
   type AlphaFloatingButtonProps,
 } from '~/src/components/AlphaFloatingButton'
+import { HStack, VStack } from '~/src/components/Stack'
+import { Text } from '~/src/components/Text'
 
 const meta: Meta<typeof AlphaFloatingButton> = {
   component: AlphaFloatingButton,
@@ -31,5 +35,67 @@ export const Primary: StoryObj<AlphaFloatingButtonProps> = {
       type: 'link',
       url: 'https://www.figma.com/design/8J76noM3T1Sp5cNPhnYQiG/Action?node-id=112-2766&t=7KmMal05dj8QL5kK-1',
     },
+  },
+}
+
+export const Demo: StoryObj<AlphaFloatingButtonProps> = {
+  render: () => {
+    const VARIANTS: AlphaFloatingButtonProps['variant'][] = [
+      'primary',
+      'secondary',
+    ]
+    const COLORS: AlphaFloatingButtonProps['color'][] = [
+      'blue',
+      'cobalt',
+      'green',
+      'orange',
+      'red',
+      'purple',
+      'pink',
+      'dark-grey',
+      'light-grey',
+    ]
+
+    return (
+      <VStack spacing={6}>
+        {VARIANTS.map((variant) => {
+          return (
+            <Fragment key={variant}>
+              <Text>{variant}</Text>
+              <HStack>
+                {COLORS.map((color) => {
+                  return (
+                    <AlphaFloatingButton
+                      key={color}
+                      prefixContent={PlusIcon}
+                      suffixContent={ArrowRightIcon}
+                      text="button"
+                      variant={variant}
+                      color={color}
+                    />
+                  )
+                })}
+              </HStack>
+
+              <HStack>
+                {COLORS.map((color) => {
+                  return (
+                    <AlphaFloatingButton
+                      key={color}
+                      active
+                      prefixContent={PlusIcon}
+                      suffixContent={ArrowRightIcon}
+                      text="button"
+                      variant={variant}
+                      color={color}
+                    />
+                  )
+                })}
+              </HStack>
+            </Fragment>
+          )
+        })}
+      </VStack>
+    )
   },
 }
