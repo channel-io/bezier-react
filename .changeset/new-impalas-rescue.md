8e6fb64c21d9704edbbb65f93ad6912a0704bee0
Yang Wooseong (Andrew) <dntjddid1@gmail.com>
2024-11-06 10:01:29 +0900

    Apply hovered color token for `AlphaButton`, `AlphaFloatingButton`, `AlphaFloatingIconButton` (#2471)
    <!--
      How to write a good PR title:
    - Follow [the Conventional Commits
    specification](https://www.conventionalcommits.org/en/v1.0.0/).
      - Give as much context as necessary and as little as possible
      - Prefix it with [WIP] while it’s a work in progress
    -->
    
    ## Self Checklist
    
    - [x] I wrote a PR title in **English** and added an appropriate
    **label** to the PR.
    - [x] I wrote the commit message in **English** and to follow [**the
    Conventional Commits
    specification**](https://www.conventionalcommits.org/en/v1.0.0/).
    - [x] I [added the
    **changeset**](https://github.com/changesets/changesets/blob/main/docs/adding-a-changeset.md)
    about the changes that needed to be released. (or didn't have to)
    - [x] I wrote or updated **documentation** related to the changes. (or
    didn't have to)
    - [x] I wrote or updated **tests** related to the changes. (or didn't
    have to)
    - [x] I tested the changes in various browsers. (or didn't have to)
      - Windows: Chrome, Edge, (Optional) Firefox
      - macOS: Chrome, Edge, Safari, (Optional) Firefox
    
    ## Related Issue
    
    <!-- Please link to issue if one exists -->
    
    <!-- Fixes #0000 -->
    
    - None
    
    ## Summary
    
    <!-- Please brief explanation of the changes made -->
    
    - global/color/green/300-0 토큰 값을 수정합니다.
    - hovered color 계산하는 로직을 수정하고 버튼의 호버 효과에 hovered color 를 적용합니다. 
    
    ## Details
    
    <!-- Please elaborate description of the changes -->
    
    - 계산 로직은
    [노션](https://www.notion.so/channelio/Redesign-Bezier-cd6e60b3257843c29ce56fab0dd2c76d?p=11b74b55ec7c8020b6fcfe272da7138b&pm=s)을
    참고해주세요. 기존 로직으로 하면 무채색일 때 눈에 너무 띄게 변하거나 눈에 안띄게 변하는 구간이 있어서 분기를 추가했습니다.
    확정된 버전이라서, 이전에 언급했던 것처럼 hovered color를 foundation 에 추가해서 스토리로 만들고, 유저가
    입력하는 색깔을 hovered color 로 바꿔서 보여주는 스토리도 추가해서 디자이너나 모바일 개발자가 참고할 수 있게 하면
    좋을 것 같습니다.
    - 버튼에 hovered color를 적용할 때 FloatingButton/secondary 인 경우는 배경색을 그대로 하고
    텍스트 색깔과 shadow 효과만 다르게 하기로 했습니다.
    - 프리뷰 용도로 색깔별로 버튼을 모아놓은 Hovered 스토리를 만들었습니다. 이건 리뷰 후에 없애도 좋을 것 같습니다. 
    
    ### Breaking change? (Yes/No)
    
    <!-- If Yes, please describe the impact and migration path for users -->
    
    - No
    
    ## References
    
    <!-- Please list any other resources or points the reviewer should be
    aware of -->
    
    -
    [스레드](https://desk.channel.io/root/threads/groups/(CT)BezierRedesign-244958/6715e85727129270fa7e/6715f05cec7def7e29f6)
    -
    [스레드](https://desk.channel.io/root/groups/(CT)BezierRedesign-244958/6720901d3c37af529d79)

---
+++ .changeset/new-impalas-rescue.md
@@ -0,0 +1,5 @@
+---
+'@channel.io/bezier-react': patch
+---
+
+Apply hovered color token for `AlphaButton`, `AlphaFloatingButton`, and `AlphaFloatingIconButton`.
--- packages/bezier-tokens/scripts/lib/utils.ts
+++ packages/bezier-tokens/scripts/lib/utils.ts
@@ -22,8 +22,10 @@ export const getHoveredColor = (value: string, theme: 'dark' | 'light') => {
   let lightness = l
   let saturation = s
 
+  const isAchromatic = saturation <= 0.1
+
   if (a === 0) {
-    alpha = 0.1
+    alpha = isAchromatic ? 0.05 : 0.1
   } else if (a <= 0.2) {
     alpha = alpha * 1.5
   }
@@ -31,7 +33,9 @@ export const getHoveredColor = (value: string, theme: 'dark' | 'light') => {
   if (a !== 0) {
     if (theme === 'light') {
       if (l <= 0.17) {
-        lightness = (l + 0.07) * 1.1
+        lightness = isAchromatic
+          ? (lightness + 0.1) * 1.1
+          : (lightness + 0.07) * 1.1
         saturation += 0.05
       } else {
         lightness *= 0.93
@@ -39,7 +43,9 @@ export const getHoveredColor = (value: string, theme: 'dark' | 'light') => {
       }
     } else {
       if (l >= 0.83) {
-        lightness = (lightness - 0.2) * 0.98
+        lightness = isAchromatic
+          ? (lightness - 0.04) * 0.97
+          : (lightness - 0.2) * 0.98
         saturation -= 0.03
       } else {
         lightness = (lightness + 0.04) * 1.005
@@ -47,7 +53,7 @@ export const getHoveredColor = (value: string, theme: 'dark' | 'light') => {
       }
     }
 
-    if (s <= 0.1 || s >= 0.9) {
+    if (isAchromatic || s >= 0.9) {
       saturation = s
     }
   }
