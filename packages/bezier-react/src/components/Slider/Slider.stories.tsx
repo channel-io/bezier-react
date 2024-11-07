e41a8821046ace502cf5d127cbd2c55f645f2f04
nayounsang <nayounsang722@gmail.com>
2024-11-07 18:00:35 +0900

    fix: process value props to number[] or undefined

--- packages/bezier-react/src/components/Slider/Slider.stories.tsx
+++ packages/bezier-react/src/components/Slider/Slider.stories.tsx
@@ -1,6 +1,9 @@
-import { type Meta } from '@storybook/react'
+import React from 'react'
+
+import { type Meta, type StoryFn } from '@storybook/react'
 
 import { Slider } from './Slider'
+import { type SliderProps } from './Slider.types'
 
 const meta: Meta<typeof Slider> = {
   component: Slider,
@@ -25,16 +28,39 @@ const meta: Meta<typeof Slider> = {
 }
 export default meta
 
-export const Primary = {
-  args: {
-    width: 285,
-    defaultValue: [5],
-    value: undefined,
-    disabled: false,
-    guide: [5],
-    min: 0,
-    max: 10,
-    step: 1,
-    disableTooltip: false,
-  },
+const Template: StoryFn<SliderProps> = (args) => {
+  const processedArgs = getProcessedArgs(args)
+  return <Slider {...processedArgs} />
+}
+
+export const Primary = Template.bind({})
+Primary.args = {
+  width: 285,
+  defaultValue: [5],
+  value: undefined,
+  disabled: false,
+  guide: [5],
+  min: 0,
+  max: 10,
+  step: 1,
+  disableTooltip: false,
+}
+
+const getProcessedArgs = (args: SliderProps): SliderProps => {
+  return { ...args, value: getProcessedValue(args.value) }
+}
+
+const getProcessedValue = (
+  value: undefined | Record<number, number> | number[]
+) => {
+  if (value === undefined) {
+    return undefined
+  }
+  if (Array.isArray(value) && value.every((item) => typeof item === 'number')) {
+    return value
+  }
+  if (typeof value === 'object' && value !== null) {
+    return Object.values(value)
+  }
+  return value
 }
