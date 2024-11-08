9cc10c78354e6bca25c12ce447a9d050b0a092de
nayounsang <nayounsang722@gmail.com>
2024-11-08 16:18:16 +0900

    refactor: split slider story into control and uncontrol

--- packages/bezier-react/src/components/Slider/Slider.stories.tsx
+++ packages/bezier-react/src/components/Slider/Slider.stories.tsx
@@ -8,11 +8,6 @@ import { type SliderProps } from './Slider.types'
 const meta: Meta<typeof Slider> = {
   component: Slider,
   argTypes: {
-    value: {
-      control: {
-        type: 'object',
-      },
-    },
     minStepsBetweenThumbs: {
       control: {
         type: 'number',
@@ -25,42 +20,28 @@ const meta: Meta<typeof Slider> = {
       action: 'onValueCommit',
     },
   },
+  args:{
+    width: 285,
+    disabled: false,
+    guide: [5],
+    min: 0,
+    max: 10,
+    step: 1,
+    disableTooltip: false,
+  }
 }
 export default meta
 
-const Template: StoryFn<SliderProps> = (args) => {
-  const processedArgs = getProcessedArgs(args)
-  return <Slider {...processedArgs} />
+const Template: StoryFn<SliderProps> = (otherSwitchProps) => {
+  return <Slider {...otherSwitchProps} />
 }
 
 export const Primary = Template.bind({})
 Primary.args = {
-  width: 285,
+  value: [5],
+}
+
+export const Uncontrolled = Template.bind({})
+Uncontrolled.args = {
   defaultValue: [5],
-  value: undefined,
-  disabled: false,
-  guide: [5],
-  min: 0,
-  max: 10,
-  step: 1,
-  disableTooltip: false,
-}
-
-const getProcessedArgs = (args: SliderProps): SliderProps => {
-  return { ...args, value: getProcessedValue(args.value) }
-}
-
-const getProcessedValue = (
-  value: undefined | Record<number, number> | number[]
-) => {
-  if (value === undefined) {
-    return undefined
-  }
-  if (Array.isArray(value) && value.every((item) => typeof item === 'number')) {
-    return value
-  }
-  if (typeof value === 'object' && value !== null) {
-    return Object.values(value)
-  }
-  return value
 }
