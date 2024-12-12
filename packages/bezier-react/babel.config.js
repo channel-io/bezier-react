187601b7ad9f876c58328a736eb28826f47768f9
Ed Sungik Choi <sungik.dev@gmail.com>
2024-12-12 13:25:17 +0900

    build(babel): remove unnecessary core-js pacakge

--- packages/bezier-react/babel.config.js
+++ packages/bezier-react/babel.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   presets: [
-    [
-      '@babel/preset-env',
-      { useBuiltIns: 'entry', corejs: '3.36', bugfixes: true },
-    ],
+    ['@babel/preset-env', { bugfixes: true }],
     /**
      * Will use the native built-in instead of trying to polyfill behavior for any plugins that require one.
      * Remove the helper function for Object.assign.
