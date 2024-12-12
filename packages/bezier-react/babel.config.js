de6a8bb40257db6259767da7d6696c215d45cf17
Ed Sungik Choi <sungik.dev@gmail.com>
2024-12-12 20:10:11 +0900

    build(babel): rm unnecessary option

--- packages/bezier-react/babel.config.js
+++ packages/bezier-react/babel.config.js
@@ -1,12 +1,7 @@
 module.exports = {
   presets: [
     ['@babel/preset-env', { bugfixes: true }],
-    /**
-     * Will use the native built-in instead of trying to polyfill behavior for any plugins that require one.
-     * Remove the helper function for Object.assign.
-     * @see https://babeljs.io/docs/babel-preset-react#usebuiltins
-     */
-    ['@babel/preset-react', { runtime: 'automatic', useBuiltIns: true }],
+    ['@babel/preset-react', { runtime: 'automatic' }],
     ['@babel/preset-typescript', { isTSX: true, allExtensions: true }],
   ],
 }
