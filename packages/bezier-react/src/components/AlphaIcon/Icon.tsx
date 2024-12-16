7c40c3eb71e5f55e0495b415807ba820d0c2f974
Ed Sungik Choi <sungik.dev@gmail.com>
2024-12-16 19:57:54 +0900

    feat(alpha-icon): implement

---
+++ packages/bezier-react/src/components/AlphaIcon/Icon.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import { forwardRef, memo } from 'react'
+import * as React from 'react'
+
+import classNames from 'classnames'
+
+import { getMarginStyles, splitByMarginProps } from '~/src/types/props-helpers'
+import { alphaColorTokenCssVar } from '~/src/utils/style'
+
+import { type IconProps } from './Icon.types'
+
+import styles from './Icon.module.scss'
+
+export const ICON_TEST_ID = 'bezier-alpha-icon'
+
+export const Icon = memo(
+  forwardRef<SVGSVGElement, IconProps>(function Icon(props, forwardedRef) {
+    const [marginProps, marginRest] = splitByMarginProps(props)
+    const marginStyles = getMarginStyles(marginProps)
+
+    const {
+      className,
+      size = 'm',
+      color,
+      source: SourceElement,
+      style,
+      ...rest
+    } = marginRest
+
+    return (
+      <SourceElement
+        ref={forwardedRef}
+        style={
+          {
+            '--b-icon-color': alphaColorTokenCssVar(color),
+            ...marginStyles.style,
+            ...style,
+          } as React.CSSProperties
+        }
+        className={classNames(
+          styles.Icon,
+          styles[`size-${size}`],
+          marginStyles.className,
+          className
+        )}
+        data-testid={ICON_TEST_ID}
+        {...rest}
+      />
+    )
+  })
+)
