6455a8f0fdd85e520ef87b8a2156726580316cde
Younsang Na <77400131+nayounsang@users.noreply.github.com>
2025-02-21 10:04:16 +0900

    Add truncated props in Badge (#2603)
    <!--
      How to write a good PR title:
    - Follow [the Conventional Commits
    specification](https://www.conventionalcommits.org/en/v1.0.0/).
      - Give as much context as necessary and as little as possible
      - Prefix it with [WIP] while it’s a work in progress
    -->
    
    ## Self Checklist
    
    - [x] I wrote a PR title in **English** and added an appropriate
    **label** to the PR.
    - [x] I wrote the commit message in **English** and to follow [**the
    Conventional Commits
    specification**](https://www.conventionalcommits.org/en/v1.0.0/).
    - [x] I [added the
    **changeset**](https://github.com/changesets/changesets/blob/main/docs/adding-a-changeset.md)
    about the changes that needed to be released. (or didn't have to)
    - [x] I wrote or updated **documentation** related to the changes. (or
    didn't have to)
    - [x] I wrote or updated **tests** related to the changes. (or didn't
    have to)
    - [x] I tested the changes in various browsers. (or didn't have to)
      - Windows: Chrome, Edge, (Optional) Firefox
      - macOS: Chrome, Edge, Safari, (Optional) Firefox
    
    ## Related Issue
    
    <!-- Please link to issue if one exists -->
    
    <!-- Fixes #0000 -->
    
    Fixes #2591 
    
    ## Summary
    
    <!-- Please brief explanation of the changes made -->
    
    - Add ellipsis to Badge
    
    ## Details
    
    <!-- Please elaborate description of the changes -->
    
    - Added `truncated` props to Badge and added the story of truncated
    Badge. It supports ellipses in Badge text.
    - The `width` must be specified from the outside or on itself.
    Otherwise, even if `truncated` props are specified, the length will
    increase because of `flex`.
    - Same truncated feature of Text.
    
    ### Breaking change? (Yes/No)
    
    <!-- If Yes, please describe the impact and migration path for users -->
    
    No
    
    ## References
    
    <!-- Please list any other resources or points the reviewer should be
    aware of -->

---
+++ .changeset/funny-eyes-shop.md
@@ -0,0 +1,5 @@
+---
+'@channel.io/bezier-react': minor
+---
+
+Add `truncated` props in the Badge
--- packages/bezier-react/src/components/Badge/Badge.stories.tsx
+++ packages/bezier-react/src/components/Badge/Badge.stories.tsx
@@ -1,6 +1,8 @@
 import { AppleIcon } from '@channel.io/bezier-icons'
 import { type Meta, type StoryFn, type StoryObj } from '@storybook/react'
 
+import { Box } from '~/src/components/Box'
+
 import { Badge } from './Badge'
 import { type BadgeProps } from './Badge.types'
 
@@ -14,6 +16,12 @@ const Template: StoryFn<BadgeProps> = ({ children, ...badgeProps }) => (
   <Badge {...badgeProps}>{children}</Badge>
 )
 
+const Truncated: StoryFn<BadgeProps> = ({ children, ...badgeProps }) => (
+  <Box width={200}>
+    <Badge {...badgeProps}>{children}</Badge>
+  </Box>
+)
+
 export const Primary: StoryObj<BadgeProps> = {
   render: Template,
 
@@ -23,4 +31,24 @@ export const Primary: StoryObj<BadgeProps> = {
     icon: AppleIcon,
     variant: 'default',
   },
+  argTypes: {
+    truncated: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+}
+
+export const Secondary: StoryObj<BadgeProps> = {
+  render: Truncated,
+
+  args: {
+    children: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    size: 'm',
+    icon: AppleIcon,
+    variant: 'default',
+    truncated: true,
+  },
+  name: 'Truncated',
 }
