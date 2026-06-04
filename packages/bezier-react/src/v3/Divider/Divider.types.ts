76edfe97050e407d297dea19078e89eff98a7c9d
Timo <155946237+timok1m@users.noreply.github.com>
2026-06-04 15:57:00 +0900

    feat: add v3 Divider component

---
+++ packages/bezier-react/src/v3/Divider/Divider.types.ts
@@ -0,0 +1,32 @@
+import type { SeparatorProps as SeparatorPrimitiveProps } from '@radix-ui/react-separator'
+
+import type { BezierComponentProps } from '~/src/types/props'
+
+interface DividerOwnProps {
+  /**
+   * If true, the divider will be rendered without side padding,
+   * which stands for the padding on the left and right sides of the horizontal divider,
+   * and the top and bottom sides of the vertical divider.
+   * @default false
+   */
+  withoutSideIndent?: boolean
+
+  /**
+   * If true, the divider will be rendered without parallel padding,
+   * which stands for the padding on the top and bottom sides of the horizontal divider,
+   * and the left and right sides of the vertical divider.
+   * @default false
+   */
+  withoutParallelIndent?: boolean
+
+  /**
+   * If true, the divider will be rendered without padding,
+   * @default false
+   */
+  withoutIndent?: boolean
+}
+
+export interface DividerProps
+  extends BezierComponentProps<'div'>,
+    SeparatorPrimitiveProps,
+    DividerOwnProps {}
--- packages/bezier-react/src/v3/index.ts
+++ packages/bezier-react/src/v3/index.ts
@@ -1,3 +1,4 @@
 // Prefer this subpath for new Bezier React usage.
 // Root exports are kept for compatibility with older component APIs.
 export * from './Box'
+export * from './Divider'
