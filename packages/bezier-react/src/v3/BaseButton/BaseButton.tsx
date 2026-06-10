7ae764470755272cefab875fccc0c9fc885be9b6
Timo <155946237+timok1m@users.noreply.github.com>
2026-06-10 13:16:04 +0900

    feat(bezier-react): add v3 base button

---
+++ packages/bezier-react/src/v3/BaseButton/BaseButton.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import { forwardRef } from 'react'
+
+import classNames from 'classnames'
+
+import { type BaseButtonProps } from './BaseButton.types'
+
+import styles from './BaseButton.module.scss'
+
+/**
+ * `BaseButton` is a reset-style button component with a focus ring, intended for internal use only.
+ */
+export const BaseButton = forwardRef<HTMLButtonElement, BaseButtonProps>(
+  function BaseButton(
+    { className, children, type = 'button', ...rest },
+    forwardedRef
+  ) {
+    return (
+      <button
+        className={classNames(styles.BaseButton, className)}
+        ref={forwardedRef}
+        type={type}
+        {...rest}
+      >
+        {children}
+      </button>
+    )
+  }
+)
