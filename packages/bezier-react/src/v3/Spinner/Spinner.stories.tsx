b8f3f601adf81ed7ba73e899c56cd9b083799c92
Timo <155946237+timok1m@users.noreply.github.com>
2026-06-10 13:19:10 +0900

    fix(bezier-react): align v3 spinner sizing

--- packages/bezier-react/src/v3/Spinner/Spinner.stories.tsx
+++ packages/bezier-react/src/v3/Spinner/Spinner.stories.tsx
@@ -1,4 +1,6 @@
-import { type Meta, type StoryFn } from '@storybook/react'
+import { type Meta, type StoryFn, type StoryObj } from '@storybook/react'
+
+import { HStack } from '~/src/v3/HStack'
 
 import { Spinner } from './Spinner'
 import { type SpinnerProps } from './Spinner.types'
@@ -24,5 +26,30 @@ export const Primary = {
 
   args: {
     size: 'm',
+  },
+}
+
+export const Sizes: StoryObj<SpinnerProps> = {
+  render: (args) => (
+    <HStack
+      spacing={16}
+      align="center"
+    >
+      {(['xs', 's', 'm', 'l', 'xl'] as const).map((size) => (
+        <Spinner
+          key={size}
+          {...args}
+          size={size}
+        />
+      ))}
+    </HStack>
+  ),
+
+  argTypes: {
+    size: {
+      table: {
+        disable: true,
+      },
+    },
   },
 }
