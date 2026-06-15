978521f7b74086ddd6c6134a41075911e5f375b2
Timo <155946237+timok1m@users.noreply.github.com>
2026-06-15 17:15:18 +0900

    feat(v3): add form field components (#2837)
    ## Self Checklist
    
    - [x] I wrote a PR title in **English** and added an appropriate
    **label** to the PR.
    - [x] I wrote the commit message in **English** and to follow [**the
    Conventional Commits
    specification**](https://www.conventionalcommits.org/en/v1.0.0/).
    - [x] I [added the
    **changeset**](https://github.com/changesets/changesets/blob/main/docs/adding-a-changeset.md)
    about the changes that needed to be released. (or didn't have to)
    - [x] I wrote or updated **documentation** related to the changes. (or
    didn't have to)
    - [x] I wrote or updated **tests** related to the changes. (or didn't
    have to)
    - [ ] I tested the changes in various browsers. (or didn't have to)
      - Windows: Chrome, Edge, (Optional) Firefox
      - macOS: Chrome, Edge, Safari, (Optional) Firefox
    
    ## Related Issue
    
    WEBTECH-8001
    
    ## Summary
    
    v3 FormField 계열 컴포넌트를 추가하고, v3 입력 컴포넌트들이 새 FormField context를 사용하도록
    연결했습니다.
    
    ## Details
    
    - v3 `FormField` 계열 컴포넌트를 추가했습니다.
    - `FormField`, `FormLabel`, `FormHelperText`, `FormErrorMessage`,
    `FormGroup`, `Help`를 제공합니다.
      - v3 form-field 역할을 기존 `FormControl` 대신 `FormField`로 제공합니다.
      - `m`, `l` 사이즈와 label/helper/error aria 연결, grouped control 지원을 포함합니다.
      - `FormErrorMessage`는 에러 아이콘을 텍스트 앞에 표시합니다.
    - `Help`를 v3에 추가했습니다.
      - tooltip 기반 동작은 기존 구조를 따릅니다.
      - hover/open 상태에서 v3 icon hover 색상을 사용합니다.
      - 독립 컴포넌트로 `V3 components/Help` story를 제공합니다.
    - FormField 하위 컴포넌트 story를 추가했습니다.
    - `FormLabel`, `FormHelperText`, `FormErrorMessage`, `FormGroup` 사용례를
    `V3 components/FormField` 하위에서 확인할 수 있습니다.
      - `FormGroup` story는 `spacing={10}` 예시를 제공합니다.
    - v3 입력 컴포넌트를 새 FormField context에 연결했습니다.
    - `TextInput`, `TextArea`, `Checkbox`가 v3 `FormField`의
    `useFormFieldProps`를 사용합니다.
      - Checkbox의 FormField 연동 테스트를 업데이트했습니다.
    - `@channel.io/bezier-react` changeset을 추가했습니다.
    
    ### Breaking change? (Yes/No)
    
    No
    
    ## References
    
    - `yarn lint`
    - `yarn typecheck`
    - `yarn jest --config packages/bezier-react/jest.config.js
    --runTestsByPath
    packages/bezier-react/src/v3/FormField/FormField.test.tsx
    packages/bezier-react/src/v3/TextInput/TextInput.test.tsx
    packages/bezier-react/src/v3/TextArea/TextArea.test.tsx
    packages/bezier-react/src/v3/Checkbox/Checkbox.test.tsx
    --coverage=false`

---
+++ packages/bezier-react/src/v3/FormField/FormField.types.ts
@@ -0,0 +1,117 @@
+import type {
+  FormFieldProps as BaseFormFieldProps,
+  BezierComponentProps,
+  ChildrenProps,
+  IdentifierProps,
+  SizeProps,
+  V3MarginProps,
+} from '~/src/types/props'
+import { type TextProps } from '~/src/v3/Text'
+
+export type FormFieldLabelPosition = 'top' | 'left'
+
+export type FormFieldSize = 'm' | 'l'
+
+export interface FormFieldAriaProps {
+  'aria-labelledby'?: string
+  'aria-describedby'?: string
+}
+
+interface CallbackRefProps {
+  ref: (node: HTMLElement | null) => void
+}
+
+interface FormFieldClassNameProps {
+  className: string
+}
+
+type PropsGetter<ExtraReturnType = {}> = <Props = {}>(
+  props: Props
+) => Props & Partial<IdentifierProps> & ExtraReturnType
+
+export type GroupPropsGetter = PropsGetter<
+  CallbackRefProps & FormFieldAriaProps
+>
+
+export type LabelPropsGetter = PropsGetter<FormFieldClassNameProps>
+
+export type FieldPropsGetter = PropsGetter<
+  Omit<FormFieldAriaProps, 'aria-labelledby'>
+>
+
+export type HelperTextPropsGetter = PropsGetter<
+  FormFieldClassNameProps &
+    CallbackRefProps & {
+      visible: boolean
+    }
+>
+
+export type ErrorMessagePropsGetter = HelperTextPropsGetter
+
+export interface FormFieldContextValue extends BaseFormFieldProps {
+  id: string
+  labelId: string
+  helperTextId: string
+  errorMessageId: string
+  size?: FormFieldSize
+  getGroupProps: GroupPropsGetter
+  getLabelProps: LabelPropsGetter
+  getFieldProps: FieldPropsGetter
+  getHelperTextProps: HelperTextPropsGetter
+  getErrorMessageProps: ErrorMessagePropsGetter
+}
+
+interface FormFieldOwnProps {
+  /**
+   * Layout position of the label.
+   * @default 'top'
+   */
+  labelPosition?: FormFieldLabelPosition
+}
+
+export interface FormFieldContainerProps
+  extends BezierComponentProps<'div'>,
+    ChildrenProps,
+    Pick<FormFieldOwnProps, 'labelPosition'> {}
+
+export interface FormFieldProps
+  extends BezierComponentProps<'div'>,
+    ChildrenProps,
+    BaseFormFieldProps,
+    SizeProps<FormFieldSize>,
+    FormFieldOwnProps {}
+
+interface FormLabelOwnProps {
+  htmlFor?: string
+  help?: React.ReactNode
+}
+
+export interface FormLabelProps
+  extends Omit<TextProps, keyof V3MarginProps>,
+    ChildrenProps,
+    Partial<IdentifierProps>,
+    FormLabelOwnProps {}
+
+interface BaseHelperTextOwnProps {
+  type: 'info' | 'error'
+}
+
+export interface BaseHelperTextProps
+  extends Omit<TextProps, keyof V3MarginProps>,
+    ChildrenProps,
+    Partial<IdentifierProps>,
+    BaseHelperTextOwnProps {}
+
+export interface FormHelperTextProps
+  extends Omit<BaseHelperTextProps, 'type'> {}
+
+export interface FormErrorMessageProps extends FormHelperTextProps {}
+
+type FormGroupDirection = 'horizontal' | 'vertical'
+
+export interface FormGroupProps
+  extends BezierComponentProps<'div'>,
+    ChildrenProps {
+  direction?: FormGroupDirection
+  spacing?: string | number
+}
---
+++ packages/bezier-react/src/v3/FormField/index.ts
@@ -0,0 +1,19 @@
+export {
+  FormField,
+  useFormFieldContext,
+  useFormFieldProps,
+} from './FormField'
+export type {
+  FormFieldAriaProps,
+  FormFieldContextValue,
+  FormFieldLabelPosition,
+  FormFieldProps,
+  FormFieldSize,
+  FormErrorMessageProps,
+  FormGroupProps,
+  FormHelperTextProps,
+  FormLabelProps,
+} from './FormField.types'
+export { FormLabel } from './FormLabel'
+export { FormHelperText, FormErrorMessage } from './FormHelperText'
+export { FormGroup } from './FormGroup'
