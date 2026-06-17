020957cac02784dcc9146c1ffdcfc1fbe74b6bf0
Timo <155946237+timok1m@users.noreply.github.com>
2026-06-17 23:50:53 +0900

    chore(bezier-react): trim v3 public surface

--- packages/bezier-react/src/v3/Overlay/index.ts
+++ packages/bezier-react/src/v3/Overlay/index.ts
@@ -1,7 +1,2 @@
-export { CONTAINER_TEST_ID, ESCAPE_KEY, OVERLAY_TEST_ID, Overlay } from './Overlay'
-export type {
-  ContainerRectAttr,
-  OverlayPosition,
-  OverlayProps,
-  TargetRectAttr,
-} from './Overlay.types'
+export { Overlay } from './Overlay'
+export type { OverlayPosition, OverlayProps } from './Overlay.types'
